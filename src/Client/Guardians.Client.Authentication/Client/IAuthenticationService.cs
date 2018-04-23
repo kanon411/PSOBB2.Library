324c9289f40804ed55fd7099843520d6022c9fc9
HelloKitty <andrew.blakely@ymail.com>
2018-04-23 11:07:29 -0500

    Integrated directly HaloLive TypeSafe.Http.NET auth client

---
+++ src/Client/Guardians.Client.Authentication/Client/IAuthenticationService.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeSafe.Http.Net;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Proxy interface for Authentication Server RPCs.
+	/// </summary>
+	public interface IAuthenticationService
+	{
+		/// <summary>
+		/// Authenticate request method. Sends the request model as a URLEncoded body.
+		/// See the documentation for information about the endpoint.
+		/// https://github.com/HaloLive/Documentation
+		/// </summary>
+		/// <param name="request">The request model.</param>
+		/// <returns>The authentication result.</returns>
+		[Post("/api/auth")]
+		Task<JWTModel> TryAuthenticate([UrlEncodedBody] AuthenticationRequestModel request);
+	}
+}
---
+++ src/Guardians.Models.Authentication/IUserAuthenticationDetailsContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Contract for types that contain or expose user details.
+	/// </summary>
+	public interface IUserAuthenticationDetailsContainer
+	{
+		/// <summary>
+		/// Username for the authentication request.
+		/// </summary>
+		string UserName { get; }
+
+		/// <summary>
+		/// Password for the authentication request.
+		/// </summary>
+		string Password { get; }
+	}
+}

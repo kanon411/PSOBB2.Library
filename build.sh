7b2eb444f1397fe528c903889e2cb6ed7e59b5d7
HelloKitty <andrew.blakely@ymail.com>
2018-04-25 17:50:04 -0500

    Try new docker path AGAIN!!

--- build.sh
+++ build.sh
@@ -3,4 +3,4 @@ dotnet publish src/Server/Guardians.Service.Authentication/Guardians.Service.Aut
 
 mkdir /app/build
 mkdir /app/build/auth
-mv /app/src/Server/Guardians.Service.Authentication/bin/Release/netcoreapp2.0/publish /app/build/auth
+mv /app/src/Server/Guardians.Service.Authentication/bin/Release/netcoreapp2.0/publish/* /app/build/auth

70b72c6bf00de6fa05472016f3020bb6db0f8f8a
HelloKitty <andrew.blakely@ymail.com>
2018-04-25 17:36:23 -0500

    Try fix docker paths again

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 dotnet restore Guardians.Library.sln
 dotnet publish src/Server/Guardians.Service.Authentication/Guardians.Service.Authentication.csproj -c release
 
-mkdir build
-mkdir build/auth
-mv ./src/Server/Guardians.Service.Authentication/bin/Release/netcoreapp2.0/publish ./build/auth
+mkdir /app/build
+mkdir /app/build/auth
+mv /app/src/Server/Guardians.Service.Authentication/bin/Release/netcoreapp2.0/publish /app/build/auth

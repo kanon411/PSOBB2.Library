04203dc27b769c30be8732b56b441c2ea874744b
HelloKitty <andrew.blakely@ymail.com>
2018-04-25 17:13:37 -0500

    Try new docker file

--- build.sh
+++ build.sh
@@ -2,5 +2,5 @@ dotnet restore Guardians.Library.sln
 dotnet publish src/Server/Guardians.Service.Authentication/Guardians.Service.Authentication.csproj -c release
 
 mkdir build
-mkdir build\auth
-mv src\Server\Guardians.Service.Authentication\bin\Release\netcoreapp2.0\publish build\auth
+mkdir build/auth
+mv src/Server/Guardians.Service.Authentication/bin/Release/netcoreapp2.0/publish build/auth

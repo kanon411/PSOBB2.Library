d5e51b7d167050a4b32dcd066602a96cf3a144cc
HelloKitty <andrew.blakely@ymail.com>
2018-04-26 13:20:54 -0500

    added appveyor test build

---
+++ appveyor-build-test.ps1
@@ -0,0 +1,13 @@
+##Looks through the entire test directory and runs dotnet build
+##foreach file in test/*
+foreach($f in Get-ChildItem ./test/)
+{
+    ##foreach file in the src/*/ directory that ends with the .csproj format
+    foreach($ff in (Get-ChildItem (Join-Path ./test/ $f.Name) | Where-Object { $_.Name.EndsWith(".csproj") }))
+    {
+        ##Add the project path + the csproj name and add the include referenced projects argument which will
+        ##force nuget dependencies
+        $projectArgs = "build " + (Join-Path (Join-Path test/ $f.Name) $ff.Name)## + "-c Release"
+        Start-Process dotnet $projectArgs -Wait
+    }
+}
--- run.bat
+++ run.bat
@@ -1,3 +1,3 @@
 cd build\auth
-start "auth" cmd /c dotnet Guardians.Service.Authentication.dll --usehttps=Certs/TLSCert.pfx --url=http://172.16.42.120:5001
+start "auth" cmd /c dotnet Guardians.Service.Authentication.dll --usehttps=Certs/TLSCert.pfx --url=http://0.0.0.0:5001
 cd ..

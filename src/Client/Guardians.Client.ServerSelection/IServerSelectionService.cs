69452d57923126f1236cbec4da4155aec1d8dda9
HelloKitty <andrew.blakely@ymail.com>
2018-05-04 17:43:10 -0500

    Added ServerSelection Models/Client

---
+++ src/Client/Guardians.Client.ServerSelection/IServerSelectionService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeSafe.Http.Net;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Proxy interface for ServerSelection (List) Server RPCs.
+	/// </summary>
+	[Header("User-Agent", "GuardiansClient")]
+	public interface IServerSelectionService
+	{
+		/// <summary>
+		/// Gets all gameservers listed without
+		/// any filtering.
+		/// </summary>
+		/// <returns>All known gameservers from the service.</returns>
+		[Header("Cache-Control", "max-age=60")]
+		[Get("/api/gameservers/all")]
+		Task<GameServerListResponseModel> GetServers();
+		
+		//TODO: We can add filtering requests on flags later
+	}
+}

abb24175818ec60283e534fec6cccae0edf9cf8a
HelloKitty <andrew.blakely@ymail.com>
2018-05-08 17:43:02 -0500

    Added GameServer webservice

---
+++ src/Server/Guardians.Service.GameServer/Startup.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Guardians
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc()
+				.RegisterHealthCheckController();
+
+			services.AddLogging();
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+#warning Do not deploy exceptions page into production
+			app.UseDeveloperExceptionPage();
+
+			loggerFactory.RegisterGuardiansLogging(Configuration);
+			loggerFactory.AddDebug();
+
+			app.UseMvc();
+		}
+	}
+}

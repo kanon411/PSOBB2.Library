7422b0fffc0c087f393f4296fef43589b3276b57
HelloKitty <andrew.blakely@ymail.com>
2018-05-09 09:42:40 -0500

    Added AuthorizeJwtAttribute for controllers/actions

---
+++ src/Server/Guardians.Server.Common/Attribute/AuthorizeJwtAttribute.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Meta-data that marks an action/controller with an <see cref="AuthorizeAttribute"/>
+	/// that indicates JWT should be used.
+	/// </summary>
+	public sealed class AuthorizeJwtAttribute : AuthorizeAttribute
+	{
+		/// <summary>
+		/// Indicates that JWT should be used.
+		/// </summary>
+		public AuthorizeJwtAttribute()
+		{
+			AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme;
+		}
+	}
+}

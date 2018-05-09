19c1c75f7eb1487cdf973054a2b70182e9c5e945
HelloKitty <andrew.blakely@ymail.com>
2018-05-09 12:53:15 -0500

    Added base ASP/MVC Controllers for Guardians

---
+++ src/Server/Guardians.Server.Common/Controllers/AuthorizationReadyController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Guardians
+{
+	public abstract class AuthorizationReadyController : BaseGuardiansController
+	{
+		/// <summary>
+		/// The reader for the claims.
+		/// </summary>
+		protected IClaimsPrincipalReader ClaimsReader { get; }
+
+		/// <inheritdoc />
+		protected AuthorizationReadyController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger)
+			: base(logger)
+		{
+			if(claimsReader == null) throw new ArgumentNullException(nameof(claimsReader));
+
+			ClaimsReader = claimsReader;
+		}
+	}
+}

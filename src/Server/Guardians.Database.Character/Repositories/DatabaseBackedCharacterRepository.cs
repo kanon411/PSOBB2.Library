c05c11f0861ec69c28ecb9526f401622e20eb574
HelloKitty <andrew.blakely@ymail.com>
2018-05-09 12:53:03 -0500

    Added Character Repositories; Added Database.Common

---
+++ src/Server/Guardians.Database.Character/Repositories/DatabaseBackedCharacterRepository.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+
+namespace Guardians
+{
+	public sealed class DatabaseBackedCharacterRepository : ICharacterRepository
+	{
+		/// <summary>
+		/// The database context.
+		/// </summary>
+		private CharacterDatabaseContext Context { get; }
+
+		/// <inheritdoc />
+		public DatabaseBackedCharacterRepository(CharacterDatabaseContext context)
+		{
+			if(context == null) throw new ArgumentNullException(nameof(context));
+
+			Context = context;
+		}
+
+		public Task<bool> Contains(int key)
+		{
+			return Context
+				.Characters
+				.AnyAsync(c => c.CharacterId == key);
+		}
+
+		public Task<bool> Contains(string key)
+		{
+			return Context
+				.Characters
+				.AnyAsync(c => c.CharacterName == key);
+		}
+
+		public Task<CharacterDatabaseModel> RetrieveAsync(int key)
+		{
+			return Context
+				.Characters
+				.FindAsync(key);
+		}
+
+		public Task<CharacterDatabaseModel> RetrieveAsync(string key)
+		{
+			throw new NotImplementedException($"TODO: Implement name based retrieve");
+		}
+
+		public async Task<bool> TryCreateAsync(CharacterDatabaseModel model)
+		{
+			await Context.Characters.AddAsync(model);
+
+			//Returns the amount of rows changed.
+			//We expect more than 1 to indicate it was added successfully
+			return 0 != await Context.SaveChangesAsync();
+		}
+
+		public Task<bool> TryDeleteAsync(int key)
+		{
+			throw new NotImplementedException($"TODO: Implement delete based on key.");
+		}
+
+		public Task<bool> TryDeleteAsync(string key)
+		{
+			throw new NotImplementedException($"TODO: Implement delete based on key.");
+		}
+	}
+}
---
+++ src/Server/Guardians.Database.Character/Repositories/ICharacterRepository.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public interface ICharacterRepository : 
+		IGenericRepositoryCrudable<int, CharacterDatabaseModel>,
+		IGenericRepositoryCrudable<string, CharacterDatabaseModel>
+	{
+		
+	}
+}

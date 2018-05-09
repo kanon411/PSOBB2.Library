0a75da997f1efe05bbb07e098e556c905e86e7d2
HelloKitty <andrew.blakely@ymail.com>
2018-05-09 12:11:56 -0500

    Added missing parts of the character database commit

---
+++ src/Server/Guardians.Database.Character/CharacterDatabaseContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guardians
+{
+	public sealed class CharacterDatabaseContext : DbContext
+	{
+		public DbSet<CharacterDatabaseModel> Characters { get; set; }
+
+		public CharacterDatabaseContext(DbContextOptions options) 
+			: base(options)
+		{
+		}
+
+		public CharacterDatabaseContext()
+		{
+
+		}
+
+		//We do the below for local database creation stuff
+#if DATABASE_MIGRATION
+		/// <inheritdoc />
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			//TODO: Should I have local or also AWS setup here?
+			optionsBuilder.UseMySql("Server=localhost;Database=guardians.characters;Uid=root;Pwd=test;");
+
+			base.OnConfiguring(optionsBuilder);
+		}
+
+		protected override void OnModelCreating(ModelBuilder modelBuilder)
+		{
+			modelBuilder.Entity<CharacterDatabaseModel>()
+				.HasAlternateKey(c => c.CharacterName);
+		}
+#endif
+	}
+}

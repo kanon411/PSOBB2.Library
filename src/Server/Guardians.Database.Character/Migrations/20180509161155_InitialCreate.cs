81b141c912a4e8303e40e1f13da68bb5ff53ef75
HelloKitty <andrew.blakely@ymail.com>
2018-05-09 12:00:31 -0500

    Added Character Database proj; Added Characters table

---
+++ src/Server/Guardians.Database.Character/Migrations/20180509161155_InitialCreate.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Guardians.Database.Character.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "characters",
+                columns: table => new
+                {
+                    CharacterId = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    AccountId = table.Column<int>(nullable: false),
+                    CharacterName = table.Column<string>(nullable: false),
+                    CreationDate = table.Column<DateTime>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.ComputedColumn)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_characters", x => x.CharacterId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "characters");
+        }
+    }
+}
---
+++ src/Server/Guardians.Database.Character/Models/CharacterDatabaseModel.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+namespace Guardians
+{
+	/// <summary>
+	/// The model for the character database table.
+	/// </summary>
+	[Table("characters")]
+	public class CharacterDatabaseModel
+	{
+		/// <summary>
+		/// Primary key for the character.
+		/// Must be unique.
+		/// </summary>
+		[Key]
+		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+		public int CharacterId { get; private set; }
+
+		/// <summary>
+		/// Account ID associated with the character.
+		/// </summary>
+		[Required]
+		public int AccountId { get; private set; }
+
+		/// <summary>
+		/// The name of the associated character.
+		/// </summary>
+		[Required]
+		public string CharacterName { get; private set; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		[Required]
+		[DatabaseGenerated(DatabaseGeneratedOption.Computed)]
+		public DateTime CreationDate { get; private set; }
+
+		public CharacterDatabaseModel()
+		{
+			
+		}
+	}
+}

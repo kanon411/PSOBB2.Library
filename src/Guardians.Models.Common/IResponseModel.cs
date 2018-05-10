598ea7e00c94fb7438a4cbcdb9a341015575e354
HelloKitty <andrew.blakely@ymail.com>
2018-05-09 20:09:58 -0500

    Added common Models interfaces from HaloLive

---
+++ src/Guardians.Models.Common/IResponseModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	//From: https://github.com/HaloLive/HaloLive.Library/tree/master/src/HaloLive.Models.Common
+	//The concept here is that response models should be related to a request.
+	//The request should have an enumerable state of results that the response can be in.
+	/// <summary>
+	/// Contract for models that are responses to a request.
+	/// </summary>
+	public interface IResponseModel<out TResultType>
+		where TResultType : struct, IComparable //TODO: Contraint to C# 7.3 enum
+	{
+		/// <summary>
+		/// Indicates the result of the response.
+		/// </summary>
+		TResultType ResultCode { get; }
+	}
+}

5c4ea0dadabb994e9baceb0398cc9004069148e8
HelloKitty <andrew.blakely@ymail.com>
2018-05-11 18:58:22 -0500

    Added CharacterListResponse model

---
+++ src/Guardians.Models.GameServer/Models/CharacterListResponse.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Response model for character list requests.
+	/// </summary>
+	[JsonObject]
+	public class CharacterListResponse : IResponseModel<CharacterListResponseCode>, ISucceedable
+	{
+		/// <summary>
+		/// Optionally backing field for 
+		/// </summary>
+		[JsonProperty(Required = Required.AllowNull)]
+		private int[] _CharacterIds { get; set; }
+
+		/// <summary>
+		/// The IDs of the characters.
+		/// </summary>
+		public IReadOnlyCollection<int> CharacterIds => _CharacterIds;
+
+		/// <inheritdoc />
+		[JsonIgnore]
+		public bool isSuccessful => ResultCode == CharacterListResponseCode.Success;
+
+		//Serializer ctor
+		/// <inheritdoc />
+		[JsonRequired]
+		[JsonProperty]
+		public CharacterListResponseCode ResultCode { get; }
+
+		/// <summary>
+		/// Creates a successful response.
+		/// <see cref="ResultCode"/> will be set to Success.
+		/// </summary>
+		/// <param name="characterIds">The character id.</param>
+		public CharacterListResponse(int[] characterIds)
+		{
+			if(characterIds == null) throw new ArgumentNullException(nameof(characterIds));
+			if(characterIds.Length == 0) throw new ArgumentException("Value cannot be an empty collection.", nameof(characterIds));
+
+			_CharacterIds = characterIds;
+			ResultCode = CharacterListResponseCode.Success;
+		}
+
+		/// <inheritdoc />
+		public CharacterListResponse(CharacterListResponseCode resultCode)
+		{
+			if(!Enum.IsDefined(typeof(CharacterListResponseCode), resultCode)) throw new InvalidEnumArgumentException(nameof(resultCode), (int)resultCode, typeof(CharacterListResponseCode));
+			if(resultCode == CharacterListResponseCode.Success)
+				throw new ArgumentException($"Cannot provided {nameof(CharacterListResponseCode)}.{CharacterListResponseCode.Success.ToString()} to failing ctor.");
+
+			//This has to be failing. Otherwise the collection be empty/null with success
+			ResultCode = resultCode;
+		}
+
+		protected CharacterListResponse()
+		{
+			
+		}
+	}
+}
---
+++ src/Guardians.Models.GameServer/ResultCodes/CharacterListResponseCode.cs
@@ -0,0 +1,24 @@
+﻿namespace Guardians
+{
+	/// <summary>
+	/// Enumeration of all potential character list response results.
+	/// </summary>
+	public enum CharacterListResponseCode
+	{
+		/// <summary>
+		/// Indicates that the character list response
+		/// was successful.
+		/// </summary>
+		Success = 0,
+
+		/// <summary>
+		/// Indicates that no characters were found.
+		/// </summary>
+		NoCharactersFoundError = 1,
+
+		/// <summary>
+		/// Indicates an unknown/general error occurred.
+		/// </summary>
+		GeneralServerError = 2
+	}
+}

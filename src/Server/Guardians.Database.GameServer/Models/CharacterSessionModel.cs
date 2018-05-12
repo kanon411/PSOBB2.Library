994cd8e6d2ee79fa46e6b43c56141aa74e74893b
HelloKitty <andrew.blakely@ymail.com>
2018-05-11 22:10:50 -0500

    Added IsSessionActive property to character_session
    No session means character isn't in the world. A session existing means
    it is/was in the world. Sessions can be reclaimed, meaing you can rejoin
    transient instances properly. Sessions can be active or inactive, active
    means it is claimed and no other use can log in.
    
    There is a race condition here. Though it is a known and handlable one.
    Meaning multiple ongoing session claims could be occuring. But only one
    will ever be able to be claimed.

--- src/Server/Guardians.Database.GameServer/Models/CharacterSessionModel.cs
+++ src/Server/Guardians.Database.GameServer/Models/CharacterSessionModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
@@ -31,6 +32,19 @@ namespace Guardians
 
 		//Navigation property
 		public virtual ZoneInstanceEntryModel ZoneEntry { get; private set; }
+
+		//Initially this will be false. Meaning there will be a short window of time
+		//where users may request to claim a session multiple times. Meaning there could be
+		//for example 5 clients may be claiming this session, be redirected to where it is it should be claimed
+		//but the design should be that only one session is ever accepted. Only one can cause True to be set.
+		//All others must be rejected.
+		/// <summary>
+		/// Indicates if a session is active. It means the character is not claimed.
+		/// Meaning it should not exist in any zone instance.
+		/// </summary>
+		[Required]
+		[DefaultValue(false)]
+		public bool IsSessionActive { get; private set; }
 
 		//Fluent API sets up default time
 		[Required]

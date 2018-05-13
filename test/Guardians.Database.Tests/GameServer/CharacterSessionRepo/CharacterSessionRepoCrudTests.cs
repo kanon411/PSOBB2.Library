33043c66e9b58f10feb8caf1ba9beaf3fd6bc785
HelloKitty <andrew.blakely@ymail.com>
2018-05-13 07:05:10 -0500

    Added Generic Tests for CharacterSessionRepo

---
+++ test/Guardians.Database.Tests/GameServer/CharacterSessionRepo/CharacterSessionRepoCrudTests.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Runs all the crud repo interface default tests against the character repository.
+	/// </summary>
+	[TestFixture]
+	public class CharacterSessionRepoCrudTests : GenericCrubRepositoryDefaultTests<CharacterDatabaseContext, DatabaseBackedCharacterSessionRepository, int, CharacterSessionModel>
+	{
+		private static int CharacterIdIncrementable = 1;
+
+		/// <inheritdoc />
+		public override CharacterSessionModel BuildRandomModel()
+		{
+			int id = Interlocked.Increment(ref CharacterIdIncrementable);
+			return new CharacterSessionModel(id, 0);
+		}
+
+		/// <inheritdoc />
+		public override int ProduceKeyFromModel(CharacterSessionModel model)
+		{
+			return model.CharacterId;
+		}
+	}
+}

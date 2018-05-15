0b1bba302ec90242883a8243ca42cae8ccd66004
HelloKitty <andrew.blakely@ymail.com>
2018-05-15 05:00:39 -0500

    Switched model projects to Netstandard2.0

---
+++ src/Guardians.Models.Common/AbstractMetadataMarker.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Guardians
+{
+	public abstract class AbstractMetadataMarker<TTypeToScanAssemblyFrom>
+		where TTypeToScanAssemblyFrom : class
+	{
+		/// <summary>
+		/// The models defined in the assembly.
+		/// </summary>
+		public static IEnumerable<Type> ModelTypes { get; }
+			= typeof(TTypeToScanAssemblyFrom)
+				.GetTypeInfo()
+				.Assembly
+				.DefinedTypes
+				.Where(t => t.GetCustomAttribute<JsonObjectAttribute>() != null)
+				.Select(t => t.AsType());
+	}
+}

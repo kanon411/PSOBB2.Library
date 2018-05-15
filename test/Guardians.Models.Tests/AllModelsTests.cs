08f4a38e56dea6ef604df18c1119d9360a590555
HelloKitty <andrew.blakely@ymail.com>
2018-05-15 06:23:47 -0500

    Add AllModels Collections Tests
    We want only ReadOnly collections exposed publicly.

--- test/Guardians.Models.Tests/AllModelsTests.cs
+++ test/Guardians.Models.Tests/AllModelsTests.cs
@@ -1,8 +1,10 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using System.Text;
+using JetBrains.Annotations;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Serialization;
 using NUnit.Compatibility;
@@ -34,7 +36,84 @@ namespace Guardians
 		public void Test_Model_Properties_All_Have_Explict_Json_Properties(MemberInfo m)
 		{
 			//assert
-			Assert.True(m.GetCustomAttribute<JsonPropertyAttribute>() != null || m.GetCustomAttribute<JsonIgnoreAttribute>() != null, $"Property: {m.Name} in Type: {m.DeclaringType.Name} does not contain explict {nameof(JsonProperty)} or {nameof(JsonIgnoreAttribute)}");
+			Assert.True(m.GetCustomAttribute<JsonPropertyAttribute>() != null || m.GetCustomAttribute<JsonIgnoreAttribute>() != null, $"{GenerateMemberInfoIdentiferPrefix(m)} does not contain explict {nameof(JsonProperty)} or {nameof(JsonIgnoreAttribute)}");
+		}
+
+		private static string GenerateMemberInfoIdentiferPrefix(MemberInfo m)
+		{
+			return $"Property: {m.Name} in Type: {m.DeclaringType.Name}";
+		}
+
+		[Test]
+		[TestCaseSource(nameof(SerializableMembers))]
+		public void Test_Model_No_Array_Directly_Exposed(MemberInfo m)
+		{
+			//assert
+			Assert.False(m.GetUnderlyingType().IsArray && m.IsPublic(), $"{GenerateMemberInfoIdentiferPrefix(m)} is an Array type but was public. Do not expose arrays publicly. Use them as backing fields to collections.");
+		}
+
+		[Test]
+		[TestCaseSource(nameof(SerializableMembers))]
+		public void Test_Model_Any_Public_Collections_Are_Readonly(MemberInfo m)
+		{
+			//assert
+			Assert.False(!m.GetUnderlyingType().IsArray && m.IsPublic() && m.GetUnderlyingType() != typeof(string) && (TypeIsReadonlyCollection(m)), $"{GenerateMemberInfoIdentiferPrefix(m)} is a collection type that is not readonly. It uses Type: {m.GetUnderlyingType()}. Use IReadonlyCollection<T>. Do not use IEnumerable<T>.");
+		}
+
+		private static bool TypeIsReadonlyCollection(MemberInfo m)
+		{
+			IEnumerable<Type> interfaces = m.GetUnderlyingType()
+				.GetInterfaces();
+
+			if(m.GetUnderlyingType().IsInterface)
+				interfaces = interfaces.Concat(new Type[1] {m.GetUnderlyingType()});
+
+			return interfaces
+				.Any(i => (i == typeof(ICollection) || i == typeof(IList))
+					|| i.IsGenericType 
+					&& (i.GetGenericTypeDefinition() == typeof(ICollection<>)
+					|| i.GetGenericTypeDefinition() == typeof(IList<>)));
+		}
+	}
+
+	public static class AllModelsTestsHelper
+	{
+		public static Type GetUnderlyingType(this MemberInfo member)
+		{
+			switch(member.MemberType)
+			{
+				case MemberTypes.Event:
+					return ((EventInfo)member).EventHandlerType;
+				case MemberTypes.Field:
+					return ((FieldInfo)member).FieldType;
+				case MemberTypes.Method:
+					return ((MethodInfo)member).ReturnType;
+				case MemberTypes.Property:
+					return ((PropertyInfo)member).PropertyType;
+				default:
+					throw new ArgumentException
+					(
+						"Input MemberInfo must be if type EventInfo, FieldInfo, MethodInfo, or PropertyInfo"
+					);
+			}
+		}
+
+		public static bool IsPublic([NotNull] this MemberInfo member)
+		{
+			if(member == null) throw new ArgumentNullException(nameof(member));
+
+			switch(member.MemberType)
+			{
+				case MemberTypes.Field:
+					return ((FieldInfo)member).IsPublic;
+				case MemberTypes.Method:
+					return ((MethodInfo)member).IsPublic;
+				//TODO: Is that the best way to do this?
+				case MemberTypes.Property:
+					return ((PropertyInfo)member).GetMethod.IsPublic;
+				default:
+					throw new InvalidOperationException($"Cannot find accessibility information for MemberType: {member.MemberType}.");
+			}
 		}
 	}
 }

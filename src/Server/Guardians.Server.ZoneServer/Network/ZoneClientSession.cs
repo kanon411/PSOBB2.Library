e603925777c3925e3ba11bdb6a8ccc61127d8da8
HelloKitty <andrew.blakely@ymail.com>
2018-06-10 21:19:09 -0500

    Added ZoneClientSession to zone server lib

---
+++ src/Server/Guardians.Server.ZoneServer/Network/ZoneClientSession.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using GladNet;
+
+namespace Guardians
+{
+	/// <summary>
+	/// The zone client session.
+	/// </summary>
+	public sealed class ZoneClientSession : ManagedClientSession<GameServerPacketPayload, GameClientPacketPayload>
+	{
+		/// <summary>
+		/// The message handlers service.
+		/// </summary>
+		private MessageHandlerService<GameClientPacketPayload, GameServerPacketPayload> MessageHandlers { get; }
+
+		/// <inheritdoc />
+		public ZoneClientSession(IManagedNetworkServerClient<GameServerPacketPayload, GameClientPacketPayload> internalManagedNetworkClient, 
+			SessionDetails details, MessageHandlerService<GameClientPacketPayload, 
+			GameServerPacketPayload> messageHandlers) 
+			: base(internalManagedNetworkClient, details)
+		{
+			MessageHandlers = messageHandlers ?? throw new ArgumentNullException(nameof(messageHandlers));
+		}
+
+		/// <inheritdoc />
+		protected override void OnSessionDisconnected()
+		{
+			//TODO: Do some cleanup or something someday
+		}
+
+		/// <inheritdoc />
+		public override Task OnNetworkMessageRecieved(NetworkIncomingMessage<GameClientPacketPayload> message)
+		{
+			return MessageHandlers.TryHandleMessage(new DefaultPeerMessageContext<GameServerPacketPayload>(this.Connection, this.SendService, null), message);
+		}
+	}
+}

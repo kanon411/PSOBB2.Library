869aaaaa75da6cb4c43f74241e33b1651bb818ae
HelloKitty <andrew.blakely@ymail.com>
2018-06-25 20:07:34 -0500

    Changed Client.UI to Client.Common

--- src/Client/Guardians.Client.Authentication/UI/Controllers/LoginFormController.cs
+++ src/Client/Guardians.Client.Authentication/UI/Controllers/LoginFormController.cs
@@ -4,7 +4,6 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using FluentValidation;
-using Microsoft.Extensions.Logging;
 using Newtonsoft.Json.Bson;
 using SceneJect.Common;
 using UnityEngine;
--- src/Client/Guardians.Client.Authentication/UI/Views/LoginErroFormView.cs
+++ src/Client/Guardians.Client.Authentication/UI/Views/LoginErroFormView.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using UnityEngine;
 using UnityEngine.Events;
+using UnityEngine.Serialization;
 
 namespace Guardians
 {

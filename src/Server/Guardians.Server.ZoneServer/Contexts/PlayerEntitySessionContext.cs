580d11789ca725eb051905deb25d826494ecb94d
HelloKitty <andrew.blakely@ymail.com>
2018-10-21 06:32:55 -0500

    Added missing additions from last commit

--- src/Server/Guardians.Server.ZoneServer/Contexts/PlayerEntitySessionContext.cs
+++ src/Server/Guardians.Server.ZoneServer/Contexts/PlayerEntitySessionContext.cs
@@ -10,10 +10,19 @@ namespace Guardians
 	{
 		public IPeerPayloadSendService<GameServerPacketPayload> ZoneSession { get; }
 
+		/// <summary>
+		/// The connection ID of the session.
+		/// </summary>
+		public int ConnectionId { get; }
+
 		/// <inheritdoc />
-		public PlayerEntitySessionContext([NotNull] IPeerPayloadSendService<GameServerPacketPayload> zoneSession)
+		public PlayerEntitySessionContext([NotNull] IPeerPayloadSendService<GameServerPacketPayload> zoneSession, int connectionId)
 		{
+			//TODO: Maybe this should be a uint?
+			if(connectionId <= 0) throw new ArgumentOutOfRangeException(nameof(connectionId));
+
 			ZoneSession = zoneSession ?? throw new ArgumentNullException(nameof(zoneSession));
+			ConnectionId = connectionId;
 		}
 	}
 }

1d0f3fdcd514353df591b62a96163c1e435a9f56
HelloKitty <andrew.blakely@ymail.com>
2018-10-21 05:10:07 -0500

    Added client movement data packet
    Not final, but this will let clients set their position for the time
    being.

---
+++ src/Guardians.Payloads.ZoneServer/Payloads/ClientMovementDataUpdateRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using ProtoBuf;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Payload sent by the client to update the server about local client
+	/// movement data.
+	/// </summary>
+	[ProtoContract]
+	[GamePayload(GamePayloadOperationCode.MovementDataUpdate)]
+	public sealed class ClientMovementDataUpdateRequest : GameClientPacketPayload
+	{
+		/// <summary>
+		/// The movement data to update the server with.
+		/// </summary>
+		[ProtoMember(1)]
+		public MovementInformation MovementData { get; }
+
+		/// <inheritdoc />
+		public ClientMovementDataUpdateRequest([NotNull] MovementInformation movementData)
+		{
+			MovementData = movementData ?? throw new ArgumentNullException(nameof(movementData));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		protected ClientMovementDataUpdateRequest()
+		{
+			
+		}
+	}
+}

5c53efc9cf15e3552a7dfff254a62842fdaf27f4
HelloKitty <andrew.blakely@ymail.com>
2018-10-24 01:24:24 -0500

    Added some zoneclient dependencies

---
+++ src/Client/Guardians.Client.Game/Handlers/ZoneClientDefaultRequestHandler.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace Guardians
+{
+	/// <summary>
+	/// This is the default handler that is invoked when an unknown payload is encountered.
+	/// Or a payload is encountered that doesn't have a registered handler.
+	/// </summary>
+	public sealed class ZoneClientDefaultRequestHandler : IPeerPayloadSpecificMessageHandler<GameClientPacketPayload, GameServerPacketPayload>
+	{
+		private ILog Logger { get; }
+
+		/// <inheritdoc />
+		public ZoneClientDefaultRequestHandler(ILog logger)
+		{
+			if(logger == null) throw new ArgumentNullException(nameof(logger));
+
+			Logger = logger;
+		}
+
+		/// <inheritdoc />
+		public Task HandleMessage(IPeerMessageContext<GameServerPacketPayload> context, GameClientPacketPayload payload)
+		{
+			if(Logger.IsWarnEnabled)
+				Logger.Warn($"Recieved unhandable Payload: {payload.GetType().Name}");
+
+			return Task.CompletedTask;
+		}
+	}
+}
---
+++ src/Client/Guardians.Client.Game/IoC/GameClientMessageHandlerRegisterModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using GladNet;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Guardians
+{
+	public sealed class GameClientMessageHandlerRegisterModule : NonBehaviourDependency
+	{
+		/// <inheritdoc />
+		public override void Register(ContainerBuilder register)
+		{
+			//New IPeerContext generic param now so we register as implemented interface
+			register.RegisterType<ZoneClientDefaultRequestHandler>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+
+			register.RegisterType<MessageHandlerService<GameServerPacketPayload, GameClientPacketPayload>>()
+				.As<MessageHandlerService<GameServerPacketPayload, GameClientPacketPayload>>();
+		}
+	}
+}

b4474a9dff2d1e4008e0f4a26a2b23f11548c956
HelloKitty <andrew.blakely@ymail.com>
2018-10-26 21:53:44 -0500

    Added some general zone client dependencies

---
+++ src/Client/Guardians.Client.Game/IoC/ZoneServerGameDependencies.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Guardians
+{
+	public sealed class ZoneServerGameDependencies : NonBehaviourDependency
+	{
+		/// <inheritdoc />
+		public override void Register(ContainerBuilder builder)
+		{
+			builder.RegisterType<EntityGuidDictionary<MovementInformation>>()
+				.AsSelf()
+				.As<IReadonlyEntityGuidMappable<MovementInformation>>()
+				.As<IEntityGuidMappable<MovementInformation>>()
+				.SingleInstance();
+
+
+			builder.RegisterType<EntityGuidDictionary<GameObject>>()
+				.As<IEntityGuidMappable<GameObject>>()
+				.As<IReadonlyEntityGuidMappable<GameObject>>()
+				.AsSelf()
+				.SingleInstance();
+
+			builder.RegisterType<DefaultGameObjectToEntityMappable>()
+				.As<IReadonlyGameObjectToEntityMappable>()
+				.As<IGameObjectToEntityMappable>()
+				.SingleInstance();
+
+			builder.RegisterType<LocalPlayerFactory>()
+				.As<IFactoryCreatable<GameObject, LocalPlayerCreationContext>>()
+				.AsSelf()
+				.SingleInstance();
+		}
+	}
+}

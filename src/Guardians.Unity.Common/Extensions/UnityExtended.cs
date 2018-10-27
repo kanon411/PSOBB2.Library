e69034e684f14cded447ccbc1e6ac20bef391da3
HelloKitty <andrew.blakely@ymail.com>
2018-10-27 06:19:14 -0500

    Added support for continuing on main unity thread with UnityYieldAwaitable.

---
+++ src/Guardians.Unity.Common/Extensions/UnityExtended.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+
+namespace Guardians
+{
+	public static class UnityExtended
+	{
+		public static SynchronizationContext UnityMainThreadContext { get; private set; }
+
+		/// <summary>
+		/// Sets the <see cref="UnityMainThreadContext"/> with the current
+		/// thread's (caller thread) sync context.
+		/// </summary>
+		public static void InitializeSyncContext()
+		{
+			UnityMainThreadContext = SynchronizationContext.Current;
+		}
+	}
+
+
+}

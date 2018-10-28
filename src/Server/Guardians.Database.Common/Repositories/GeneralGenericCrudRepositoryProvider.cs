09528abcb470cbf6e337a4ce66bbf4906e392635
HelloKitty <andrew.blakely@ymail.com>
2018-10-28 07:19:39 -0500

    Added NpcEntry Crud Repository

---
+++ src/Server/Guardians.Database.Common/Repositories/GeneralGenericCrudRepositoryProvider.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Guardians
+{
+	public sealed class GeneralGenericCrudRepositoryProvider<TKey, TModelType> : IGenericRepositoryCrudable<TKey, TModelType> 
+		where TModelType : class
+	{
+		private DbSet<TModelType> ModelSet { get; }
+
+		private DbContext Context { get; }
+
+		/// <inheritdoc />
+		public GeneralGenericCrudRepositoryProvider(DbSet<TModelType> modelSet, DbContext context)
+		{
+			ModelSet = modelSet ?? throw new ArgumentNullException(nameof(modelSet));
+			Context = context ?? throw new ArgumentNullException(nameof(context));
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> ContainsAsync(TKey key)
+		{
+			return await RetrieveAsync(key).ConfigureAwait(false) != null;
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> TryCreateAsync(TModelType model)
+		{
+			//TODO: Should we validate no key already exists?
+			ModelSet.Add(model);
+			return await SaveAndCheckResultsAsync()
+				.ConfigureAwait(false);
+		}
+
+		private async Task<bool> SaveAndCheckResultsAsync()
+		{
+			return await Context.SaveChangesAsync().ConfigureAwait(false) != 0;
+		}
+
+		/// <inheritdoc />
+		public Task<TModelType> RetrieveAsync(TKey key)
+		{
+			return ModelSet.FindAsync(key);
+		}
+
+		/// <inheritdoc />
+		public async Task<bool> TryDeleteAsync(TKey key)
+		{
+			//If it doesn't exist then this will just fail, so get out soon.
+			if(!await ContainsAsync(key).ConfigureAwait(false))
+				return false;
+
+			TModelType modelType = await RetrieveAsync(key)
+				.ConfigureAwait(false);
+
+			ModelSet.Remove(modelType);
+
+			return await SaveAndCheckResultsAsync()
+				.ConfigureAwait(false);
+		}
+	}
+}
---
+++ src/Server/Guardians.Database.GameServer/Repositories/NPC/DatabaseBackedNpcEntryRepository.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guardians
+{
+	public sealed class DatabaseBackedNpcEntryRepository : IGenericRepositoryCrudable<int, NPCEntryModel>
+	{
+		private NpcDatabaseContext NpcDatabase { get; }
+
+		/// <inheritdoc />
+		public DatabaseBackedNpcEntryRepository(NpcDatabaseContext npcDatabase)
+		{
+			NpcDatabase = npcDatabase ?? throw new ArgumentNullException(nameof(npcDatabase));
+		}
+
+		/// <inheritdoc />
+		public Task<bool> ContainsAsync(int key)
+		{
+			var generalCrudProvider = BuildGeneralCrudProvider();
+
+			return generalCrudProvider.ContainsAsync(key);
+		}
+
+		private GeneralGenericCrudRepositoryProvider<int, NPCEntryModel> BuildGeneralCrudProvider()
+		{
+			return new GeneralGenericCrudRepositoryProvider<int, NPCEntryModel>(NpcDatabase.Entries, NpcDatabase);
+		}
+
+		/// <inheritdoc />
+		public Task<bool> TryCreateAsync(NPCEntryModel model)
+		{
+			var generalCrudProvider = BuildGeneralCrudProvider();
+
+			return generalCrudProvider.TryCreateAsync(model);
+		}
+
+		/// <inheritdoc />
+		public Task<NPCEntryModel> RetrieveAsync(int key)
+		{
+			var generalCrudProvider = BuildGeneralCrudProvider();
+
+			return generalCrudProvider.RetrieveAsync(key);
+		}
+
+		/// <inheritdoc />
+		public Task<bool> TryDeleteAsync(int key)
+		{
+			var generalCrudProvider = BuildGeneralCrudProvider();
+
+			return generalCrudProvider.TryDeleteAsync(key);
+		}
+	}
+}

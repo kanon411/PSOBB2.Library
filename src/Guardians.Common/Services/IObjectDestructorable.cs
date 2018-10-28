81c9ec48c5c9826ec32016a6438264ff0b4bff5b
HelloKitty <andrew.blakely@ymail.com>
2018-10-27 21:00:58 -0500

    Added IObjectDestructorable
    Contract for types that offer object destruction.

---
+++ src/Guardians.Common/Services/IObjectDestructorable.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Contract for types that provide destruction logic for specific
+	/// types of <typeparamref name="TObjectType"/>.
+	/// </summary>
+	/// <typeparam name="TObjectType">The type that can be destructed.</typeparam>
+	public interface IObjectDestructorable<in TObjectType>
+	{
+		/// <summary>
+		/// Attempts to destroy the provided <see cref="obj"/>.
+		/// </summary>
+		/// <param name="obj">The object to destroy.</param>
+		/// <returns>True if the object could be successfully deconstructed.</returns>
+		bool Destroy(TObjectType obj);
+	}
+}

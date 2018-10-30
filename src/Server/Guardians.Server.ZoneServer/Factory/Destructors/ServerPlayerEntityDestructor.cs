104a342bdc9a1777f12fb3598446eaba033392d4
HelloKitty <andrew.blakely@ymail.com>
2018-10-30 10:21:52 -0500

    Added hack for interest leaving on player disconnect
    This is NOT a final solution, but it is what will work for now. There
    are afew design flaws, and this is one of them. Will address at a later
    point along with the others. This one took a long time to even come up
    with a hack for. So, I don't expect to solve this soon.

--- src/Server/Guardians.Server.ZoneServer/Factory/Destructors/ServerPlayerEntityDestructor.cs
+++ src/Server/Guardians.Server.ZoneServer/Factory/Destructors/ServerPlayerEntityDestructor.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using GladNet;
 using JetBrains.Annotations;
@@ -15,17 +16,32 @@ namespace Guardians
 
 		private IEntityGuidMappable<InterestCollection> GuidToInterestCollectionMappable { get; }
 
+		private IReadonlyEntityGuidMappable<GameObject> GuidToGameObjectMap { get; }
+
 		/// <inheritdoc />
-		public ServerPlayerEntityDestructor([NotNull] IObjectDestructorable<NetworkEntityGuid> entityDestructor, [NotNull] IEntityGuidMappable<IPeerPayloadSendService<GameServerPacketPayload>> guidToSessionMappable, [NotNull] IEntityGuidMappable<InterestCollection> guidToInterestCollectionMappable)
+		public ServerPlayerEntityDestructor([NotNull] IObjectDestructorable<NetworkEntityGuid> entityDestructor, [NotNull] IEntityGuidMappable<IPeerPayloadSendService<GameServerPacketPayload>> guidToSessionMappable, [NotNull] IEntityGuidMappable<InterestCollection> guidToInterestCollectionMappable, IReadonlyEntityGuidMappable<GameObject> guidToGameObjectMap)
 		{
 			EntityDestructor = entityDestructor ?? throw new ArgumentNullException(nameof(entityDestructor));
 			GuidToSessionMappable = guidToSessionMappable ?? throw new ArgumentNullException(nameof(guidToSessionMappable));
 			GuidToInterestCollectionMappable = guidToInterestCollectionMappable ?? throw new ArgumentNullException(nameof(guidToInterestCollectionMappable));
+			GuidToGameObjectMap = guidToGameObjectMap;
 		}
 
 		/// <inheritdoc />
 		public bool Destroy(PlayerEntityDestructionContext obj)
 		{
+			//TODO: A hack, that removes this entity from other nearby interest lists
+			//TODO: This really isn't a good solution, we need something that scales better and that isn't faulty. This could have some odd race conditions to cause ghost entities.
+			SphereCollider ourCollider = GuidToGameObjectMap[obj.EntityGuid].GetComponentInChildren<SphereCollider>();
+			//This is slow, and hacky. We need a better solution for entities getting deconstructed.
+			foreach(var exit in Physics.OverlapSphere(ourCollider.transform.position, ourCollider.radius)
+				.Select(c => c.gameObject.transform.root.GetComponentInChildren<InterestRadiusGatewayExit>())
+				.Where(e => e != null))
+			{
+				//This simulates us leaving their interest radius.
+				exit.OnTriggerExit(ourCollider);
+			}
+
 			//We have some player specific stuff, especially for the server, that we need to handle
 			//removing that regular Entities won't have have the client or even the server.
 			GuidToSessionMappable.Remove(obj.EntityGuid);

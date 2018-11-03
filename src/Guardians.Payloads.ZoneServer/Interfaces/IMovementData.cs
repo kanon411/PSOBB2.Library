add6e32febb91040be2a3136447327af101acdcf
HelloKitty <andrew.blakely@ymail.com>
2018-11-03 09:16:07 -0500

    Add IMovementData (Start of Movement Refactoring)

---
+++ src/Guardians.Payloads.ZoneServer/Interfaces/IMovementData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+using UnityEngine;
+
+namespace Guardians
+{
+	[ProtoInclude(1, typeof(PositionChangeMovementData))]
+	public interface IMovementData
+	{
+		/// <summary>
+		/// The timestamp that a particular movement data
+		/// was created at on the server.
+		/// </summary>
+		long TimeStamp { get; }
+
+		//TODO: Doc better
+		/// <summary>
+		/// The initial position
+		/// that the moving entity started with
+		/// before this movement data was created.
+		/// </summary>
+		Vector3 InitialPosition { get; }
+	}
+}
---
+++ src/Guardians.Payloads.ZoneServer/Models/Movement/PositionChangeMovementData.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using ProtoBuf;
+using UnityEngine;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Movement data that can be used to denote
+	/// just a change in world position.
+	/// </summary>
+	[ProtoContract]
+	public sealed class PositionChangeMovementData : IMovementData
+	{
+		//TODO: Add support for instant vs lerped
+
+		/// <inheritdoc />
+		[ProtoMember(1)]
+		public long TimeStamp { get; }
+
+		/// <inheritdoc />
+		[ProtoMember(2)]
+		public Vector3 InitialPosition { get; }
+
+		/// <inheritdoc />
+		public PositionChangeMovementData(long timeStamp, Vector3 initialPosition)
+		{
+			if(timeStamp <= 0) throw new ArgumentOutOfRangeException(nameof(timeStamp));
+			TimeStamp = timeStamp;
+			InitialPosition = initialPosition;
+		}
+
+		protected PositionChangeMovementData()
+		{
+			
+		}
+	}
+}

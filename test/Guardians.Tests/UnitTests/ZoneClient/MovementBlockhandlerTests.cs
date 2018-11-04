a8c380e6773345811bcff95dd9b7cf557f9d60cc
HelloKitty <andrew.blakely@ymail.com>
2018-11-04 13:08:02 -0600

    Add test to check MovementHandler registeration

---
+++ test/Guardians.Tests/UnitTests/ZoneClient/MovementBlockhandlerTests.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using NUnit.Framework;
+using SceneJect;
+using SceneJect.Common;
+
+namespace Guardians
+{
+	[TestFixture]
+	public sealed class MovementBlockhandlerTests
+	{
+		[Test]
+		public void Test_Can_Resolve_RegisteredMovementBlockHandlers()
+		{
+			//TODO: If we change the ctor signature for the handler service we need to change this test
+			//arrange
+			//TODO: This won't work if we have multiple configurable modules.
+			//arrange
+			ContainerBuilder builder = new ContainerBuilder();
+			builder.RegisterAssemblyModules(typeof(ZoneClientHandlerRegisterationAutofacModule).Assembly);
+
+			//Manually register SceneJect services
+			builder.Register(context => new DefaultGameObjectFactory(context.Resolve<IComponentContext>(), new DefaultInjectionStrategy()))
+				.As<IGameObjectFactory>()
+				.SingleInstance();
+
+			builder.Register(context => new DefaultGameObjectComponentAttachmentFactory(context.Resolve<IComponentContext>(), new DefaultInjectionStrategy()))
+				.As<IGameObjectComponentAttachmentFactory>()
+				.SingleInstance();
+
+			builder.Register(context => new DefaultManualInjectionStrategy(context.Resolve<IComponentContext>()))
+				.As<IManualInjectionStrategy>()
+				.SingleInstance();
+
+			IContainer resolver = builder.Build();
+
+			//act
+			IReadOnlyCollection<IMovementBlockHandler> handlers = resolver.Resolve<IReadOnlyCollection<IMovementBlockHandler>>();
+
+			//assert
+			Assert.NotNull(handlers);
+			Console.WriteLine($"Found Handler Count: {handlers.Count}");
+			Assert.IsNotEmpty(handlers);
+		}
+	}
+}

31ab76b3b27cf19d1eba28104b0da6c33bca8d10
HelloKitty <andrew.blakely@ymail.com>
2018-11-05 07:59:21 -0600

    Created skeleton for movement generation (client/server)

---
+++ src/Guardians.ZoneServer.Common/Services/BaseMovementGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+using UnityEngine;
+
+namespace Guardians
+{
+	public abstract class BaseMovementGenerator<TDataInputType> : IMovementGenerator<GameObject>
+		where TDataInputType : class, IMovementData
+	{
+		/// <summary>
+		/// The movement data used by this generator.
+		/// </summary>
+		protected TDataInputType MovementData { get; }
+
+		protected bool hasStartFired { get; private set; }
+
+		/// <inheritdoc />
+		protected BaseMovementGenerator([NotNull] TDataInputType movementData)
+		{
+			MovementData = movementData ?? throw new ArgumentNullException(nameof(movementData));
+		}
+
+		protected abstract void Start(GameObject entity, long currentTime);
+
+		/// <inheritdoc />
+		public void Update(GameObject entity, long currentTime)
+		{
+			if(!hasStartFired)
+			{
+				Start(entity, currentTime);
+				hasStartFired = true;
+			}
+			else 
+				InternalUpdate(entity, currentTime); //don't update if we called Start
+		}
+
+		/// <summary>
+		/// Called on <see cref="Update"/>
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="currentTime"></param>
+		protected abstract void InternalUpdate(GameObject entity, long currentTime);
+	}
+}

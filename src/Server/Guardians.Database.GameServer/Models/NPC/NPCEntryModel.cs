6258e459ed6024b4ee69d471bb522c7367273e7c
HelloKitty <andrew.blakely@ymail.com>
2018-11-09 13:08:23 -0600

    Added Movement Support to NpcEntry in Database

--- src/Server/Guardians.Database.GameServer/Models/NPC/NPCEntryModel.cs
+++ src/Server/Guardians.Database.GameServer/Models/NPC/NPCEntryModel.cs
@@ -18,6 +18,7 @@ namespace Guardians
 		/// Primary key for the NPC template.
 		/// Must be unique.
 		/// </summary>
+		[Column(Order = 1)]
 		[Key]
 		[DatabaseGenerated(DatabaseGeneratedOption.Identity)]
 		public int EntryId { get; private set; }
@@ -26,6 +27,7 @@ namespace Guardians
 		/// Defines the template data from which this entry/instance of a creature
 		/// should be created from.
 		/// </summary>
+		[Column(Order = 2)]
 		[Required]
 		[ForeignKey(nameof(NpcTemplate))]
 		public int NpcTemplateId { get; private set; }
@@ -39,6 +41,7 @@ namespace Guardians
 		/// <summary>
 		/// The spawn position of the creature/NPC.
 		/// </summary>
+		[Column(Order = 3)]
 		[Required]
 		public Vector3<float> SpawnPosition { get; private set; }
 
@@ -46,6 +49,7 @@ namespace Guardians
 		/// The initial Y-axis orientation/rotation of the NPC/Creature when spawned.
 		/// Especially important for stationary NPCs.
 		/// </summary>
+		[Column(Order = 4)]
 		[Required]
 		public float InitialOrientation { get; private set; }
 
@@ -53,11 +57,20 @@ namespace Guardians
 		/// The map/zone that this creature exists in.
 		/// (Each map/zone has an origin and the <see cref="SpawnPosition"/> is based on that).
 		/// </summary>
+		[Column(Order = 5)]
 		[Required]
 		public int MapId { get; private set; }
 
+		[Column(Order = 6)]
+		[Required]
+		public NpcMovementType MovementType { get; private set; }
+
+		[Column(Order = 7)]
+		public int MovementData { get; private set; }
+
 		/// <inheritdoc />
-		public NPCEntryModel(int npcTemplateId, Vector3<float> spawnPosition, float initialOrientation, int mapId)
+		public NPCEntryModel(int npcTemplateId, Vector3<float> spawnPosition, float initialOrientation, int mapId, 
+			NpcMovementType movementType, int movementData)
 		{
 			if(mapId <= 0) throw new ArgumentOutOfRangeException(nameof(mapId));
 			if(npcTemplateId <= 0) throw new ArgumentOutOfRangeException(nameof(npcTemplateId));
@@ -65,6 +78,8 @@ namespace Guardians
 			NpcTemplateId = npcTemplateId;
 			SpawnPosition = spawnPosition ?? throw new ArgumentNullException(nameof(spawnPosition));
 			MapId = mapId;
+			MovementType = movementType;
+			MovementData = movementData;
 			InitialOrientation = initialOrientation;
 		}
 

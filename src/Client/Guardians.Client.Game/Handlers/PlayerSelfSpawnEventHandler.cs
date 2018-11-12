8044b358f14b3600c02f3313ccfeb98c8978e4dc
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 18:47:43 -0600

    Added OnGameInitialize call to PlayerSpawn event handler

--- src/Client/Guardians.Client.Game/Handlers/PlayerSelfSpawnEventHandler.cs
+++ src/Client/Guardians.Client.Game/Handlers/PlayerSelfSpawnEventHandler.cs
@@ -16,28 +16,40 @@ namespace Guardians
 	{
 		private IFactoryCreatable<GameObject, DefaultEntityCreationContext> PlayerFactory { get; }
 
+		private IReadOnlyCollection<IGameInitializable> Initializables { get; }
+
 		/// <inheritdoc />
-		public PlayerSelfSpawnEventHandler(ILog logger, IFactoryCreatable<GameObject, DefaultEntityCreationContext> playerFactory)
+		public PlayerSelfSpawnEventHandler(
+			ILog logger, 
+			IFactoryCreatable<GameObject, DefaultEntityCreationContext> playerFactory,
+			IReadOnlyCollection<IGameInitializable> initializables)
 			: base(logger)
 		{
-
 			PlayerFactory = playerFactory ?? throw new ArgumentNullException(nameof(playerFactory));
+			Initializables = initializables ?? throw new ArgumentNullException(nameof(initializables));
 		}
 
 		/// <inheritdoc />
-		public override Task HandleMessage(IPeerMessageContext<GameClientPacketPayload> context, PlayerSelfSpawnEventPayload payload)
+		public override async Task HandleMessage(IPeerMessageContext<GameClientPacketPayload> context, PlayerSelfSpawnEventPayload payload)
 		{
 			//TODO: Actually handle this. Right now it's just demo code, it actually could fail.
 			if(Logger.IsInfoEnabled)
 				Logger.Info($"Recieved server commanded PlayerSpawn. Player GUID: {payload.CreationData.EntityGuid} Position: {payload.CreationData.InitialMovementData.InitialPosition}");
 
+			await new UnityYieldAwaitable();
+
 			//Don't do any checks for now, we just spawn
 			PlayerFactory.Create(new DefaultEntityCreationContext(payload.CreationData.EntityGuid, payload.CreationData.InitialMovementData, EntityPrefab.LocalPlayer));
 
-			//TODO: We need to make this the first packet, or couple of packets. We don't want to do this inbetween potentially slow operatons.
-			context.PayloadSendService.SendMessageImmediately(new ServerTimeSyncronizationRequestPayload(DateTime.UtcNow.Ticks));
 
-			return Task.CompletedTask;
+			//Call all OnGameInitializables
+			foreach(var init in Initializables)
+				await init.OnGameInitialized()
+					.ConfigureAwait(false);
+
+			//TODO: We need to make this the first packet, or couple of packets. We don't want to do this inbetween potentially slow operatons.
+			await context.PayloadSendService.SendMessageImmediately(new ServerTimeSyncronizationRequestPayload(DateTime.UtcNow.Ticks))
+				.ConfigureAwait(false);
 		}
 	}
 }

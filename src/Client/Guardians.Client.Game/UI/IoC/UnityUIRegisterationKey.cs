6bd269677256002f2658d065db35895220117bae
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 18:43:34 -0600

    Added UI Adapter Key/Interface

---
+++ src/Client/Guardians.Client.Game/UI/IoC/UnityUIRegisterationKey.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Enumeration of keys for Unity UI
+	/// IoC container registeration.
+	/// </summary>
+	public enum UnityUIRegisterationKey
+	{
+		Unknown = 0,
+
+		PlayerUnitFrame = 1
+	}
+}

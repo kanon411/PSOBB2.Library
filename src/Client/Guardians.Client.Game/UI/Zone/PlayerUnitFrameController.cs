61cff41367d8c65e69f39ba449e2dbc7f099ac35
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 18:48:00 -0600

    Add demo PlayerUnitFrameController

---
+++ src/Client/Guardians.Client.Game/UI/Zone/PlayerUnitFrameController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Autofac.Features.AttributeFilters;
+using UnityEngine.UI;
+
+namespace Guardians
+{
+	public sealed class PlayerUnitFrameController : IGameInitializable
+	{
+		private IUIText PlayerNameTextField { get; }
+
+		/// <inheritdoc />
+		public PlayerUnitFrameController([KeyFilter(UnityUIRegisterationKey.PlayerUnitFrame)] IUIText playerNameTextField)
+		{
+			PlayerNameTextField = playerNameTextField ?? throw new ArgumentNullException(nameof(playerNameTextField));
+		}
+
+		public async Task OnGameInitialized()
+		{
+			//Join main thread to touch Unity3D UI
+			await new UnityYieldAwaitable();
+
+			PlayerNameTextField.Text = "Testing New IoC";
+		}
+	}
+}

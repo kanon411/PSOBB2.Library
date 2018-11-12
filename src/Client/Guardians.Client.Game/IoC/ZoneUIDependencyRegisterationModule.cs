73229547865c304d3bdca98e8f4c71afcaf70648
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 18:46:34 -0600

    Added UI adapter registeration

---
+++ src/Client/Guardians.Client.Game/IoC/ZoneUIDependencyRegisterationModule.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+using SceneJect.Common;
+using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
+
+namespace Guardians
+{
+	public sealed class ZoneUIDependencyRegisterationModule : NonBehaviourDependency
+	{
+		/// <inheritdoc />
+		public override void Register(ContainerBuilder register)
+		{
+			foreach(var go in SceneManager.GetActiveScene().GetRootGameObjects())
+			{
+				foreach(var registerable in go.GetComponentsInChildren<SerializedMonoBehaviour>()
+					.Select(m => m as IUIAdapterRegisterable)
+					.Where(m => m != null))
+				{
+					//Registers the adapter with the specified Key and Service Type.
+					register.RegisterInstance(registerable)
+						.SingleInstance()
+						.Keyed(registerable.RegisterationKey, registerable.UISerivdeType);
+				}
+			}
+		}
+	}
+}

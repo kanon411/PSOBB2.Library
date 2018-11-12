171e8256f618f67e6e8367738c5daf94c4f4388f
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 18:46:56 -0600

    Added IGameInitializable and registeration modules

---
+++ src/Client/Guardians.Client.Game/IoC/GameInitializablesRegisterModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using GladNet;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Simplified creatable type for <see cref="GameInitializableRegisterationAutofacModule"/>
+	/// </summary>
+	public sealed class GameInitializablesRegisterModule : AutofacBasedDependencyRegister<GameInitializableRegisterationAutofacModule>
+	{
+
+	}
+}
---
+++ src/Client/Guardians.Client.Game/IoC/Modules/GameInitializableRegisterationAutofacModule.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autofac;
+using Autofac.Features.AttributeFilters;
+using Fasterflect;
+using GladNet;
+
+namespace Guardians
+{
+	public sealed class GameInitializableRegisterationAutofacModule : Module
+	{
+		public GameInitializableRegisterationAutofacModule()
+		{
+			
+		}
+
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			//New IPeerContext generic param now so we register as implemented interface
+			foreach(var gameInit in GetType().Assembly.GetExportedTypes()
+				.Where(t => t.Implements(typeof(IGameInitializable))))
+			{
+				builder.RegisterType(gameInit)
+					.As<IGameInitializable>()
+					.AsSelf()
+					.SingleInstance()
+					//TODO: We don't want to have to manually deal with this, we should create Attribute/Metadata to determine if this should be enabled.
+					.WithAttributeFiltering();
+			}
+		}
+	}
+}

a099af88f43c22d3f3c41b6188f6bc7bc046be99
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 21:11:24 -0600

    Added simple ILocalPlayerDetails container

---
+++ src/Client/Guardians.Client.Game/Services/DefaultLocalPlayerDetails.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public sealed class DefaultLocalPlayerDetails : ILocalPlayerDetails, IReadonlyLocalPlayerDetails
+	{
+		/// <inheritdoc />
+		public NetworkEntityGuid LocalPlayerGuid { get; set; }
+
+		public DefaultLocalPlayerDetails()
+		{
+			
+		}
+	}
+}
---
+++ src/Client/Guardians.Client.Game/Services/ILocalPlayerDetails.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public interface ILocalPlayerDetails
+	{
+		NetworkEntityGuid LocalPlayerGuid { get; set; }
+	}
+
+	public interface IReadonlyLocalPlayerDetails
+	{
+		NetworkEntityGuid LocalPlayerGuid { get; }
+	}
+}

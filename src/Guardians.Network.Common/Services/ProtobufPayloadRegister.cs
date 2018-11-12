8a828f7069753c5b5ee9eb988a96a8d96d04e128
HelloKitty <andrew.blakely@ymail.com>
2018-11-11 21:11:09 -0600

    Revert protobuf duplicate registry fix
    It didn't work, broke Unity3D subtype serialization.

--- src/Guardians.Network.Common/Services/ProtobufPayloadRegister.cs
+++ src/Guardians.Network.Common/Services/ProtobufPayloadRegister.cs
@@ -20,10 +20,8 @@ namespace Guardians
 
 		public void Register(IReadOnlyDictionary<GamePayloadOperationCode, Type> clientPayloadByOpCode, IReadOnlyDictionary<GamePayloadOperationCode, Type> serverPayloadByOpCode)
 		{
-			if(!RuntimeTypeModel.Default.IsDefined(typeof(GameClientPacketPayload)))
-				RuntimeTypeModel.Default.Add(typeof(GameClientPacketPayload), true);
-			if(!RuntimeTypeModel.Default.IsDefined(typeof(GameServerPacketPayload)))
-				RuntimeTypeModel.Default.Add(typeof(GameServerPacketPayload), true);
+			RuntimeTypeModel.Default.Add(typeof(GameClientPacketPayload), true);
+			RuntimeTypeModel.Default.Add(typeof(GameServerPacketPayload), true);
 
 			clientPayloadByOpCode
 				.AsEnumerable()
@@ -34,13 +32,11 @@ namespace Guardians
 					Console.Write($"Registering Type: {pair.Value} Key: {(int)pair.Key}");
 
 					//TODO: Will this ever prevent a subtype registeration?
-					if(!RuntimeTypeModel.Default.IsDefined(pair.Value))
-					{
-						RuntimeTypeModel.Default.Add(pair.Value, true);
+					RuntimeTypeModel.Default.Add(pair.Value, true);
 
-						RuntimeTypeModel.Default[pair.Value.BaseType]
-							.AddSubType((int)pair.Key, pair.Value);
-					}
+					//TODO: Sometimes for unit testing this fails before the Protobuf model isn't reset. Figure a way to handle it. IfDefined breaks the Unity3D.
+					RuntimeTypeModel.Default[pair.Value.BaseType]
+						.AddSubType((int)pair.Key, pair.Value);
 				});
 		}
 	}

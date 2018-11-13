dadc9ab3fbfb7e9180ec1e5d8a2aa8eccd690672
HelloKitty <andrew.blakely@ymail.com>
2018-11-12 20:13:15 -0600

    Added GameInit attribute reading to GameInitializableRegisterationModule

--- src/Client/Guardians.Client.Game/IoC/Modules/GameInitializableRegisterationAutofacModule.cs
+++ src/Client/Guardians.Client.Game/IoC/Modules/GameInitializableRegisterationAutofacModule.cs
@@ -1,27 +1,38 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using Autofac;
 using Autofac.Features.AttributeFilters;
 using Fasterflect;
 using GladNet;
+using UnityEngine;
 
 namespace Guardians
 {
 	public sealed class GameInitializableRegisterationAutofacModule : Module
 	{
+		/// <summary>
+		/// The scene to gather initializables for.
+		/// </summary>
+		[Tooltip("Should indicate the scene type to gather IGameInitializables for.")]
+		[SerializeField]
+		private GameInitializableSceneSpecificationAttribute.SceneType Scene = GameInitializableSceneSpecificationAttribute.SceneType.ZoneGameScene;
+
 		public GameInitializableRegisterationAutofacModule()
 		{
-			
+
 		}
 
 		/// <inheritdoc />
 		protected override void Load(ContainerBuilder builder)
 		{
-			//New IPeerContext generic param now so we register as implemented interface
+			if(!Enum.IsDefined(typeof(GameInitializableSceneSpecificationAttribute.SceneType), Scene)) throw new InvalidEnumArgumentException(nameof(Scene), (int)Scene, typeof(GameInitializableSceneSpecificationAttribute.SceneType));
+
 			foreach(var gameInit in GetType().Assembly.GetExportedTypes()
-				.Where(t => t.Implements(typeof(IGameInitializable))))
+				.Where(t => t.Implements(typeof(IGameInitializable)))
+				.Where(t => t.Attributes<GameInitializableSceneSpecificationAttribute>().Any(a => a.Scene == Scene)))
 			{
 				builder.RegisterType(gameInit)
 					.As<IGameInitializable>()

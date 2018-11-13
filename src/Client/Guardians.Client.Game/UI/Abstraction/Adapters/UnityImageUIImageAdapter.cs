c9c3a42543db5989fc6e97c02adf24cd8fbd67b6
HelloKitty <andrew.blakely@ymail.com>
2018-11-12 20:12:11 -0600

    Added UIImage interface and UnityEngine adapter

---
+++ src/Client/Guardians.Client.Game/UI/Abstraction/Adapters/UnityImageUIImageAdapter.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Guardians
+{
+	public sealed class UnityImageUIImageAdapter : BaseUnityUIAdapter<Image, IUIImage>, IUIImage
+	{
+		/// <inheritdoc />
+		public void SetSpriteTexture(Texture2D texture)
+		{
+			if(UnityUIObject.sprite == null)
+				UnityUIObject.sprite = Sprite.Create(texture, Rect.zero, Vector2.zero); //TODO: What should defaults be?
+			else
+			{
+				//Sprites complain if we don't have proper size, so we need size based on the texture2D
+				UnityUIObject.sprite = Sprite.Create(texture, new Rect(0, 0, texture.width, texture.height), Vector2.zero);
+			}
+		}
+	}
+}

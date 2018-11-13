c8174772ecb5156fc9bba3ffe11036734a0e4021
HelloKitty <andrew.blakely@ymail.com>
2018-11-13 14:58:25 -0600

    Added AuthTokenRepository dependency module

---
+++ src/Client/Guardians.Client.Game/IoC/AuthenticationTokenDependencyRegister.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public sealed class AuthenticationTokenDependencyRegister : AutofacBasedDependencyRegister<AuthenticationTokenAutofacModule>
+	{
+		
+	}
+}
---
+++ src/Client/Guardians.Client.Game/IoC/Modules/AuthenticationTokenAutofacModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using GladNet;
+
+namespace Guardians
+{
+	public sealed class AuthenticationTokenAutofacModule : Module
+	{
+		public AuthenticationTokenAutofacModule()
+		{
+			
+		}
+
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			//This is used in many scenes to get and/or set the
+			//the auth token for the session.
+			builder.RegisterType<AuthenticationTokenRepository>()
+				.AsImplementedInterfaces()
+				.SingleInstance();
+		}
+	}
+}

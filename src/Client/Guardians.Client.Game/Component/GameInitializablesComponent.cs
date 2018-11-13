711bea6cef57b87b4b2e205a0ae8ef3efd6c9697
HelloKitty <andrew.blakely@ymail.com>
2018-11-13 01:01:19 -0600

    Created Unity component to invoke GameInitializables

---
+++ src/Client/Guardians.Client.Game/Component/GameInitializablesComponent.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Guardians
+{
+	[Injectee]
+	public sealed class GameInitializablesComponent : MonoBehaviour
+	{
+		[Inject]
+		private IReadOnlyCollection<IGameInitializable> Initializables { get; }
+
+		private async Task Start()
+		{
+			var taskList = Initializables
+				.Select(i => i.OnGameInitialized())
+				.ToList();
+
+			await Task.WhenAll(taskList)
+				.ConfigureAwait(false);
+		}
+	}
+}

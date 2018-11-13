bcd1162d2c8def44432f014652cfd3b7ca7e0fd1
HelloKitty <andrew.blakely@ymail.com>
2018-11-13 00:18:56 -0600

    Created login scene autofac module

---
+++ src/Client/Guardians.Client.Game/IoC/Modules/LoginScreenDependenciesAutofacModule.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Autofac.Features.AttributeFilters;
+using GladNet;
+
+namespace Guardians
+{
+	public sealed class LoginScreenDependenciesAutofacModule : Module
+	{
+		public LoginScreenDependenciesAutofacModule()
+		{
+			
+		}
+
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<LoginScreenUIElements>()
+				.AsSelf()
+				.WithAttributeFiltering();
+		}
+	}
+}

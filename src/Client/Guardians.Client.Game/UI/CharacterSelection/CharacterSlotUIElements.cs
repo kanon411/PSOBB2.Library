95c40e9c20ea08fff76267b759a16d188fb4c167
HelloKitty <andrew.blakely@ymail.com>
2018-11-13 17:45:53 -0600

    Added Character slot elements behavior
    Used for exposing the adapted UI elements

---
+++ src/Client/Guardians.Client.Game/UI/CharacterSelection/CharacterSlotUIElements.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Guardians
+{
+	public sealed class CharacterSlotUIElements : MonoBehaviour
+	{
+		[SerializeField]
+		private UnityTextUITextAdapter _CharacterNameText;
+
+		/// <summary>
+		/// The character name UI Text.
+		/// </summary>
+		public IUIText CharacterNameText => _CharacterNameText;
+	}
+}

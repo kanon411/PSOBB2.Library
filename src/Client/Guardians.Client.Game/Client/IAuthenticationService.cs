947d88ecb0f3e2fad9d35fa5aee4e23ec09b2fa6
HelloKitty <andrew.blakely@ymail.com>
2018-11-12 23:40:51 -0600

    Moved IAuthenticationService to Game project

---
+++ src/Client/Guardians.Client.Game/Client/IAuthenticationService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using TypeSafe.Http.Net;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Proxy interface for Authentication Server RPCs.
+	/// </summary>
+	[Header("User-Agent", "GuardiansClient")]
+	public interface IAuthenticationService
+	{
+		/// <summary>
+		/// Authenticate request method. Sends the request model as a URLEncoded body.
+		/// See the documentation for information about the endpoint.
+		/// https://github.com/HaloLive/Documentation
+		/// </summary>
+		/// <param name="request">The request model.</param>
+		/// <returns>The authentication result.</returns>
+		[SupressResponseErrorCodes((int)HttpStatusCode.BadRequest)] //OAuth spec returns 400 BadRequest on failed auth
+		[Post("/api/auth")]
+		Task<JWTModel> TryAuthenticate([UrlEncodedBody] AuthenticationRequestModel request);
+	}
+}

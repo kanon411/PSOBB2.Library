fc725081371c669ae8ef438a8a6fffb8fd765aa8
HelloKitty <andrew.blakely@ymail.com>
2018-11-13 18:14:59 -0600

    Added IUIButton to CharacterSlotElements container

--- src/Client/Guardians.Client.Game/UI/CharacterSelection/CharacterSlotUIElements.cs
+++ src/Client/Guardians.Client.Game/UI/CharacterSelection/CharacterSlotUIElements.cs
@@ -11,9 +11,17 @@ namespace Guardians
 		[SerializeField]
 		private UnityTextUITextAdapter _CharacterNameText;
 
+		[SerializeField]
+		private UnityButtonUIButtonAdapter _CharacterSlotButton;
+
 		/// <summary>
 		/// The character name UI Text.
 		/// </summary>
 		public IUIText CharacterNameText => _CharacterNameText;
+
+		/// <summary>
+		/// The button element for the character slot.
+		/// </summary>
+		public IUIButton CharacterSlotButton => _CharacterSlotButton;
 	}
 }

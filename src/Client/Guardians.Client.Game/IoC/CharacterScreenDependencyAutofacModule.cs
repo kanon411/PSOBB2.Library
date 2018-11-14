eb1aa071a1232cb80d83de856927cc97876cf838
HelloKitty <andrew.blakely@ymail.com>
2018-11-13 18:13:59 -0600

    Refactored character screen dependencies to autofac module

---
+++ src/Client/Guardians.Client.Game/IoC/CharacterScreenDependencyAutofacModule.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Security;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Autofac;
+using Common.Logging;
+
+namespace Guardians
+{
+	public sealed class CharacterScreenDependencyAutofacModule : NetworkServiceDiscoveryableAutofaceModule
+	{
+		private string ServiceDiscoveryUrl { get; }
+
+		/// <inheritdoc />
+		public CharacterScreenDependencyAutofacModule(string serviceDiscoveryUrl)
+		{
+			if(string.IsNullOrEmpty(serviceDiscoveryUrl)) throw new ArgumentException("Value cannot be null or empty.", nameof(serviceDiscoveryUrl));
+			ServiceDiscoveryUrl = serviceDiscoveryUrl;
+		}
+
+		public CharacterScreenDependencyAutofacModule()
+		{
+			//Default URL
+			ServiceDiscoveryUrl = @"http://localhost:80";
+		}
+
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			ContainerBuilder register = builder;
+
+			//Set the sync context
+			UnityExtended.InitializeSyncContext();
+
+			//Postsharp requires we setup some backend stuff
+			//CachingServices.DefaultBackend = new MemoryCachingBackend();
+
+			register.RegisterInstance(new UnityLogger(LogLevel.All))
+				.As<ILog>()
+				.SingleInstance();
+
+			register.RegisterType<AuthenticationTokenRepository>()
+				.As<IReadonlyAuthTokenRepository>()
+				.As<IAuthTokenRepository>()
+				.SingleInstance();
+
+			register.RegisterType<TypeSafeServiceDiscoveryServiceClient>()
+				.As<IServiceDiscoveryService>()
+				.WithParameter(new TypedParameter(typeof(string), ServiceDiscoveryUrl))
+				.SingleInstance();
+
+			register.Register(context =>
+			{
+				//The below is not true for right now, we have global service discovery point to the gameserver for testing.
+				//This registeration is abit complicated
+				//because we are skipping the game server selection
+				//to do this we must query the service discovery and THEN
+				//we query the the gameserver's service discovery.
+				IServiceDiscoveryService serviceDiscovery = context.Resolve<IServiceDiscoveryService>();
+
+				return new RemoteNetworkCharacterService(QueryForRemoteServiceEndpoint(serviceDiscovery, "GameServer"));
+			})
+				.As<ICharacterService>()
+				.SingleInstance();
+
+			//Name query service
+			register.Register(context => new CachedNameQueryServiceDecorator(new RemoteNetworkedNameQueryService(context.Resolve<ICharacterService>())))
+				.As<INameQueryService>()
+				.SingleInstance();
+
+			register.RegisterType<LocalCharacterDataRepository>()
+				.As<ICharacterDataRepository>()
+				.SingleInstance();
+
+			register.RegisterType<CharacterAvatarQueryService>()
+				.AsImplementedInterfaces();
+		}
+	}
+}
--- src/Client/Guardians.Client.Game/IoC/GuardiansUnityCharacterScreenDependencyContainer.cs
+++ src/Client/Guardians.Client.Game/IoC/GuardiansUnityCharacterScreenDependencyContainer.cs
@@ -20,85 +20,16 @@ using UnityEngine;
 namespace Guardians
 {
 	//TODO: Extract this into an Autofac module
-	public sealed class GuardiansUnityCharacterScreenDependencyContainer : NonBehaviourDependency
+	public sealed class GuardiansUnityCharacterScreenDependencyContainer : AutofacBasedDependencyRegister<CharacterScreenDependencyAutofacModule>
 	{
 		[SerializeField]
 		private string ServiceDiscoveryUrl;
 
 		/// <inheritdoc />
-		public override void Register(ContainerBuilder register)
+		protected override CharacterScreenDependencyAutofacModule CreateModule()
 		{
-			//Set the sync context
-			UnityExtended.InitializeSyncContext();
-
-			//https://stackoverflow.com/questions/4926676/mono-https-webrequest-fails-with-the-authentication-or-decryption-has-failed
-			ServicePointManager.ServerCertificateValidationCallback = MyRemoteCertificateValidationCallback;
-			ServicePointManager.SecurityProtocol = SecurityProtocolType.Tls12;
-			ServicePointManager.CheckCertificateRevocationList = false;
-
-			//Postsharp requires we setup some backend stuff
-			//CachingServices.DefaultBackend = new MemoryCachingBackend();
-
-			ServiceCollection services = new ServiceCollection();
-
-			register.RegisterInstance(new UnityLogger(LogLevel.All))
-				.As<ILog>()
-				.SingleInstance();
-
-			register.RegisterType<AuthenticationTokenRepository>()
-				.As<IReadonlyAuthTokenRepository>()
-				.As<IAuthTokenRepository>()
-				.SingleInstance();
-
-			register.RegisterType<TypeSafeServiceDiscoveryServiceClient>()
-				.As<IServiceDiscoveryService>()
-				.WithParameter(new TypedParameter(typeof(string), ServiceDiscoveryUrl))
-				.SingleInstance();
-
-			register.Register(context =>
-				{
-					//The below is not true for right now, we have global service discovery point to the gameserver for testing.
-					//This registeration is abit complicated
-					//because we are skipping the game server selection
-					//to do this we must query the service discovery and THEN
-					//we query the the gameserver's service discovery.
-					IServiceDiscoveryService serviceDiscovery = context.Resolve<IServiceDiscoveryService>();
-
-					return new RemoteNetworkCharacterService(QueryForRemoteServiceEndpoint(serviceDiscovery, "GameServer"));
-				})
-				.As<ICharacterService>()
-				.SingleInstance();
-
-			//Name query service
-			register.Register(context => new CachedNameQueryServiceDecorator(new RemoteNetworkedNameQueryService(context.Resolve<ICharacterService>())))
-				.As<INameQueryService>()
-				.SingleInstance();
-
-			register.RegisterType<LocalCharacterDataRepository>()
-				.As<ICharacterDataRepository>()
-				.SingleInstance();
-
-			register.Populate(services);
-		}
-
-		//TODO: Put this in a base class or something
-		private async Task<string> QueryForRemoteServiceEndpoint(IServiceDiscoveryService serviceDiscovery, string serviceType)
-		{
-			ResolveServiceEndpointResponse endpointResponse = await serviceDiscovery.DiscoverService(new ResolveServiceEndpointRequest(ClientRegionLocale.US, serviceType));
-
-			if(!endpointResponse.isSuccessful)
-				throw new InvalidOperationException($"Failed to query for Service: {serviceType} Result: {endpointResponse.ResultCode}");
-
-			Debug.Log($"Recieved service discovery response: {endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort} for Type: {serviceType}");
-
-			//TODO: Do we need extra slash?
-			return $"{endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort}/";
-		}
-
-		//https://stackoverflow.com/questions/4926676/mono-https-webrequest-fails-with-the-authentication-or-decryption-has-failed
-		private bool MyRemoteCertificateValidationCallback(object sender, X509Certificate certificate, X509Chain chain, SslPolicyErrors sslpolicyerrors)
-		{
-			return true;
+			//We need to provide the service discovery url
+			return new CharacterScreenDependencyAutofacModule(ServiceDiscoveryUrl);
 		}
 	}
 }

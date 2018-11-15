06edfe9bb2a4c5a50c99909b5ec8c2983422192b
HelloKitty <andrew.blakely@ymail.com>
2018-11-14 21:50:45 -0600

    Added entity data field enum

---
+++ src/Guardians.Common/Entity/EntityDataFieldType.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Enumeration of Entity Data FieldTypes.
+	/// Each entry in this enumeration represents a 4 byte field
+	/// of entity data.
+	/// </summary>
+	public enum EntityDataFieldType
+	{
+		/// <summary>
+		/// The maximum health of the entity.
+		/// Can be modified or changed, with bufs and etc.
+		/// </summary>
+		EntityMaxHealth = 0,
+
+		/// <summary>
+		/// The current health of the entity.
+		/// </summary>
+		EntityCurrentHealth = 1
+	}
+}

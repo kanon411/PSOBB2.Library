0f5a611f78df335235b04c86356efb52340a5d8c
HelloKitty <andrew.blakely@ymail.com>
2018-11-16 01:00:51 -0600

    Made IEntityDataFieldContainer Readonly

---
+++ src/Guardians.Common/Collections/IReadonlyEntityDataFieldContainer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Contract for containers that hold
+	/// field values.
+	/// </summary>
+	public interface IReadonlyEntityDataFieldContainer
+	{
+		TValueType GetFieldValue<TValueType>(int index)
+			where TValueType : struct;
+	}
+
+	/// <summary>
+	/// Contract for containers that hold
+	/// field values.
+	/// </summary>
+	public interface IReadonlyEntityDataFieldContainer<in TFieldType>
+	{
+		TValueType GetFieldValue<TValueType>(TFieldType index)
+			where TValueType : struct;
+	}
+}

7ee887e348fbbc1ac3a7c05a13e29f4cd9a175ae
HelloKitty <andrew.blakely@ymail.com>
2018-11-18 02:44:08 -0600

    Added the write enabled entity data container

---
+++ src/Guardians.Common/Collections/IEntityDataFieldContainer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public interface IEntityDataFieldContainer : IReadonlyEntityDataFieldContainer
+	{
+		void SetFieldValue<TValueType>(int index, TValueType value)
+			where TValueType : struct;
+	}
+
+	public interface IEntityDataFieldContainer<in TFieldType> : IReadonlyEntityDataFieldContainer<TFieldType>, IEntityDataFieldContainer
+	{
+		void SetFieldValue<TValueType>(TFieldType index, TValueType value)
+			where TValueType : struct;
+	}
+}

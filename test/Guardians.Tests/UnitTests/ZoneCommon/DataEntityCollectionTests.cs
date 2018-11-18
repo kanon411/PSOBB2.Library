8783a44645536ba8053f08f964db91a9ee9c9f29
HelloKitty <andrew.blakely@ymail.com>
2018-11-18 16:52:14 -0600

    Added inital DataEntityCollection test

---
+++ test/Guardians.Tests/UnitTests/ZoneCommon/DataEntityCollectionTests.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+
+namespace Guardians.Tests.Collections
+{
+	[TestFixture]
+	public sealed class DataEntityCollectionTests
+	{
+		[Test]
+		public void Test_Can_Create_Ctor()
+		{
+			Assert.DoesNotThrow(() => new EntityFieldDataCollection<TestFieldType>());
+		}
+	}
+}

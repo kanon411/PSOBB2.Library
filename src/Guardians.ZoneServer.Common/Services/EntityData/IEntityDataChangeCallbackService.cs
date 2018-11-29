e7736d8267b675585562336db17709ebe7992bf8
HelloKitty <andrew.blakely@ymail.com>
2018-11-29 17:47:23 -0600

    Add IEntityDataChangeCallbackService (contract for calling entity changes)

---
+++ src/Guardians.ZoneServer.Common/Services/EntityData/IEntityDataChangeCallbackService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public interface IEntityDataChangeCallbackService
+	{
+		/// <summary>
+		/// Invokes any registered change callbacks for the <see cref="entity"/>
+		/// if callbacks have been registered for changes involving <see cref="field"/>.
+		/// </summary>
+		/// <param name="entity"></param>
+		/// <param name="field"></param>
+		/// <param name="dataContainer"></param>
+		void InvokeChangeEvents(NetworkEntityGuid entity, EntityDataFieldType field, IEntityDataFieldContainer dataContainer);
+	}
+}

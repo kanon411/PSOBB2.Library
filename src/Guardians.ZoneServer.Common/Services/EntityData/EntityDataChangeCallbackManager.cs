7313ec42164569dfea374ef1df81cb709cc7effc
HelloKitty <andrew.blakely@ymail.com>
2018-11-29 17:47:48 -0600

    Added inital implementation of entity data callback manager

---
+++ src/Guardians.ZoneServer.Common/Services/EntityData/EntityDataChangeCallbackManager.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using JetBrains.Annotations;
+
+namespace Guardians
+{
+	//TODO: We need some handling for callback cleanup, especially when an entity disappears.
+	public sealed class EntityDataChangeCallbackManager : IEntityDataChangeCallbackRegisterable, IEntityDataChangeCallbackService
+	{
+		private Dictionary<NetworkEntityGuid, Dictionary<EntityDataFieldType, Action<IEntityDataFieldContainer>>> CallbackMap { get; }
+
+		public EntityDataChangeCallbackManager()
+		{
+			CallbackMap = new Dictionary<NetworkEntityGuid, Dictionary<EntityDataFieldType, Action<IEntityDataFieldContainer>>>();
+		}
+
+		/// <inheritdoc />
+		public void RegisterCallback<TCallbackValueCastType>(NetworkEntityGuid entity, EntityDataFieldType dataField, Action<NetworkEntityGuid, EnityDataChangedArgs<TCallbackValueCastType>> callback) 
+			where TCallbackValueCastType : struct
+		{
+			//TODO: Anyway we can avoid this for registering callbacks, wasted cycles kinda
+			if(!CallbackMap.ContainsKey(entity))
+				CallbackMap.Add(entity, new Dictionary<EntityDataFieldType, Action<IEntityDataFieldContainer>>());
+
+			CallbackMap[entity][dataField] += dataContainer =>
+			{
+				//TODO: If we ever support original value we should change this
+				//So, the callback needs to send the entity guid and the entity data change args which contain the original (not working yet) and new value.
+				callback(entity, new EnityDataChangedArgs<TCallbackValueCastType>(default(TCallbackValueCastType), dataContainer.GetFieldValue<TCallbackValueCastType>((int)dataField)));
+			};
+		}
+
+		/// <inheritdoc />
+		public void InvokeChangeEvents(NetworkEntityGuid entity, EntityDataFieldType field, IEntityDataFieldContainer dataContainer)
+		{
+			//We aren't watching ANY data changes for this particular entity.
+			if(!CallbackMap.ContainsKey(entity))
+				return;
+
+			//If we have any registered callbacks for this entity's data change we should dispatch it (they will all be called)
+			if(CallbackMap[entity].ContainsKey(field))
+				CallbackMap[entity][field]?.Invoke(dataContainer);
+		}
+	}
+}

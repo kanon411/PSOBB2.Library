8548fe9029ecfd0da2c8efabfd300fa7e6994845
HelloKitty <andrew.blakely@ymail.com>
2018-11-30 03:04:19 -0600

    Added FieldValueUpdateEventHandler for entity data update packets

---
+++ src/Client/Guardians.Client.Game/Handlers/FieldValueUpdateEventHandler.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+using UnityEngine;
+
+namespace Guardians
+{
+	public sealed class FieldValueUpdateEventHandler : BaseZoneClientGameMessageHandler<FieldValueUpdateEvent>
+	{
+		private IReadonlyEntityGuidMappable<IEntityDataFieldContainer> EntityDataContainerMap { get; }
+
+		/// <inheritdoc />
+		public FieldValueUpdateEventHandler(ILog logger,
+			IReadonlyEntityGuidMappable<IEntityDataFieldContainer> entityDataContainerMap) 
+			: base(logger)
+		{
+			EntityDataContainerMap = entityDataContainerMap ?? throw new ArgumentNullException(nameof(entityDataContainerMap));
+		}
+
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GameClientPacketPayload> context, FieldValueUpdateEvent payload)
+		{
+			//Assume the update fields aren't null and there is at least 1
+			foreach(EntityAssociatedData<FieldValueUpdate> update in payload.FieldValueUpdates)
+			{
+				//TODO: We shouldn't assume we know the entity, but technically we should based on order of server-side events.
+				IEntityDataFieldContainer entityDataContainer = EntityDataContainerMap[update.EntityGuid];
+
+				foreach(var entry in update.Data.FieldValueUpdateMask
+					.EnumerateSetBitsByIndex()
+					.Zip(update.Data.FieldValueUpdates, (setIndex, value) => new {setIndex, value}))
+				{
+					entityDataContainer.SetFieldValue(entry.setIndex, entry.value);
+				}
+			}
+
+			return Task.CompletedTask;
+		}
+	}
+}

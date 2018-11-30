577941011199cf0858377661c8bdf97691b8af67
HelloKitty <andrew.blakely@ymail.com>
2018-11-30 17:01:57 -0600

    Readded ContentServer service; fixed simple faults

--- src/Server/Guardians.Service.ContentServer/Startup.cs
+++ src/Server/Guardians.Service.ContentServer/Startup.cs
@@ -8,6 +8,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;

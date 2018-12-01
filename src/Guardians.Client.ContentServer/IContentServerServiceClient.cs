a698718d1ed3c3edde97e7635435edb39f3d4075
HelloKitty <andrew.blakely@ymail.com>
2018-12-01 06:26:36 -0600

    Added ContentServer client project; Added Seperate Authentication Client Project

---
+++ src/Guardians.Client.ContentServer/IContentServerServiceClient.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeSafe.Http.Net;
+
+namespace Guardians
+{
+	//TODO: We should seperate editor service from the general content server that the client reads.
+	//From ProjectVindictive: https://github.com/HelloKitty/ProjectVindictive.Library/blob/master/src/ProjectVindictive.SDK.Editor.Build/Client/IUserContentManagementServiceClient.cs
+	//TODO: Automate user-agent SDK version headers
+	[Header("User-Agent", "SDK 0.0.1")]
+	public interface IContentServerServiceClient
+	{
+		/// <summary>
+		/// Attempts to get a new URL that can be used to upload the world.
+		/// If successful the URl contained in the response will contain a valid upload
+		/// URL which can be used to upload world content.
+		/// </summary>
+		/// <param name="authToken">The user authentication token.</param>
+		/// <returns>A model representing the result of the world URL generation request.</returns>
+		[Post("/api/World")]
+		Task<RequestedUrlResponseModel> GetNewWorldUploadUrl([DynamicHeader("Authorization")] string authToken);
+	}
+}

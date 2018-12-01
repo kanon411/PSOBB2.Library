700a7ccb4a210fc75fb71ca422708443a851a277
HelloKitty <andrew.blakely@ymail.com>
2018-12-01 05:27:51 -0600

    Added world endpoint for creators to set the upload completion
    Validation is unimplemented for now.

--- src/Server/Guardians.Service.ContentServer/Controllers/WorldController.cs
+++ src/Server/Guardians.Service.ContentServer/Controllers/WorldController.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Amazon.Runtime;
+using Amazon.S3;
+using Amazon.S3.Model;
+using Amazon.Util;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
@@ -25,6 +29,89 @@ namespace Guardians
 			if(logger == null) throw new ArgumentNullException(nameof(logger));
 
 			Logger = logger;
+		}
+
+		[HttpPost("/{id}/uploaded")]
+		[AuthorizeJwt]
+		public async Task<IActionResult> SetWorldAsUploaded([FromRoute(Name = "id")] long worldId, [FromServices] IWorldEntryRepository worldEntryRepository, [FromServices] IAmazonS3 storageClient)
+		{
+			//At this point, the user is telling us they finished uploading the world.
+			//They could be lying so we should check that the resource exists AND
+			//we should also check that it's an asset bundle and gather some information from the header.
+
+			//First we verify a world exists with this id
+			if(!await worldEntryRepository.ContainsAsync(worldId).ConfigureAwait(false))
+			{
+				//TODO: We should say something more specific
+				return BadRequest();
+			}
+
+			WorldEntryModel model = await worldEntryRepository.RetrieveAsync(worldId)
+				.ConfigureAwait(false);
+
+			//Check the model is associated with this account. Only 1 account can own a world resource
+			if(model.AccountId != ClaimsReader.GetUserIdInt(User))
+				return Unauthorized();
+
+			//Now that we know the world is in the database and the account making this authorized requests owns it
+			//we can now actually check that the resource exists on the storeage system
+			//TODO: This relies on some outdated API/deprecated stuff.
+			bool resourceExists = await S3ResourceExists(storageClient, "projectvindictive-dev", (await Amazon.Util.ProfileManager.GetAWSCredentials("projectvindictive-dev").GetCredentialsAsync().ConfigureAwait(false)).AccessKey)
+				.ConfigureAwait(false); //TODO: Don't hardcore bucket name
+
+			//TODO: Be more descriptive
+			if(!resourceExists)
+				return NotFound();
+
+			//Ok, so the user IS the resource owner AND he did upload something, so let's validate the assetbundle header.
+			//TODO: Refactor this into an object that does the validation and generates readable data
+			//TODO: Actually implement asset bundle validation
+			//We haven't implemented this yet, we should do asset bundle parsing and validation
+			//This REALLY important to prevent invalid bundles from being uploaded
+			//or content that isn't even an asset bundle being uploaded
+			//See: https://github.com/HearthSim/UnityPack/wiki/Format-Documentation
+
+			//For now, since it's unimplemented let's just set it validated
+			await worldEntryRepository.SetWorldValidated(model.WorldId)
+				.ConfigureAwait(false);
+
+			return Ok();
+		}
+
+		private async Task<bool> S3ResourceExists(IAmazonS3 client, string bucket, string key)
+		{
+			//This is actually how the old AWS client worked: https://github.com/aws/aws-sdk-net/blob/master/sdk/src/Services/S3/Custom/_bcl/IO/S3FileInfo.cs
+			//Kinda bad design tbh Amazon lol
+			try
+			{
+				var request = new GetObjectMetadataRequest
+				{
+					BucketName = bucket,
+					Key = key.Replace('\\', '/') //S3helper.EncodeKey: https://github.com/aws/aws-sdk-net/blob/b691e46e57a3e24477e6a5fa2e849da44db7002f/sdk/src/Services/S3/Custom/_bcl/IO/S3Helper.cs
+				};
+				((Amazon.Runtime.Internal.IAmazonWebServiceRequest)request).AddBeforeRequestHandler(FileIORequestEventHandler);
+
+				// If the object doesn't exist then a "NotFound" will be thrown
+				await client.GetObjectMetadataAsync(request)
+					.ConfigureAwait(false);
+
+				return true;
+			}
+			catch(AmazonS3Exception e)
+			{
+				return false;
+			}
+		}
+
+		//From: https://github.com/aws/aws-sdk-net/blob/b691e46e57a3e24477e6a5fa2e849da44db7002f/sdk/src/Services/S3/Custom/_bcl/IO/S3Helper.cs
+		internal static void FileIORequestEventHandler(object sender, RequestEventArgs args)
+		{
+			WebServiceRequestEventArgs wsArgs = args as WebServiceRequestEventArgs;
+			if(wsArgs != null)
+			{
+				string currentUserAgent = wsArgs.Headers[AWSSDKUtils.UserAgentHeader];
+				wsArgs.Headers[AWSSDKUtils.UserAgentHeader] = currentUserAgent + " FileIO";
+			}
 		}
 
 		/// <summary>

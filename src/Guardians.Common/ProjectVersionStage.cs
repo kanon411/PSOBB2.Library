25d08e2b04f51422637d6611118a26834af87534
HelloKitty <andrew.blakely@ymail.com>
2018-12-04 16:00:38 -0600

    Added assert class for guarding against missing critical features

---
+++ src/Guardians.Common/ProjectVersionStage.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Type that helps with managing the project longterm by
+	/// offering methods and properties that can be used to determine if a missing critical feature is not
+	/// available in the version it should be in.
+	/// </summary>
+	public static class ProjectVersionStage
+	{
+		public static void AssertAlpha()
+		{
+			//TODO: When the project leaves alpha we should throw on this assert.
+			return;
+		}
+
+		public static void AssertBeta()
+		{
+			//TODO: When the project leaves beta we should throw on this assert.
+			return;
+		}
+	}
+}

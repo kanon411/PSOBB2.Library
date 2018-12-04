4bc61ed0458061604a3d6303fd595eb7aeefaab0
HelloKitty <andrew.blakely@ymail.com>
2018-12-04 16:02:25 -0600

    Added contentdownload authorization object

---
+++ src/Server/Guardians.Service.ContentServer/Services/IContentDownloadAuthroizationValidator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardians
+{
+	public interface IContentDownloadAuthroizationValidator
+	{
+		/// <summary>
+		/// Indicates if a provided user with the id <see cref="userId"/>
+		/// has authorization to access the content related to a world
+		/// for downloading.
+		/// </summary>
+		/// <param name="userId">The id of the user.</param>
+		/// <param name="worldId">The id of the world to check access for.</param>
+		/// <returns>True if the user is authorized to download the content.</returns>
+		Task<bool> CanUserAccessWorldContet(int userId, long worldId);
+	}
+}
--- src/Server/Guardians.Service.ContentServer/Startup.cs
+++ src/Server/Guardians.Service.ContentServer/Startup.cs
@@ -59,6 +59,7 @@ namespace Guardians.Service.ContentServer
 			//Adds and registers S3 service for URLBuilding and communication/credentials and etc
 			services.AddS3Service(Configuration);
 			services.AddTransient<IWorldEntryRepository, DatabaseBackedWorldEntryRepository>();
+			services.AddTransient<IDownloadAuthroizationValidator, UnimplementedDownloadAuthorizationValidator>()
 		}
 
 		private static void RegisterDatabaseServices(IServiceCollection services)

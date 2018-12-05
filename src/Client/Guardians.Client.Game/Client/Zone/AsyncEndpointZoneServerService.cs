c08bcdd269fe185e3c957b2bcf7691bb0b98a843
HelloKitty <andrew.blakely@ymail.com>
2018-12-04 19:40:54 -0600

    Added AsyncEndpoint zoneserver service

---
+++ src/Client/Guardians.Client.Game/Client/Zone/AsyncEndpointZoneServerService.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Guardians
+{
+	public sealed class AsyncEndpointZoneServerService : BaseAsyncEndpointService<IZoneServerService>, IZoneServerService
+	{
+		/// <inheritdoc />
+		public AsyncEndpointZoneServerService(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public AsyncEndpointZoneServerService(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public async Task<long> GetZoneWorld(int zoneId)
+		{
+			return await (await GetService().ConfigureAwait(false)).GetZoneWorld(zoneId).ConfigureAwait(false);
+		}
+	}
+}

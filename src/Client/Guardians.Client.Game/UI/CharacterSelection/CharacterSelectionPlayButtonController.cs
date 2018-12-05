9d2478fd0163c06682181ff128b7e6dffdcc98cc
HelloKitty <andrew.blakely@ymail.com>
2018-12-04 21:11:36 -0600

    Added character session creation on Play

--- src/Client/Guardians.Client.Game/UI/CharacterSelection/CharacterSelectionPlayButtonController.cs
+++ src/Client/Guardians.Client.Game/UI/CharacterSelection/CharacterSelectionPlayButtonController.cs
@@ -15,13 +15,25 @@ namespace Guardians
 
 		private ISceneManager SceneManagementService { get; }
 
+		private ICharacterService CharacterService { get; }
+
+		private ICharacterDataRepository CharacterData { get; }
+
+		private IReadonlyAuthTokenRepository AuthTokenRepo { get; }
+
 		/// <inheritdoc />
 		public CharacterSelectionPlayButtonController(
 			[KeyFilter(UnityUIRegisterationKey.Login)] IUIButton playButton,
-			ISceneManager sceneManagementService)
+			ISceneManager sceneManagementService,
+			ICharacterService characterService,
+			ICharacterDataRepository characterData,
+			IReadonlyAuthTokenRepository authTokenRepo)
 		{
 			PlayButton = playButton ?? throw new ArgumentNullException(nameof(playButton));
 			SceneManagementService = sceneManagementService ?? throw new ArgumentNullException(nameof(sceneManagementService));
+			CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
+			CharacterData = characterData ?? throw new ArgumentNullException(nameof(characterData));
+			AuthTokenRepo = authTokenRepo ?? throw new ArgumentNullException(nameof(authTokenRepo));
 		}
 
 		/// <inheritdoc />
@@ -29,12 +41,23 @@ namespace Guardians
 		{
 			await new UnityYieldAwaitable();
 
-			//TODO: In the future we need to connect to the GameServer and try to create a session. If that suceeds we'll know what map to load too.
-			PlayButton.AddOnClickListener(() =>
+			PlayButton.AddOnClickListenerAsync(async () =>
 			{
+				//We must actually create a session
+				//before the character can login.
+
+				CharacterSessionEnterResponse enterResponse = await CharacterService.TryEnterSession(CharacterData.CharacterId, AuthTokenRepo.RetrieveWithType())
+					.ConfigureAwait(false);
+
+				//TODO: handle character session failure
+
+				//Must be on main thread to do UI/Scene loading
+				await new UnityYieldAwaitable();
+
 				//Prevent multiple play interactions.
 				PlayButton.IsInteractable = false;
 				SceneManagementService.LoadLevel((int)GameInitializableSceneSpecificationAttribute.SceneType.ZoneGameScene, LoadSceneMode.Single);
+
 			});
 		}
 	}

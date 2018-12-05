522ea08539da75cbd34c5eb7e1d80e8bd6649a19
HelloKitty <andrew.blakely@ymail.com>
2018-12-04 19:43:21 -0600

    Added asyncendpoint client for ContentServer client

---
+++ src/Guardians.Client.ContentServer/AsyncEndpointContentServerService.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Guardians
+{
+	public sealed class AsyncEndpointContentServerService : BaseAsyncEndpointService<IContentServerServiceClient>, IContentServerServiceClient
+	{
+		/// <inheritdoc />
+		public AsyncEndpointContentServerService(Task<string> futureEndpoint) 
+			: base(futureEndpoint)
+		{
+		}
+
+		/// <inheritdoc />
+		public AsyncEndpointContentServerService(Task<string> futureEndpoint, RefitSettings settings) 
+			: base(futureEndpoint, settings)
+		{
+		}
+
+		/// <inheritdoc />
+		public async Task<RequestedUrlResponseModel> GetNewWorldUploadUrl(string authToken)
+		{
+			return await (await GetService().ConfigureAwait(false)).GetNewWorldUploadUrl(authToken).ConfigureAwait(false);
+		}
+
+		/// <inheritdoc />
+		public async Task<WorldDownloadURLResponse> RequestWorldDownloadUrl(long worldId, string authToken)
+		{
+			return await (await GetService().ConfigureAwait(false)).RequestWorldDownloadUrl(worldId, authToken).ConfigureAwait(false);
+		}
+	}
+}

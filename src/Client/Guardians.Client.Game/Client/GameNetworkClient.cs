4d1e51308b9d40c2e71f8b3c11ec1fd98e5409c8
HelloKitty <andrew.blakely@ymail.com>
2018-12-05 16:38:10 -0600

    Hackily added zone endpoint querying to GameNetworkClient

--- src/Client/Guardians.Client.Game/Client/GameNetworkClient.cs
+++ src/Client/Guardians.Client.Game/Client/GameNetworkClient.cs
@@ -28,22 +28,34 @@ namespace Guardians
 		//[Inject]
 		//private IGameConnectionEndpointDetails ConnectionEndpoint { get; }
 
+		[Inject]
+		private ICharacterService CharacterDataService { get; set; }
+
+		[Inject]
+		private IReadonlyAuthTokenRepository AuthTokenRepo { get; set; }
+
+		[Inject]
+		private ICharacterDataRepository CharacterDataRepo { get; set; }
+
+		[Inject]
+		private IZoneServerService ZoneService { get; set; }
+
 		[Tooltip("Indicates if the client should connect on Start.")]
 		[SerializeField]
 		private bool ConnectOnStart = false;
 
-		private void Start()
+		private async Task Start()
 		{
 			if(ConnectOnStart)
-				StartConnection();
+				await StartNetworkClient()
+					.ConfigureAwait(true); //we want the network client to dispatch on the current thread. At least I think, been awhile since I saw GladNet source.
 		}
 
 		//TODO: Is it safe or ok to await in start without ever completing?
-		public void StartConnection()
+		public async Task StartConnection()
 		{
 			//Just start the startup task.
-			Task.Factory.StartNew(StartNetworkClient, CancellationToken.None, TaskCreationOptions.None, TaskScheduler.FromCurrentSynchronizationContext())
-				.ConfigureAwait(true);
+			
 		}
 
 		//Starts the client by connecting
@@ -53,13 +65,32 @@ namespace Guardians
 			if(Logger.IsDebugEnabled)
 				Logger.Debug("Starting game client");
 
+			//First we need to know what zone this session should be going to
+			CharacterSessionDataResponse sessionData = await CharacterDataService.GetCharacterSessionData(CharacterDataRepo.CharacterId, AuthTokenRepo.RetrieveWithType())
+				.ConfigureAwait(true);
+
+			//TODO: Handle this better
+			if(!sessionData.isSuccessful)
+			{
+				Logger.Error($"Failed to query session data for Character: {CharacterDataRepo.CharacterId}. Cannot connect to instance server.");
+				return;
+			}
+
+			ResolveServiceEndpointResponse zoneEndpointResponse = await ZoneService.GetServerEndpoint(sessionData.ZoneId);
+
+			if(!zoneEndpointResponse.isSuccessful)
+			{
+				Logger.Error($"Failed to query endpoint for Zone: {sessionData.ZoneId} which Character: {CharacterDataRepo.CharacterId} is in. Cannot connect to instance server.");
+				return;
+			}
+
 			//TODO: Don't hardcode gameserver connection details
 			//As soon as we start we should attempt to connect to the login server.
-			bool result = await Client.ConnectAsync(IPAddress.Parse("72.190.177.214"), 5006)
+			bool result = await Client.ConnectAsync(IPAddress.Parse(zoneEndpointResponse.Endpoint.EndpointAddress), zoneEndpointResponse.Endpoint.EndpointPort)
 				.ConfigureAwait(true);
 
 			if(!result)
-				throw new InvalidOperationException($"Failed to connect to Server: {"127.0.0.1"} Port: {5006}");
+				throw new InvalidOperationException($"Failed to connect to Server: {zoneEndpointResponse.Endpoint.EndpointAddress} Port: {zoneEndpointResponse.Endpoint.EndpointPort}");
 
 			if(Logger.IsDebugEnabled)
 				Logger.Debug($"Connected client. isConnected: {Client.isConnected}");

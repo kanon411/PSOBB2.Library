8098655b370bb21a6847cd65d3957e7f462cd147
HelloKitty <andrew.blakely@ymail.com>
2018-12-06 14:30:47 -0600

    Fixed fault with DB migration generation duplicated column add IsValidated

--- src/Server/Guardians.Database.GameServer/Migrations/20181206201808_ZoneServerUniqueEndpointContraintAdd.cs
+++ src/Server/Guardians.Database.GameServer/Migrations/20181206201808_ZoneServerUniqueEndpointContraintAdd.cs
@@ -12,12 +12,6 @@ namespace Guardians.Database.GameServer.Migrations
                 nullable: false,
                 oldClrType: typeof(string));
 
-            migrationBuilder.AddColumn<bool>(
-                name: "IsValidated",
-                table: "world_entry",
-                nullable: false,
-                defaultValue: false);
-
             migrationBuilder.AddUniqueConstraint(
                 name: "AK_zone_endpoints_ZoneServerAddress_ZoneServerPort",
                 table: "zone_endpoints",
@@ -29,10 +23,6 @@ namespace Guardians.Database.GameServer.Migrations
             migrationBuilder.DropUniqueConstraint(
                 name: "AK_zone_endpoints_ZoneServerAddress_ZoneServerPort",
                 table: "zone_endpoints");
-
-            migrationBuilder.DropColumn(
-                name: "IsValidated",
-                table: "world_entry");
 
             migrationBuilder.AlterColumn<string>(
                 name: "ZoneServerAddress",

9fd7f8e7956689639e5734d67a96b791959cd347
HelloKitty <andrew.blakely@ymail.com>
2018-12-07 15:59:49 -0600

    Added sequence number to the voice data packet

--- src/Guardians.Payloads.Voice/Payloads/VoiceDataSetRequest.cs
+++ src/Guardians.Payloads.Voice/Payloads/VoiceDataSetRequest.cs
@@ -15,21 +15,30 @@ namespace Guardians
 		[ProtoMember(1)]
 		private byte[] _VoiceSegmentData { get; set; }
 
+		//Technically, the longer the session goes on the larger the voice data will get due to
+		//protobuf variant encoding.
+		/// <summary>
+		/// The sequence number for the voice data.
+		/// </summary>
+		[ProtoMember(2)]
+		public uint SequenceNumber { get; private set; }
+
 		[ProtoIgnore]
 		public IReadOnlyCollection<byte> VoiceSegmentData => _VoiceSegmentData;
 
 		/// <inheritdoc />
-		public VoiceDataChangeRaiseRequest(byte[] voiceSegmentData)
+		public VoiceDataChangeRaiseRequest(byte[] voiceSegmentData, uint sequenceNumber)
 		{
 			_VoiceSegmentData = voiceSegmentData ?? throw new ArgumentNullException(nameof(voiceSegmentData));
+			SequenceNumber = sequenceNumber;
 		}
 
 		/// <summary>
 		/// Serializer ctor.
 		/// </summary>
-		private VoiceDataChangeRaiseRequest()
+		private VoiceDataChangeRaiseRequest(uint sequenceNumber)
 		{
-			
+			SequenceNumber = sequenceNumber;
 		}
 	}
 }

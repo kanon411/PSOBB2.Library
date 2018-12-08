7ca943ec08eb8bb175d875e297f9e2ff160029ff
HelloKitty <andrew.blakely@ymail.com>
2018-12-08 13:32:49 -0600

    Remove or depcreate existing voice protocol packets

--- src/Guardians.Payloads.Voice/Payloads/VoiceInitializationRequest.cs
+++ src/Guardians.Payloads.Voice/Payloads/VoiceInitializationRequest.cs
@@ -6,6 +6,7 @@ using ProtoBuf;
 
 namespace Guardians
 {
+	//TODO: We actually don't need this, since we want to enforce the same encoding settings. If we allow people to change them they will, and that would be bad for bandwidth
 	/// <summary>
 	/// Payload sent by the client to initialize the voice chat
 	/// session.

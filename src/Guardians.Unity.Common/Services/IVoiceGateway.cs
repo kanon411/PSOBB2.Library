e3d734cf04bbf06731319e85e1546fbc68c63f65
HelloKitty <andrew.blakely@ymail.com>
2018-12-08 15:33:06 -0600

    Added IVoiceGateway interface

---
+++ src/Guardians.Unity.Common/Services/IVoiceGateway.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Contract for voice management objects that allow enties to join.
+	/// </summary>
+	public interface IVoiceGateway
+	{
+		/// <summary>
+		/// Joins the provided entity <see cref="entity"/>
+		/// into the voice session.
+		/// </summary>
+		/// <param name="entity">The entity to join into the session.</param>
+		void JoinVoiceSession(NetworkEntityGuid entity);
+
+		/// <summary>
+		/// Removes the provided entity <see cref="entity"/>
+		/// from the voice session.
+		/// </summary>
+		/// <param name="entity">The entity to remove.</param>
+		void LeaveVoiceSession(NetworkEntityGuid entity);
+	}
+}

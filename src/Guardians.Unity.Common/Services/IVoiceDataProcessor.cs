728fa2b15b8f075eaa686db82389ae53cc5f4139
HelloKitty <andrew.blakely@ymail.com>
2018-12-08 17:01:14 -0600

    Added missing voice processor interface

---
+++ src/Guardians.Unity.Common/Services/IVoiceDataProcessor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public interface IVoiceDataProcessor
+	{
+		/// <summary>
+		/// Processes the provided voice data.
+		/// </summary>
+		/// <param name="entity">The entity.</param>
+		/// <param name="voiceData"></param>
+		/// <param name="sequenceNumber"></param>
+		void ProcessIncomingVoiceData(NetworkEntityGuid entity, ArraySegment<byte> voiceData, uint sequenceNumber);
+	}
+}

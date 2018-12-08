3f6aa0bccf3dad5c269577f915d1833d1f429510
HelloKitty <andrew.blakely@ymail.com>
2018-12-08 15:31:26 -0600

    Added a default GladNet TestPlayer for dissonance voice tracking

---
+++ src/Dissonance/Dissonance.GladNet.Client/GladNetTestPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace Dissonance
+{
+	public sealed class GladNetTestPlayer : MonoBehaviour, IDissonancePlayer
+	{
+		/// <inheritdoc />
+		public string PlayerId => "Test";
+
+		/// <inheritdoc />
+		public Vector3 Position => this.transform.position;
+
+		/// <inheritdoc />
+		public Quaternion Rotation => this.transform.rotation;
+
+		//TODO: is it ok if we don't change this?
+		/// <inheritdoc />
+		public NetworkPlayerType Type => NetworkPlayerType.Local;
+
+		/// <inheritdoc />
+		public bool IsTracking => true;
+	}
+}

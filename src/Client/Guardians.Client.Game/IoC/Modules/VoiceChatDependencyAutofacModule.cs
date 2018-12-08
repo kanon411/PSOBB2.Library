cdd6cf3f1389a233ac4b99f209b597fd24bc41e2
HelloKitty <andrew.blakely@ymail.com>
2018-12-08 17:01:53 -0600

    Added voice dependency registeration module

---
+++ src/Client/Guardians.Client.Game/IoC/Modules/VoiceChatDependencyAutofacModule.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Common.Logging;
+using Dissonance.GladNet;
+using Dissonance.Networking;
+using GladNet;
+using ProtoBuf;
+
+namespace Guardians
+{
+	public sealed class VoiceChatDependencyAutofacModule : Module
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.RegisterType<GladNetDissonanceCommunicationNetworkClient>()
+				.As<ICommsNetwork>()
+				.As<IVoiceGateway>()
+				.As<IVoiceDataProcessor>()
+				.As<IGameTickable>()
+				.SingleInstance();
+		}
+	}
+}
---
+++ src/Client/Guardians.Client.Game/IoC/VoiceChatDependencyRegisterationModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// For voice chat dependencies.
+	/// </summary>
+	public sealed class VoiceChatDependencyRegisterationModule : AutofacBasedDependencyRegister<VoiceChatDependencyAutofacModule>
+	{
+		
+	}
+}

26b2a303efeb9a9bd1e4be0adcd805fc785c7b8c
HelloKitty <andrew.blakely@ymail.com>
2018-12-11 16:01:52 -0600

    Added CollectionsLocking attribute for AOP IGameTickables

---
+++ src/Server/Guardians.Server.ZoneServer/Tickable/Base/Attribute/CollectionsLockingAttribute.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Guardians
+{
+	//TODO: Move somewhere else?
+	/// <summary>
+	/// Enumeration of the types of locking.
+	/// </summary>
+	public enum LockType
+	{
+		Read = 1,
+
+		Write = 2,
+	}
+
+	[AttributeUsage(AttributeTargets.Class, AllowMultiple = false, Inherited = true)]
+	public sealed class CollectionsLockingAttribute : Attribute
+	{
+		/// <summary>
+		/// The locking type to apply to the collections
+		/// lock.
+		/// </summary>
+		public LockType DesiredLockingType { get; }
+
+		/// <inheritdoc />
+		public CollectionsLockingAttribute(LockType desiredLockingType)
+		{
+			if(!Enum.IsDefined(typeof(LockType), desiredLockingType)) throw new InvalidEnumArgumentException(nameof(desiredLockingType), (int)desiredLockingType, typeof(LockType));
+			DesiredLockingType = desiredLockingType;
+		}
+	}
+}

cf0e5648468a7fb2fbec1efb6dacee40768f1c88
HelloKitty <andrew.blakely@ymail.com>
2018-12-12 16:38:49 -0600

    Add missing player tracker interface

---
+++ src/Guardians.Unity.Common/Services/IPlayerTrackingRegisterable.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Dissonance;
+
+namespace Guardians
+{
+	public interface IPlayerTrackingRegisterable
+	{
+		void RegisterTracker(IDissonancePlayer player);
+
+		void RemoveTracker(IDissonancePlayer player);
+	}
+}

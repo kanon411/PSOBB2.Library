2207f95024311f4085cd915be2c1302d62a8cf8e
HelloKitty <andrew.blakely@ymail.com>
2018-12-12 16:37:49 -0600

    Fix networked yaxis rotation

--- src/Client/Guardians.Client.Game/Entity/Movement/ClientPositionChangeWithLookMovementGenerator.cs
+++ src/Client/Guardians.Client.Game/Entity/Movement/ClientPositionChangeWithLookMovementGenerator.cs
@@ -21,7 +21,7 @@ namespace Guardians
 			//We don't need to deal with time when a position change occurs.
 
 			//TODO: This is demo code, we should handle actual movement differently.
-			entity.transform.SetPositionAndRotation(MovementData.InitialPosition, entity.transform.rotation * Quaternion.Euler(Vector3.up * MovementData.YAxisRotation));
+			entity.transform.SetPositionAndRotation(MovementData.InitialPosition, Quaternion.Euler(Vector3.up * MovementData.YAxisRotation));
 
 			//We also have the camera look. So we need to set that somehow, we don't have a good way to set the hierarchy of bones/trackers yet
 			//TODO: This is hacky, we need a clean efficient way to set this replicated data.

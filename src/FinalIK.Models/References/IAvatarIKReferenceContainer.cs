a485d295354a38eabe136e325ac8bf2c82dd3c91
HelloKitty <andrew.blakely@ymail.com>
2018-12-13 02:27:16 -0600

    Added generic IAvatarIKReferenceContainer

---
+++ src/FinalIK.Models/References/IAvatarIKReferenceContainer.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FinalIK
+{
+	public interface IAvatarIKReferenceContainer<out TReferenceType>
+		where TReferenceType : IInverseKinematicReferenceable
+	{
+		//The reason this is lowercase is for compatibility with
+		//FinalIK. It uses a lot of public fields/props with lowercases.
+		TReferenceType references { get; }
+	}
+}

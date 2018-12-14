c41b089c6f435bc97db681fd14b0e3a6e631d98e
HelloKitty <andrew.blakely@ymail.com>
2018-12-14 13:28:47 -0600

    Added debugging tool for changing player model

---
+++ src/Client/Guardians.Client.Game/DebugComponents/DebugChangeLocalPlayerModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SceneJect.Common;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+namespace Guardians
+{
+	[Injectee]
+	public sealed class DebugChangeLocalPlayerModel : NetworkRequestSender
+	{
+		[SerializeField]
+		private int ModelId;
+
+		[Button]
+		public void SendModelChangeRequest()
+		{
+			this.SendService.SendMessage(new PlayerModelChangeRequestPayload(ModelId));
+		}
+	}
+}

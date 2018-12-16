f04c7ad7381c8a1964dcc9e15be5bd00f5ec7885
HelloKitty <andrew.blakely@ymail.com>
2018-12-16 03:56:41 -0600

    Added BaseExternalComponent for Types that are external monobehaviour event forwared Types

---
+++ src/Client/Guardians.Client.Game/ComponentsExternal/BaseExternalComponent.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+
+namespace Guardians
+{
+	public abstract class BaseExternalComponent
+	{
+		/// <summary>
+		/// The component logger.
+		/// </summary>
+		protected ILog Logger { get; }
+
+		/// <inheritdoc />
+		protected BaseExternalComponent([NotNull] ILog logger)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+		}
+	}
+}

a4161ecc8afb42deeb9837419d9598f2b253b80e
HelloKitty <andrew.blakely@ymail.com>
2018-12-16 03:58:37 -0600

    Added external behaviour autofac registeration

---
+++ src/Client/Guardians.Client.Game/IoC/ExternalBehaviourRegisterationModule.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// For external behaviour dependencies.
+	/// </summary>
+	public sealed class ExternalBehaviourRegisterationModule : AutofacBasedDependencyRegister<ExternalBehavioursAutofacModule>
+	{
+		
+	}
+}

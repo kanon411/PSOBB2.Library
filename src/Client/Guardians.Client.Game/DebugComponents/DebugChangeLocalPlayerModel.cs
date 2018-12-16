071d6f3ff5f91e95a222286092e9aad1d6462980
HelloKitty <andrew.blakely@ymail.com>
2018-12-15 21:22:17 -0600

    Expanded model changing debugging tool

--- src/Client/Guardians.Client.Game/DebugComponents/DebugChangeLocalPlayerModel.cs
+++ src/Client/Guardians.Client.Game/DebugComponents/DebugChangeLocalPlayerModel.cs
@@ -4,6 +4,7 @@ using System.Text;
 using SceneJect.Common;
 using Sirenix.OdinInspector;
 using UnityEngine;
+using UnityEngine.UI;
 
 namespace Guardians
 {
@@ -13,9 +14,15 @@ namespace Guardians
 		[SerializeField]
 		private int ModelId;
 
+		[SerializeField]
+		private InputField ModelChangeInputField;
+
 		[Button]
 		public void SendModelChangeRequest()
 		{
+			if(ModelChangeInputField != null)
+				ModelId = int.Parse(ModelChangeInputField.text);
+
 			this.SendService.SendMessage(new PlayerModelChangeRequestPayload(ModelId));
 		}
 	}

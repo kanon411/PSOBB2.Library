669a43fde54d78e5a1f094cbd339323d5cdb2132
HelloKitty <andrew.blakely@ymail.com>
2018-12-16 03:55:51 -0600

    Added Base and ExternalizedDependency MonoBehaviour
    The purpose of these types are for the rare ocassional MonoBehaviour
    that exists in the scene and forwarding events to an externalized
    dependency.

---
+++ src/Client/Guardians.Client.Game/Component/Base/BaseGuardiansMonoBehaviour.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Guardians
+{
+	[Injectee]
+	public abstract class BaseGuardiansMonoBehaviour : MonoBehaviour
+	{
+		[Inject]
+		protected ILog Logger { get; private set; }
+	}
+}
---
+++ src/Client/Guardians.Client.Game/Component/Base/ExternalizedDependencyMonoBehaviour.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using SceneJect.Common;
+using UnityEngine;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Base-type for <see cref="MonoBehaviour"/>'s
+	/// that interact with non-<see cref="MonoBehaviour"/> dependencies.
+	/// Mostly the <see cref="MonoBehaviour"/> acts as an object that dispatches engine events
+	/// without running any actual logic of its own.
+	/// </summary>
+	/// <typeparam name="TDependencyType">The dependency type.</typeparam>
+	[Injectee]
+	public abstract class ExternalizedDependencyMonoBehaviour<TDependencyType> : BaseGuardiansMonoBehaviour
+		where TDependencyType : class
+	{
+		/// <summary>
+		/// The external engine dependency that the MonoBehaviour interacts with.
+		/// </summary>
+		[Inject]
+		protected TDependencyType ExternalDependency { get; private set; }
+	}
+}

916680872812b6cb76086cee63518f0a745da240
HelloKitty <andrew.blakely@ymail.com>
2018-12-16 03:57:07 -0600

    Implement resource/content loadable interfaces

---
+++ src/Client/Guardians.Client.Game/ContentManagement/IPrefabContentResourceHandle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+
+namespace Guardians
+{
+	public interface IPrefabContentResourceHandle : ILoadedContentResourceHandle
+	{
+		/// <summary>
+		/// Loads a prefab <see cref="GameObject"/>
+		/// from the resource handle.
+		/// </summary>
+		/// <returns>The prefab.</returns>
+		GameObject LoadPrefab();
+
+		/// <summary>
+		/// Loads a prefab <see cref="GameObject"/>
+		/// from the resource handle async.
+		/// </summary>
+		/// <returns>An awaitable that will contain the prefab.</returns>
+		Task<GameObject> LoadPrefabAsync();
+	}
+}

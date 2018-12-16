6121d5334dca769d2c37c5113b56a5bca3a2cc50
HelloKitty <andrew.blakely@ymail.com>
2018-12-16 03:57:46 -0600

    Added default content resource manager

---
+++ src/Client/Guardians.Client.Game/ContentManagement/DefaultLoadableContentResourceManager.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Guardians
+{
+	//TODO: We should do some threading and safety stuff.
+	public sealed class DefaultLoadableContentResourceManager : ILoadableContentResourceManager
+	{
+		private IContentServerServiceClient ContentClient { get; }
+
+		private IReadonlyAuthTokenRepository AuthTokenRepo { get; }
+
+		//We should only tocuh this on the main thread, including cleanup and updating it.
+		private Dictionary<long, ReferenceCountedPrefabContentResourceHandle> ResourceHandleCache { get; }
+
+		/// <inheritdoc />
+		public DefaultLoadableContentResourceManager(
+			[NotNull] IContentServerServiceClient contentClient, 
+			[NotNull] IReadonlyAuthTokenRepository authTokenRepo)
+		{
+			ContentClient = contentClient ?? throw new ArgumentNullException(nameof(contentClient));
+			AuthTokenRepo = authTokenRepo ?? throw new ArgumentNullException(nameof(authTokenRepo));
+
+			ResourceHandleCache = new Dictionary<long, ReferenceCountedPrefabContentResourceHandle>();
+		}
+
+		/// <inheritdoc />
+		public bool IsAvatarResourceAvailable(long avatarId)
+		{
+			if(avatarId < 0) throw new ArgumentOutOfRangeException(nameof(avatarId));
+
+			return ResourceHandleCache.ContainsKey(avatarId);
+		}
+
+		/// <inheritdoc />
+		public async Task<IPrefabContentResourceHandle> LoadAvatarPrefabAsync(long avatarId)
+		{
+			//If it's already available, we can just return immediately
+			if(IsAvatarResourceAvailable(avatarId))
+				return TryLoadAvatarPrefab(avatarId);
+
+			ContentDownloadURLResponse downloadUrlResponse = await ContentClient.RequestAvatarDownloadUrl(avatarId, AuthTokenRepo.RetrieveWithType())
+				.ConfigureAwait(false);
+
+			//TODO: Handle failure
+			TaskCompletionSource<IPrefabContentResourceHandle> completionSource = new TaskCompletionSource<IPrefabContentResourceHandle>();
+
+			//Asset bundle requests can sadly only happen on the main thread, so we must join the main thread.
+			await new UnityYieldAwaitable();
+
+			//TODO: We should handle caching, versioning and etc here.
+			UnityWebRequestAsyncOperation asyncOperation = UnityWebRequestAssetBundle.GetAssetBundle(downloadUrlResponse.DownloadURL, 0).SendWebRequest();
+
+			//TODO: We should render these operations to the loading screen UI.
+			asyncOperation.completed += operation =>
+			{
+				var bundleHandle = new ReferenceCountedPrefabContentResourceHandle(DownloadHandlerAssetBundle.GetContent(asyncOperation.webRequest));
+
+				//We should cache the bundle so it can be looked up non-async and loading faster in the future
+				//then claim a reference
+				this.ResourceHandleCache[avatarId] = bundleHandle;
+				bundleHandle.ClaimReference();
+				completionSource.SetResult(bundleHandle);
+
+				//TODO: Handle failure
+			};
+
+			return await completionSource.Task
+				.ConfigureAwait(false);
+		}
+
+		/// <inheritdoc />
+		public IPrefabContentResourceHandle TryLoadAvatarPrefab(long avatarId)
+		{
+			if(!IsAvatarResourceAvailable(avatarId))
+				throw new InvalidOperationException($"Cannot load AvatarId: {avatarId} from memory. Call {nameof(LoadAvatarPrefabAsync)} if not already in memory.");
+
+			//Important to claim reference, since this is ref counted.
+			var handle = ResourceHandleCache[avatarId];
+			handle.ClaimReference();
+
+			return handle;
+		}
+	}
+}

554140664afc655e54bdc2f0527e86b93ea4c55d
HelloKitty <andrew.blakely@ymail.com>
2018-12-27 18:18:53 -0600

    Changed zoneserver to always release build

--- build-norestore-zoneserver.bat
+++ build-norestore-zoneserver.bat
@@ -1,6 +1,6 @@
-dotnet publish src/server/Guardians.Server.ZoneServer/Guardians.Server.ZoneServer.csproj -c Debug
+dotnet publish src/server/Guardians.Server.ZoneServer/Guardians.Server.ZoneServer.csproj -c Release
 
 if not exist "build\zoneserver" mkdir build\zoneserver
 
-xcopy src\server\Guardians.Server.ZoneServer\bin\Debug\netstandard2.0\publish build\zoneserver /Y /q /EXCLUDE:BuildExclude.txt
+xcopy src\server\Guardians.Server.ZoneServer\bin\Release\netstandard2.0\publish build\zoneserver /Y /q /EXCLUDE:BuildExclude.txt
 EXIT 0

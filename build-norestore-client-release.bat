9efdf0026e606a6a53c6af66cc7f92d9a254e6d1
HelloKitty <andrew.blakely@ymail.com>
2018-12-27 18:18:42 -0600

    Added client release buildsciprt

---
+++ build-norestore-client-release.bat
@@ -0,0 +1,6 @@
+dotnet publish src/Guardians.Client.All/Guardians.Client.All.csproj -c Release
+
+if not exist "build\client" mkdir build\client
+
+xcopy src\Guardians.Client.All\bin\Release\netstandard2.0\publish build\client /Y /q /EXCLUDE:BuildExclude.txt
+EXIT 0

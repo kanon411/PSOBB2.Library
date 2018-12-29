9fefdc4b0d60939438af46fa21a57d01150ce101
HelloKitty <andrew.blakely@ymail.com>
2018-12-29 13:19:17 -0600

    Temporary solution for grounding with movement predicition

--- src/Client/Guardians.Client.Game/Entity/Movement/ClientPositionChangeWithLookMovementGenerator.cs
+++ src/Client/Guardians.Client.Game/Entity/Movement/ClientPositionChangeWithLookMovementGenerator.cs
@@ -10,6 +10,8 @@ namespace Guardians
 	{
 		private Vector3 ComputedNormalizedMovementDirection;
 
+		private float lastHitHeight;
+
 		/// <inheritdoc />
 		public ClientPositionChangeWithLookMovementGenerator(PositionChangeMovementDataWithLook movementData) 
 			: base(movementData)
@@ -35,15 +37,10 @@ namespace Guardians
 			ComputedNormalizedMovementDirection = new Vector3(ComputedNormalizedMovementDirection.x, 0, ComputedNormalizedMovementDirection.z).normalized;
 		}
 
+		//TODO: This is a strange mess.
 		/// <inheritdoc />
 		protected override void InternalUpdate(GameObject entity, long currentTime)
 		{
-			//TODO: We are harcoding speed here, we shouldn't do that.
-			ProjectVersionStage.AssertAlpha();
-			//TODO: The time syncronization is not working, it's off by like 0.15 seconds for some reason.
-			//entity.transform.position = MovementData.InitialPosition + ComputedNormalizedMovementDirection * ComputeTimestampDiffSeconds(currentTime) * 3.0f;
-			entity.transform.position = MovementData.InitialPosition + (ComputedNormalizedMovementDirection * ComputeTimestampDiffSeconds(currentTime) * 3.0f);
-
 			//TODO: This is likely to be SLOW and costly. Can we avoid this?
 			NavMesh.SamplePosition(entity.transform.position, out NavMeshHit navHit, 1.0f, NavMesh.AllAreas);
 			if(navHit.hit)
@@ -51,7 +48,16 @@ namespace Guardians
 				entity.transform.position = navHit.position;
 				//Kinda a hack, but let's set the Y of the initialpos so that it stays on the navmesh while lerping
 				entity.transform.position.Set(entity.transform.position.x, navHit.position.y, entity.transform.position.z);
+				lastHitHeight = navHit.position.y;
 			}
+
+			//TODO: We are harcoding speed here, we shouldn't do that.
+			ProjectVersionStage.AssertAlpha();
+			//TODO: The time syncronization is not working, it's off by like 0.15 seconds for some reason.
+			//entity.transform.position = MovementData.InitialPosition + ComputedNormalizedMovementDirection * ComputeTimestampDiffSeconds(currentTime) * 3.0f;
+			entity.transform.position = MovementData.InitialPosition + (ComputedNormalizedMovementDirection * ComputeTimestampDiffSeconds(currentTime) * 3.0f);
+
+			entity.transform.position = new Vector3(entity.transform.position.x, lastHitHeight, entity.transform.position.z);
 		}
 
 		private float ComputeTimestampDiffSeconds(long currentTime)

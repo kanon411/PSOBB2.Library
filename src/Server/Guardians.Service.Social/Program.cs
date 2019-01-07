87d4b6e09c5f748f3ddece3bbf3220d38aa9e99e
HelloKitty <andrew.blakely@ymail.com>
2019-01-07 10:12:39 -0600

    Removed starter stuff from Social app

--- src/Server/Guardians.Service.Social/Program.cs
+++ src/Server/Guardians.Service.Social/Program.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
-using Guardians.Service.Social;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;

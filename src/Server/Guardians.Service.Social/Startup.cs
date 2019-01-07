2f6c2807e706a0deaf1cb6759ec4b8e8963a4bc7
HelloKitty <andrew.blakely@ymail.com>
2019-01-07 10:07:20 -0600

    Initially created backend social service

---
+++ src/Server/Guardians.Service.Social/Startup.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace Guardians
+{
+	public class Startup
+	{
+		public Startup(IConfiguration configuration)
+		{
+			Configuration = configuration;
+		}
+
+		public IConfiguration Configuration { get; }
+
+		// This method gets called by the runtime. Use this method to add services to the container.
+		public void ConfigureServices(IServiceCollection services)
+		{
+			services.AddMvc(options =>
+				{
+					//This prevents ASP Core from trying to validate Vector3's children, which contain Vector3 (because Unity3D thanks)
+					//so it will cause stack overflows. This will avoid it.
+					//options.ModelMetadataDetailsProviders.Add(new SuppressChildValidationMetadataProvider(typeof(Vector3)));
+				})
+				.RegisterHealthCheckController();
+
+			X509Certificate2 cert = null;
+			string certPath = "Certs/TestCert.pfx";
+
+			try
+			{
+				cert = X509Certificate2Loader.Create(certPath).Load();
+			}
+			catch(Exception e)
+			{
+				throw new System.InvalidOperationException($"Failed to load {nameof(X509Certificate2)} from Path: {certPath} \n\n StackTrace: {e.StackTrace}", e);
+			}
+
+			//This provides JwtBearer support for Authorize attribute/header
+			services.AddJwtAuthorization(cert);
+		}
+
+		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+		public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+		{
+#warning Do not deploy exceptions page into production
+			app.UseDeveloperExceptionPage();
+
+			app.UseResponseCaching();
+			app.UseAuthentication();
+
+			loggerFactory.RegisterGuardiansLogging(Configuration);
+			loggerFactory.AddDebug();
+
+			app.UseMvcWithDefaultRoute();
+		}
+	}
+}

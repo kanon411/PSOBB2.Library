f267247c0894c4371b66e0e8823f5525423ff90a
HelloKitty <andrew.blakely@ymail.com>
2019-01-08 16:48:12 -0600

    Added Base SignalR Hub Type

---
+++ src/Server/Guardians.Service.Social/Hubs/Base/BaseSignalRHub.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+
+namespace Guardians
+{
+	/// <summary>
+	/// The base controller Type for guardians SignalR <see cref="Hub"/>
+	/// </summary>
+	public abstract class BaseSignalRHub : Hub
+	{
+		/// <summary>
+		/// The logging service for the Hub.
+		/// </summary>
+		protected ILogger<BaseSignalRHub> Logger { get; }
+
+		/// <inheritdoc />
+		protected BaseSignalRHub([FromServices] ILogger<BaseSignalRHub> logger)
+		{
+			if(logger == null) throw new ArgumentNullException(nameof(logger));
+
+			Logger = logger;
+		}
+	}
+}

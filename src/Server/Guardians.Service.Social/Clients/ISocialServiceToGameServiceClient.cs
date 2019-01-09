a43dceab30307b484783feb8a412f2593fa666be
HelloKitty <andrew.blakely@ymail.com>
2019-01-08 21:32:08 -0600

    Added ISocialServiceToGameServerClient

---
+++ src/Server/Guardians.Service.Social/Clients/ISocialServiceToGameServiceClient.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Refit;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Proxy interface for ServerSelection (List) Server RPCs.
+	/// </summary>
+	[Headers("User-Agent: GuardiansBackend")]
+	public interface ISocialServiceToGameServiceClient
+	{
+		[Headers("Authorization: Bearer")] //see Refits example for tokenless auto-injection to the header.
+		[Get("/api/CharacterSession/account/{id}/data")]
+		Task<CharacterSessionDataResponse> GetCharacterSessionDataByAccount([AliasAs("id")] int accountId);
+	}
+}

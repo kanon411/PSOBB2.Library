91b965e8495356f6ec15dd6d2a57fc24d77b85fd
HelloKitty <andrew.blakely@ymail.com>
2019-01-08 21:05:54 -0600

    Added genserverr to CharSessioNDataResponseCode

--- src/Guardians.Models.GameServer/ResultCodes/CharacterSessionDataResponseCode.cs
+++ src/Guardians.Models.GameServer/ResultCodes/CharacterSessionDataResponseCode.cs
@@ -6,6 +6,11 @@
 
 		NoSessionAvailable = 2,
 
-		Unauthorized = 3
+		Unauthorized = 3,
+
+		/// <summary>
+		/// Potentially represents an error on the gameserver.
+		/// </summary>
+		GeneralServerError = 4,
 	}
 }

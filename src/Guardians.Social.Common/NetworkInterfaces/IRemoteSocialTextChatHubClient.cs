0d48eff56c42d49f8dd4e43110dfc8a8070fc6ec
HelloKitty <andrew.blakely@ymail.com>
2019-01-09 21:02:25 -0600

    Added IRemoteSocialTextChatHubClient

---
+++ src/Guardians.Social.Common/NetworkInterfaces/IRemoteSocialTextChatHubClient.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Guardians
+{
+	public interface IRemoteSocialTextChatHubClient
+	{
+		/// <summary>
+		/// Async method that handles recieveing a <see cref="TargetlessChannelChatMessageEventModel"/>
+		/// (a zone text chat message) with information about the sender.
+		/// </summary>
+		/// <param name="message">The message to send.</param>
+		/// <returns>Awaitable.</returns>
+		Task RecieveZoneChannelTextChatMessageAsync(TargetlessChannelChatMessageEventModel message);
+	}
+}

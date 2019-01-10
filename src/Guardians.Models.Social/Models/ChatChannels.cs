64650b736ef07c5d9401b0149cbebd4a2acff6da
HelloKitty <andrew.blakely@ymail.com>
2019-01-09 20:01:16 -0600

    Added chat channels enum

---
+++ src/Guardians.Models.Social/Models/ChatChannels.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Enumeration of chat channel types.
+	/// </summary>
+	public enum ChatChannels
+	{
+		/// <summary>
+		/// Internal messaging.
+		/// </summary>
+		Internal = 0,
+
+		/// <summary>
+		/// The chat channel for zone chat.
+		/// </summary>
+		ZoneChannel = 1,
+	}
+}

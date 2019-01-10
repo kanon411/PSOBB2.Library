8d52cd12e777689ce13cd26d84e374436f77f92c
HelloKitty <andrew.blakely@ymail.com>
2019-01-09 20:57:16 -0600

    Added base TargetlessChannelChatMessageEvent Model

---
+++ src/Guardians.Models.Social/Models/TargetlessChannelChatMessageEventModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Data model that represents an enveloped <see cref="TargetlessChannelChatMessageRequestModel"/>
+	/// that is associated with the <see cref="NetworkEntityGuid"/> of the message sender.
+	/// </summary>
+	[JsonObject]
+	public sealed class TargetlessChannelChatMessageEventModel
+	{
+		/// <summary>
+		/// The entity associated <see cref="TargetlessChannelChatMessageRequestModel"/>.
+		/// The <see cref="NetworkEntityGuid"/> represents the original sender of the message.
+		/// </summary>
+		[JsonProperty]
+		public EntityAssociatedData<TargetlessChannelChatMessageRequestModel> ChannelMessage { get; private set; }
+
+		/// <inheritdoc />
+		public TargetlessChannelChatMessageEventModel([JetBrains.Annotations.NotNull] EntityAssociatedData<TargetlessChannelChatMessageRequestModel> channelMessage)
+		{
+			ChannelMessage = channelMessage ?? throw new ArgumentNullException(nameof(channelMessage));
+		}
+
+		/// <summary>
+		/// Serializer ctor.
+		/// </summary>
+		private TargetlessChannelChatMessageEventModel()
+		{
+			
+		}
+	}
+}

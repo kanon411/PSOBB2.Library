b2e8014d33a2d6e19f95ab6b98710e9dda595d80
HelloKitty <andrew.blakely@ymail.com>
2019-01-10 19:28:33 -0600

    Rewrote SocialConnectionGameInitializable to handle connection

--- src/Client/Guardians.Client.Game/Initializables/SocialConnectionGameInitializable.cs
+++ src/Client/Guardians.Client.Game/Initializables/SocialConnectionGameInitializable.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.DependencyInjection;
 
 namespace Guardians
 {
@@ -10,20 +11,41 @@ namespace Guardians
 	[GameInitializableSceneSpecification(GameInitializableSceneSpecificationAttribute.SceneType.ZoneGameScene)]
 	public sealed class SocialConnectionGameInitializable : IGameInitializable
 	{
-		private HubConnection SocialConnection { get; }
+		private IEnumerable<ISignalRConnectionHubInitializable> InitializableSocialServices { get; }
+
+		private IServiceDiscoveryService ServiceDiscoveryService { get; }
 
 		/// <inheritdoc />
-		public SocialConnectionGameInitializable([NotNull] HubConnection socialConnection)
+		public SocialConnectionGameInitializable(
+			[NotNull] IEnumerable<ISignalRConnectionHubInitializable> initializableSocialServices,
+			[NotNull] IServiceDiscoveryService serviceDiscoveryService)
 		{
-			SocialConnection = socialConnection ?? throw new ArgumentNullException(nameof(socialConnection));
+			InitializableSocialServices = initializableSocialServices ?? throw new ArgumentNullException(nameof(initializableSocialServices));
+			ServiceDiscoveryService = serviceDiscoveryService ?? throw new ArgumentNullException(nameof(serviceDiscoveryService));
 		}
 
 		/// <inheritdoc />
-		public Task OnGameInitialized()
+		public async Task OnGameInitialized()
 		{
+			//We need to connect the hub to the social backend
+			ResolveServiceEndpointResponse endpointResponse = await ServiceDiscoveryService.DiscoverService(new ResolveServiceEndpointRequest(ClientRegionLocale.US, "SocialService"))
+				.ConfigureAwait(false);
+
+			//TODO: Handle failed service disc query
+			HubConnection connection = new HubConnectionBuilder()
+				.WithUrl($"http://{endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort}/realtime/textchat")
+				.AddJsonProtocol()
+				.Build();
+
+			foreach(var i in InitializableSocialServices)
+			{
+				i.Connection = connection;
+			}
+
 			//Just start the service when the game initializes
 			//This will make it so that the signalR clients will start to recieve messages.
-			return SocialConnection.StartAsync();
+			await connection.StartAsync()
+				.ConfigureAwait(false);
 		}
 	}
 }

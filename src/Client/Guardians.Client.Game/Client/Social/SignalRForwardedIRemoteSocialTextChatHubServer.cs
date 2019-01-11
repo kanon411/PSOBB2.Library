bd0948c50361b93a3498091dff7c8eb8e75c7b80
HelloKitty <andrew.blakely@ymail.com>
2019-01-11 03:08:30 -0600

    Created implementation for TextChatHub server RPC interface

---
+++ src/Client/Guardians.Client.Game/Client/Social/SignalRForwardedIRemoteSocialTextChatHubServer.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Guardians
+{
+	public sealed class SignalRForwardedIRemoteSocialTextChatHubServer : IRemoteSocialTextChatHubClient, ISignalRConnectionHubInitializable
+	{
+		private HubConnection _connection;
+
+		/// <inheritdoc />
+		public HubConnection Connection
+		{
+			get => _connection;
+			set
+			{
+				//TODO: What is this is being nulled out??
+				//This part is pretty important, we want this to be the reciever of the callbacks.
+				value.RegisterClientInterface(this);
+				_connection = value;
+			}
+		}
+
+		private INameQueryService NameQueryable { get; }
+
+		/// <inheritdoc />
+		public SignalRForwardedIRemoteSocialTextChatHubServer([NotNull] INameQueryService nameQueryable)
+		{
+			NameQueryable = nameQueryable ?? throw new ArgumentNullException(nameof(nameQueryable));
+		}
+
+		public async Task RecieveZoneChannelTextChatMessageAsync(ZoneChatMessageEventModel message)
+		{
+			string entityName = await NameQueryable.RetrieveAsync(message.ChannelMessage.EntityGuid.EntityId)
+				.ConfigureAwait(false);
+
+			//TODO: Would performance be better if the server did this?
+			string renderableMessage = $"[1. Zone] {entityName}: {message.ChannelMessage.Data.Message}";
+		}
+	}
+}

e26ab01eab077748904bf8d2b18e39dc86de26b7
HelloKitty <andrew.blakely@ymail.com>
2019-01-10 19:29:06 -0600

    Added SignalRForwardedIRemoteSocialTextChatHubClient

---
+++ src/Client/Guardians.Client.Game/Client/Social/SignalRForwardedIRemoteSocialTextChatHubClient.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace Guardians
+{
+	public sealed class SignalRForwardedIRemoteSocialTextChatHubClient : IRemoteSocialTextChatHubServer, ISignalRConnectionHubInitializable
+	{
+		//Externally initializable
+		[UsedImplicitly]
+		public HubConnection Connection { get; set; }
+
+		/// <inheritdoc />
+		public Task SendZoneChannelTextChatMessageAsync(ZoneChatMessageRequestModel message)
+		{
+			return Connection.SendAsync(nameof(SendZoneChannelTextChatMessageAsync), message);
+		}
+	}
+}

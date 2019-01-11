7ec666227fa782b38834154f2a84be9d42a06d99
HelloKitty <andrew.blakely@ymail.com>
2019-01-10 20:09:55 -0600

    Added Auth/CharacterHeader to SiginalR Connection in SocialInit

--- src/Client/Guardians.Client.Game/Initializables/SocialConnectionGameInitializable.cs
+++ src/Client/Guardians.Client.Game/Initializables/SocialConnectionGameInitializable.cs
@@ -15,13 +15,21 @@ namespace Guardians
 
 		private IServiceDiscoveryService ServiceDiscoveryService { get; }
 
+		private ILocalPlayerDetails PlayerDetails { get; }
+
+		private IReadonlyAuthTokenRepository AuthTokenProvider { get; }
+
 		/// <inheritdoc />
 		public SocialConnectionGameInitializable(
 			[NotNull] IEnumerable<ISignalRConnectionHubInitializable> initializableSocialServices,
-			[NotNull] IServiceDiscoveryService serviceDiscoveryService)
+			[NotNull] IServiceDiscoveryService serviceDiscoveryService,
+			[NotNull] ILocalPlayerDetails playerDetails,
+			[NotNull] IReadonlyAuthTokenRepository authTokenProvider)
 		{
 			InitializableSocialServices = initializableSocialServices ?? throw new ArgumentNullException(nameof(initializableSocialServices));
 			ServiceDiscoveryService = serviceDiscoveryService ?? throw new ArgumentNullException(nameof(serviceDiscoveryService));
+			PlayerDetails = playerDetails ?? throw new ArgumentNullException(nameof(playerDetails));
+			AuthTokenProvider = authTokenProvider ?? throw new ArgumentNullException(nameof(authTokenProvider));
 		}
 
 		/// <inheritdoc />
@@ -31,9 +39,16 @@ namespace Guardians
 			ResolveServiceEndpointResponse endpointResponse = await ServiceDiscoveryService.DiscoverService(new ResolveServiceEndpointRequest(ClientRegionLocale.US, "SocialService"))
 				.ConfigureAwait(false);
 
+			if(!endpointResponse.isSuccessful)
+				throw new InvalidOperationException($"Failed to query for SocialService. Reason: {endpointResponse.ResultCode}");
+
 			//TODO: Handle failed service disc query
 			HubConnection connection = new HubConnectionBuilder()
-				.WithUrl($"http://{endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort}/realtime/textchat")
+				.WithUrl($@"http://{endpointResponse.Endpoint.EndpointAddress}:{endpointResponse.Endpoint.EndpointPort}/realtime/textchat", options =>
+					{
+						options.Headers.Add(SocialNetworkConstants.CharacterIdRequestHeaderName, PlayerDetails.LocalPlayerGuid.EntityId.ToString());
+						options.AccessTokenProvider = () => Task.FromResult(AuthTokenProvider.Retrieve());
+					})
 				.AddJsonProtocol()
 				.Build();
 

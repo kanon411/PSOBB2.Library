d0840c3b8d5fc6cd14221ad2858239c0f6672a47
HelloKitty <andrew.blakely@ymail.com>
2019-01-11 19:24:58 -0600

    Added ITextChatEventFactory

---
+++ src/Client/Guardians.Client.Game/Factory/ITextChatEventFactory.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Guardians
+{
+	public interface ITextChatEventFactory
+	{
+		TextChatEventData CreateChatData<TMessageType>(EntityAssociatedData<TMessageType> incomingChatMessageEventData)
+			where TMessageType : ITextMessageContainable;
+
+		TextChatEventData CreateChatData<TMessageType>(TMessageType incomingChatMessageEventData)
+			where TMessageType : ITextMessageContainable;
+	}
+}

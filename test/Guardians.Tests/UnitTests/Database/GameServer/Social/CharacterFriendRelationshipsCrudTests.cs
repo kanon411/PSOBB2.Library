00f9db7ae90ddc1ff5b0c7cdf16730029a9f5b0d
HelloKitty <andrew.blakely@ymail.com>
2019-01-13 00:33:17 -0600

    Added generic tests for CharacterFriendRelationships repo

---
+++ test/Guardians.Tests/UnitTests/Database/GameServer/Social/CharacterFriendRelationshipsCrudTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
+using NUnit.Framework;
+
+namespace Guardians
+{
+	/// <summary>
+	/// Runs all the crud repo interface default tests against the character repository.
+	/// </summary>
+	[TestFixture]
+	public class CharacterFriendRelationshipsCrudTests : GenericCrubRepositoryDefaultTests<CharacterDatabaseContext, DatabaseBackedCharacterFriendsRepository, int, CharacterFriendRelationshipModel>
+	{
+		public override IEnumerable<int> TestCaseKeys => new int[] { 1, 2, 3, 5, 6, 7, 8, 9, 22, 6666, short.MaxValue, int.MaxValue };
+
+		/// <inheritdoc />
+		public override CharacterFriendRelationshipModel BuildRandomModel(bool generateKey)
+		{
+			Random random = new Random((int)DateTime.UtcNow.Ticks);
+
+			return new CharacterFriendRelationshipModel(random.Next(), random.Next());
+		}
+
+		/// <inheritdoc />
+		public override int ProduceKeyFromModel(CharacterFriendRelationshipModel model)
+		{
+			return model.FriendshipRelationshipId;
+		}
+	}
+}

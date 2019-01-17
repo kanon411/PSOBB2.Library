23ff749d3c9300a11ed84810c1b9d67e250f0d11
HelloKitty <andrew.blakely@ymail.com>
2019-01-16 21:02:30 -0600

    Added guild entry database migration

--- src/Server/Guardians.Database.GameServer/Migrations/20190117023634_AddedGuildEntryMigration.Designer.cs
+++ src/Server/Guardians.Database.GameServer/Migrations/20190117023634_AddedGuildEntryMigration.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using Guardians;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Guardians.Database.GameServer.Migrations
 {
     [DbContext(typeof(CharacterDatabaseContext))]
-    partial class CharacterDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20190117023634_AddedGuildEntryMigration")]
+    partial class AddedGuildEntryMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

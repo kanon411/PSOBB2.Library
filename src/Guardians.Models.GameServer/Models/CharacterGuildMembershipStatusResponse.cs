adf72a504d6d02ea3348a9eebebdc43dfe776b9c
HelloKitty <andrew.blakely@ymail.com>
2019-01-21 07:29:14 -0600

    Rename CharacterGuildStatusResponse to CharacterGuildMembershipStatusResponse

--- src/Guardians.Models.GameServer/Models/CharacterGuildMembershipStatusResponse.cs
+++ src/Guardians.Models.GameServer/Models/CharacterGuildMembershipStatusResponse.cs
@@ -5,45 +5,45 @@ using Newtonsoft.Json;
 
 namespace Guardians
 {
-	public sealed class CharacterGuildStatusResponse : IResponseModel<CharacterGuildStatusResponseCode>, ISucceedable
+	public sealed class CharacterGuildMembershipStatusResponse : IResponseModel<CharacterGuildMembershipStatusResponseCode>, ISucceedable
 	{
 		/// <inheritdoc />
 		[JsonProperty]
-		public CharacterGuildStatusResponseCode ResultCode { get; private set; }
+		public CharacterGuildMembershipStatusResponseCode ResultCode { get; private set; }
 
 		/// <inheritdoc />
 		[JsonIgnore]
-		public bool isSuccessful => ResultCode == CharacterGuildStatusResponseCode.Success;
+		public bool isSuccessful => ResultCode == CharacterGuildMembershipStatusResponseCode.Success;
 
 		[JsonProperty]
 		public int GuildId { get; private set; }
 
 		/// <inheritdoc />
-		public CharacterGuildStatusResponse(CharacterGuildStatusResponseCode resultCode)
+		public CharacterGuildMembershipStatusResponse(CharacterGuildMembershipStatusResponseCode resultCode)
 		{
 			//TODO: Verify that it's not success.
 			ResultCode = resultCode;
 		}
 
 		/// <inheritdoc />
-		public CharacterGuildStatusResponse(int guildId)
+		public CharacterGuildMembershipStatusResponse(int guildId)
 		{
 			if(guildId <= 0) throw new ArgumentOutOfRangeException(nameof(guildId), "Should use other ctor if a guild does not exist for the character.");
 
-			ResultCode = CharacterGuildStatusResponseCode.Success;
+			ResultCode = CharacterGuildMembershipStatusResponseCode.Success;
 			GuildId = guildId;
 		}
 
 		/// <summary>
 		/// Serializer ctor.
 		/// </summary>
-		private CharacterGuildStatusResponse()
+		private CharacterGuildMembershipStatusResponse()
 		{
 			
 		}
 	}
 
-	public enum CharacterGuildStatusResponseCode
+	public enum CharacterGuildMembershipStatusResponseCode
 	{
 		/// <summary>
 		/// Indicates the creation was successful.

36afe689975a45d8ef235c8af73ea2c6db265737
HelloKitty <andrew.blakely@ymail.com>
2019-01-21 07:29:50 -0600

    Added GuildController to GameServer

---
+++ src/Server/Guardians.Service.GameServer/Controllers/GuildController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Guardians
+{
+	[Route("api/[controller]")]
+	public sealed class GuildController : AuthorizationReadyController
+	{
+		/// <inheritdoc />
+		public GuildController(IClaimsPrincipalReader claimsReader, ILogger<AuthorizationReadyController> logger) 
+			: base(claimsReader, logger)
+		{
+
+		}
+	}
+}

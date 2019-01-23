b3debaef072cf6321a0ed3824f44fb6cd4d61318
HelloKitty <andrew.blakely@ymail.com>
2019-01-23 12:53:27 -0600

    Added HubClient method for GuildChatMessageEventModel

--- src/Guardians.Social.Common/NetworkInterfaces/IRemoteSocialTextChatHubClient.cs
+++ src/Guardians.Social.Common/NetworkInterfaces/IRemoteSocialTextChatHubClient.cs
@@ -14,5 +14,13 @@ namespace Guardians
 		/// <param name="message">The message to send.</param>
 		/// <returns>Awaitable.</returns>
 		Task RecieveZoneChannelTextChatMessageAsync(ZoneChatMessageEventModel message);
+
+		/// <summary>
+		/// Async method that handles recieveing a <see cref="GuildChatMessageEventModel"/>
+		/// (a guild text chat message) with information about the sender.
+		/// </summary>
+		/// <param name="message">The message to send.</param>
+		/// <returns>Awaitable.</returns>
+		Task RecieveGuildChannelTextChatMessageAsync(GuildChatMessageEventModel message);
 	}
 }

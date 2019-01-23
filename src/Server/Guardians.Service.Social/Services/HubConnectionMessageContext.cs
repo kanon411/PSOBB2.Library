a78f9cc16fb73596db607ade91e4ab7efa63db0b
HelloKitty <andrew.blakely@ymail.com>
2019-01-23 08:22:47 -0600

    Added new PeerMessageContext for SignalR/Social

---
+++ src/Server/Guardians.Service.Social/Services/HubConnectionMessageContext.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using GladNet;
+using Microsoft.AspNetCore.SignalR;
+
+namespace Guardians
+{
+	public sealed class HubConnectionMessageContext : IPeerMessageContext<object>
+	{
+		/// <inheritdoc />
+		public IConnectionService ConnectionService { get; }
+
+		/// <inheritdoc />
+		public IPeerPayloadSendService<object> PayloadSendService { get; }
+
+		/// <inheritdoc />
+		public IPeerRequestSendService<object> RequestSendService { get; }
+
+		public IGroupManager Groups { get; }
+
+		/// <inheritdoc />
+		public HubConnectionMessageContext(
+			[JetBrains.Annotations.NotNull] IConnectionService connectionService,
+			[JetBrains.Annotations.NotNull] IPeerPayloadSendService<object> payloadSendService,
+			[JetBrains.Annotations.NotNull] IPeerRequestSendService<object> requestSendService,
+			[JetBrains.Annotations.NotNull] IGroupManager groups)
+		{
+			ConnectionService = connectionService ?? throw new ArgumentNullException(nameof(connectionService));
+			PayloadSendService = payloadSendService ?? throw new ArgumentNullException(nameof(payloadSendService));
+			RequestSendService = requestSendService ?? throw new ArgumentNullException(nameof(requestSendService));
+			Groups = groups ?? throw new ArgumentNullException(nameof(groups));
+		}
+	}
+}

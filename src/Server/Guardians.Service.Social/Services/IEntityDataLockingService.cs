e646a01112b245aee145c50b027fb6dda21ccc34
HelloKitty <andrew.blakely@ymail.com>
2019-01-25 11:07:25 -0600

    Added IEntityDataLockingService

---
+++ src/Server/Guardians.Service.Social/Services/IEntityDataLockingService.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Guardians
+{
+	public interface IEntityDataLockingService
+	{
+		/// <summary>
+		/// Releases one interest unit for the provided
+		/// entity guid <see cref="guid"/>.
+		/// </summary>
+		/// <param name="guid">The guid to release interest for.</param>
+		/// <returns></returns>
+		Task ReleaseEntityInterestAsync(NetworkEntityGuid guid);
+
+		/// <summary>
+		/// Aquires an increase of one interest unit for the provided
+		/// entity guid <see cref="guid"/>.
+		/// </summary>
+		/// <param name="guid">The guid to increase interest for.</param>
+		/// <returns></returns>
+		Task RegisterEntityInterestAsync(NetworkEntityGuid guid);
+
+		/// <summary>
+		/// Indicates if the entity data locking service
+		/// has a registeration for the provided entity <see cref="guid"/>.
+		/// </summary>
+		/// <param name="guid"></param>
+		/// <returns>True if a lock service is available for the entity.</returns>
+		Task<bool> ContainsLockingServiceForAsync(NetworkEntityGuid guid);
+
+		/// <summary>
+		/// Attempts to aquire a lock for the provided <see cref="guid"/> <see cref="NetworkEntityGuid"/>.
+		/// This is not object instance based. It is on internal value basis.
+		/// </summary>
+		/// <param name="guid">The Entity to lock for.</param>
+		/// <returns>An awaitable and dispoable async lock object.</returns>
+		Task<IDisposable> AquireEntityLockAsync(NetworkEntityGuid guid);
+	}
+}

e8c14def6e9c8319662f6c3446b63658e02e6f56
HelloKitty <andrew.blakely@ymail.com>
2019-01-25 13:18:49 -0600

    Rewrite OnHubConnection for guilds to assume potential guild data exists

--- src/Server/Guardians.Service.Social/Services/ConnectionEventListeners/CharacterGuildOnHubConnectionEventListener.cs
+++ src/Server/Guardians.Service.Social/Services/ConnectionEventListeners/CharacterGuildOnHubConnectionEventListener.cs
@@ -33,39 +33,74 @@ namespace Guardians
 			//TODO: Verify that the character they requested is owned by them.
 			ProjectVersionStage.AssertAlpha();
 
-			CharacterGuildMembershipStatusResponse response = null;
-
-			try
-			{
-				response = await SocialToGameClient.GetCharacterMembershipGuildStatus(int.Parse(hubConnectedTo.Context.UserIdentifier))
-					.ConfigureAwait(false);
-			}
-			catch(Exception e)
-			{
-				if(Logger.IsEnabled(LogLevel.Error))
-					Logger.LogError($"Failed to get guild status of Connection: {hubConnectedTo.Context.UserIdentifier}. Exception: {e.Message}\n\nStack:{e.StackTrace}");
-
-				return HubOnConnectionState.Abort;
-			}
-
 			NetworkEntityGuid guid = new NetworkEntityGuidBuilder()
 				.WithId(int.Parse(hubConnectedTo.Context.UserIdentifier))
 				.WithType(EntityType.Player)
 				.Build();
 
-			//TODO: We don't really want to directly expose this
-			GuildStatusMappable.Add(guid, response);
-
-			if(response.isSuccessful)
+			//We may already be able to register.
+			if(await TryRegisterGuildStatus(guid, hubConnectedTo.Groups, hubConnectedTo.Context.ConnectionId).ConfigureAwait(false) == HubOnConnectionState.Success)
 			{
-				//TODO: don't hardcode
-				await hubConnectedTo.Groups.AddToGroupAsync(hubConnectedTo.Context.ConnectionId, $"guild:{response.GuildId}")
+				return HubOnConnectionState.Success;
+			}
+
+			HubOnConnectionState state = await TryRequestCharacterGuildStatus(guid, hubConnectedTo.Context.UserIdentifier)
+				.ConfigureAwait(false);
+
+			if(state == HubOnConnectionState.Success)
+				return await TryRegisterGuildStatus(guid, hubConnectedTo.Groups, hubConnectedTo.Context.ConnectionId)
+					.ConfigureAwait(false);
+
+			//Just error, we don't need to abort. Something didn't work right though.
+			return HubOnConnectionState.Error;
+		}
+
+		private async Task<HubOnConnectionState> TryRegisterGuildStatus(NetworkEntityGuid guid, IGroupManager groups, string connectionId)
+		{
+			//It's possible we already maintain guild information for this entity
+			//This can happen if multiple connections share an entity.
+			if(GuildStatusMappable.ContainsKey(guid))
+			{
+				await RegisterGuildOnExistingResponse(guid, groups, connectionId)
+					.ConfigureAwait(false);
+
+				return HubOnConnectionState.Success;
+			}
+
+			return HubOnConnectionState.Error;
+		}
+
+		private async Task<HubOnConnectionState> TryRequestCharacterGuildStatus(NetworkEntityGuid guid, string userIdentifier)
+		{
+			CharacterGuildMembershipStatusResponse response = null;
+
+			try
+			{
+				response = await SocialToGameClient.GetCharacterMembershipGuildStatus(int.Parse(userIdentifier))
 					.ConfigureAwait(false);
 			}
-			else
-				return HubOnConnectionState.Error;
+			catch(Exception e)
+			{
+				if(Logger.IsEnabled(LogLevel.Error))
+					Logger.LogError($"Failed to get guild status of Connection: {userIdentifier}. Exception: {e.Message}\n\nStack:{e.StackTrace}");
 
+				return HubOnConnectionState.Abort;
+			}
+
+			//Don't ADD, we have to assume that we might have an entity, maybe web or mobile, already connected
+			//and merely just update the guild status
+			GuildStatusMappable.Add(guid, response);
 			return HubOnConnectionState.Success;
+		}
+
+		private async Task RegisterGuildOnExistingResponse(NetworkEntityGuid guid, IGroupManager groupManager, string connectionId)
+		{
+			if(GuildStatusMappable[guid].isSuccessful)
+			{
+				//TODO: don't hardcode
+				await groupManager.AddToGroupAsync(connectionId, $"guild:{GuildStatusMappable[guid].GuildId}")
+					.ConfigureAwait(false);
+			}
 		}
 	}
 }

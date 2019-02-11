8420780e45c818ccf93349364da0a87266f15e5d
HelloKitty <andrew.blakely@ymail.com>
2019-02-11 04:26:20 -0600

    Removed uneeded build scripts

--- build-norestore-voice.bat
+++
@@ -1,8 +0,0 @@
-dotnet publish src/Dissonance/Dissonance.GladNet.Client/Dissonance.GladNet.Client.csproj -c Debug
-dotnet publish src/Dissonance/Dissonance.Editor/Dissonance.Editor.csproj -c Debug
-
-if not exist "build\voice" mkdir build\voice
-
-xcopy src\Dissonance\Dissonance.GladNet.Client\bin\Debug\netstandard2.0\publish build\voice /Y /q /EXCLUDE:BuildExclude.txt
-xcopy src\Dissonance\Dissonance.GladNet.Editor\bin\Debug\netstandard2.0\publish build\voice /Y /q /EXCLUDE:BuildExclude.txt
-EXIT 0

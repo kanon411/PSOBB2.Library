826c4df08b440011dfcc7670f1b9f0eb5430ec9f
HelloKitty <andrew.blakely@ymail.com>
2019-02-12 00:07:22 -0600

    Added ICharacterSelectionEntryDataChangeEventSubscribable

---
+++ src/Client/PSOBB.Client.Game/Engine/CharacterSelection/Events/ICharacterSelectionEntryDataChangeEventSubscribable.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSOBB
+{
+	public interface ICharacterSelectionEntryDataChangeEventSubscribable
+	{
+		event EventHandler<CharacterSelectionEntryDataChangeEventArgs> OnCharacterSelectionEntryChanged;
+	}
+
+	public sealed class CharacterSelectionEntryDataChangeEventArgs : EventArgs
+	{
+		public NetworkEntityGuid CharacterEntityGuid { get; }
+
+		/// <inheritdoc />
+		public CharacterSelectionEntryDataChangeEventArgs([NotNull] NetworkEntityGuid characterEntityGuid)
+		{
+			CharacterEntityGuid = characterEntityGuid ?? throw new ArgumentNullException(nameof(characterEntityGuid));
+		}
+	}
+}

10c42c3545ab4e681f4670c4d82477b5dd6ad2cf
HelloKitty <andrew.blakely@ymail.com>
2019-02-11 21:51:33 -0600

    Added CommonGameDependencyRegisteration

---
+++ src/Client/PSOBB.Client.Game/IoC/CommonGameDependencyRegisteration.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PSOBB.Client
+{
+	public sealed class CommonGameDependencyRegisteration : AutofacBasedDependencyRegister<CommonGameDependencyModule>
+	{
+		[SerializeField]
+		public GameSceneType SceneType;
+
+		/// <inheritdoc />
+		protected override CommonGameDependencyModule CreateModule()
+		{
+			return new CommonGameDependencyModule(SceneType);
+		}
+	}
+}

29da43215ec1907df48bac667182f86612274fde
HelloKitty <andrew.blakely@ymail.com>
2019-02-11 18:19:49 -0600

    Fix remaining proj/build issues

--- build.bat
+++ build.bat
@@ -3,5 +3,4 @@ if not exist "build" mkdir build
 start cmd /c build-norestore-client.bat
 start cmd /c build-norestore-zoneserver.bat
 start cmd /c build-norestore-backend.bat
-start cmd /c build-norestore-sdk.bat
 EXIT 0

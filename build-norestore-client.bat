65a92736b1bf9dd3e48565f9acbb5953d2c193d5
HelloKitty <andrew.blakely@ymail.com>
2019-02-11 20:46:45 -0600

    Fix client build with copy scripts

--- build-norestore-client.bat
+++ build-norestore-client.bat
@@ -3,4 +3,3 @@ dotnet publish src/PSOBB.Client.All/PSOBB.Client.All.csproj -c Debug
 if not exist "build\client" mkdir build\client
 
 xcopy src\PSOBB.Client.All\bin\Debug\netstandard2.0\publish build\client /Y /q /EXCLUDE:BuildExclude.txt
-EXIT 0

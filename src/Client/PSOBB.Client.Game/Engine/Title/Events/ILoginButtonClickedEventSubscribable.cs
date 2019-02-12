f344eaffda3283f541122fd978015d83fde2d056
HelloKitty <andrew.blakely@ymail.com>
2019-02-11 23:26:26 -0600

    Added ILoginButtonClickedEventSubscribable

---
+++ src/Client/PSOBB.Client.Game/Engine/Title/Events/ILoginButtonClickedEventSubscribable.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSOBB.Client
+{
+	public interface ILoginButtonClickedEventSubscribable
+	{
+		event EventHandler OnLoginButtonClicked;
+	}
+}

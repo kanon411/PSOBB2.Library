26163026bc66c850c507d091545d1b17ee03001f
HelloKitty <andrew.blakely@ymail.com>
2019-02-12 04:00:18 -0600

    Added LoadNewSceneEventPayloadHandler

---
+++ src/Client/PSOBB.Client.Game/Handlers/LoadNewSceneEventPayloadHandler.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Logging;
+using GladNet;
+
+namespace PSOBB
+{
+	[AdditionalRegisterationAs(typeof(IServerRequestedSceneChangeEventSubscribable))]
+	[SceneTypeCreate(GameSceneType.PreZoneBurstingScreen)]
+	public sealed class LoadNewSceneEventPayloadHandler : BaseZoneClientGameMessageHandler<LoadNewSceneEventPayload>, IServerRequestedSceneChangeEventSubscribable
+	{
+		/// <inheritdoc />
+		public event EventHandler<ServerRequestedSceneChangeEventArgs> OnServerRequestedSceneChange;
+
+		/// <inheritdoc />
+		public LoadNewSceneEventPayloadHandler(ILog logger) 
+			: base(logger)
+		{
+
+		}
+
+		/// <inheritdoc />
+		public override Task HandleMessage(IPeerMessageContext<GameClientPacketPayload> context, LoadNewSceneEventPayload payload)
+		{
+			if(Logger.IsInfoEnabled)
+				Logger.Info($"InstanceServer requested SceneLoad: {payload.SceneToLoad}");
+
+			//Just dispatch it to any interested parties.
+			OnServerRequestedSceneChange?.Invoke(this, new ServerRequestedSceneChangeEventArgs(payload.SceneToLoad));
+
+			return Task.CompletedTask;
+		}
+	}
+}

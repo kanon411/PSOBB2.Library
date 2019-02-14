783644aeab1c312bb32bf991dd70e99c447810b4
HelloKitty <andrew.blakely@ymail.com>
2019-02-14 03:30:46 -0600

    Added DefaultLobby to GameSceneType

--- src/PSOBB.Unity.Common/Attributes/GameSceneType.cs
+++ src/PSOBB.Unity.Common/Attributes/GameSceneType.cs
@@ -1,4 +1,6 @@
-﻿namespace PSOBB
+﻿using System;
+
+namespace PSOBB
 {
 	/// <summary>
 	/// Enumeration of scene types for a game initializable.
@@ -7,13 +9,21 @@
 	{
 		TitleScreen = 0,
 
+		//TODO: Remove this at some point. 
+		[Obsolete("ZoneGameScene isn't a thing anymore. In PSOBB there are lobby and there are game. Maybe other types too.")]
 		ZoneGameScene = 1,
 		
 		CharacterSelection = 2,
 
 		//TODO: Remove this at some point.
+		[Obsolete("WorldDownloadingScreen does not exist in PSOBB. See PreZoneBurstingScreen instead.")]
 		WorldDownloadingScreen = 3,
 
 		PreZoneBurstingScreen = 4,
+
+		/// <summary>
+		/// The default lobby type for PSOBB.
+		/// </summary>
+		DefaultLobby = 5,
 	}
 }

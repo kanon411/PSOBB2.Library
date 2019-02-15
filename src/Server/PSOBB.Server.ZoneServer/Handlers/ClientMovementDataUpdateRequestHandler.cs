20db19d8915e379a97a5354b629bcc4c65b7a6bd
HelloKitty <andrew.blakely@ymail.com>
2019-02-15 09:59:35 -0600

    Rewrote ClientMovementDataUpdateRequestHandler to use movement generator

--- src/Server/PSOBB.Server.ZoneServer/Handlers/ClientMovementDataUpdateRequestHandler.cs
+++ src/Server/PSOBB.Server.ZoneServer/Handlers/ClientMovementDataUpdateRequestHandler.cs
@@ -13,41 +13,27 @@ namespace PSOBB
 	{
 		private IEntityGuidMappable<IMovementData> MovementDataMap { get; }
 
-		private IReadonlyEntityGuidMappable<GameObject> WorldEntities { get; }
+		private IEntityGuidMappable<IMovementGenerator<GameObject>> MovementGenerator { get; }
 
 		/// <inheritdoc />
 		public ClientMovementDataUpdateRequestHandler(
 			[NotNull] ILog logger, 
 			[NotNull] IReadonlyConnectionEntityCollection connectionIdToEntityMap, 
 			[NotNull] IEntityGuidMappable<IMovementData> movementDataMap,
-			[NotNull] IReadonlyEntityGuidMappable<GameObject> worldEntities,
-			IContextualResourceLockingPolicy<NetworkEntityGuid> lockingPolicy) 
+			IContextualResourceLockingPolicy<NetworkEntityGuid> lockingPolicy,
+			[NotNull] IEntityGuidMappable<IMovementGenerator<GameObject>> movementGenerator) 
 			: base(logger, connectionIdToEntityMap, lockingPolicy)
 		{
 			MovementDataMap = movementDataMap ?? throw new ArgumentNullException(nameof(movementDataMap));
-			WorldEntities = worldEntities ?? throw new ArgumentNullException(nameof(worldEntities));
+			MovementGenerator = movementGenerator ?? throw new ArgumentNullException(nameof(movementGenerator));
 		}
 
 		/// <inheritdoc />
 		protected override async Task HandleMessage(IPeerSessionMessageContext<GameServerPacketPayload> context, ClientMovementDataUpdateRequest payload, NetworkEntityGuid guid)
 		{
-			//if(Logger.IsDebugEnabled)
-			//	Logger.Debug($"Recieved Movement Update for: {guid} with Data: {payload.MovementData.InitialPosition}");
-
 			try
 			{
-				//TODO: Handle position data better, we need to actually move the entities.
-				MovementDataMap[guid] = payload.MovementData;
-
-				//TODO: Nothing is actually upading the movement, we need ZoneServer movement generators.
-				ProjectVersionStage.AssertInternalTesting();
-
-				/*//TODO: Make it so that this is simplier to use.
-				//We must run this next part on the main thread because it sets a transform.
-				await new UnityYieldAwaitable();
-
-				//TODO: This is kinda demo code, directly setting the position of the root object.
-				WorldEntities[guid].transform.position = payload.MovementData.InitialPosition;*/
+				MovementGenerator[guid] = new ServerPlayerInputChangeMovementGenerator(payload.MovementInput, data => MovementDataMap[guid] = data);
 			}
 			catch(Exception e)
 			{

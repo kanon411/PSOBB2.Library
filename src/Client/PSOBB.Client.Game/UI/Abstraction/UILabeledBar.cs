6ee0ce09f27a5c446e5b1332c9c56494d5d5ede8
HelloKitty <andrew.blakely@ymail.com>
2019-02-14 20:22:20 -0600

    Added UILabeledBar

---
+++ src/Client/PSOBB.Client.Game/UI/Abstraction/UILabeledBar.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSOBB
+{
+	public sealed class UILabeledBar
+	{
+		public IUIText BarCurrent { get; }
+
+		public IUIText BarMaximum { get; }
+
+		public IUIFillableImage BarFillable { get; }
+
+		/// <inheritdoc />
+		public UILabeledBar([NotNull] IUIText barCurrent, [NotNull] IUIText barMaximum, [NotNull] IUIFillableImage barFillable)
+		{
+			BarCurrent = barCurrent ?? throw new ArgumentNullException(nameof(barCurrent));
+			BarMaximum = barMaximum ?? throw new ArgumentNullException(nameof(barMaximum));
+			BarFillable = barFillable ?? throw new ArgumentNullException(nameof(barFillable));
+		}
+	}
+}

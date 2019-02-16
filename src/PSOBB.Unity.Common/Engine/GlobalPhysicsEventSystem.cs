c418d40db3ce3ad649a4d066cd91fd4fbd06c878
HelloKitty <andrew.blakely@ymail.com>
2019-02-15 18:57:26 -0600

    Added hacky GlobalPhysicsEventSystem

---
+++ src/PSOBB.Unity.Common/Engine/GlobalPhysicsEventSystem.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PSOBB
+{
+	public sealed class GlobalPhysicsEventSystem : IPhysicsTriggerEventSubscribable
+	{
+		private object SyncObj = new object();
+
+		private Dictionary<PhysicsTriggerEventType, Action<object, PhysicsTriggerEventArgs>> PhysicsCallbackMap { get; }
+
+		public static GlobalPhysicsEventSystem Instance { get; } = new GlobalPhysicsEventSystem();
+
+		public GlobalPhysicsEventSystem()
+		{
+			PhysicsCallbackMap = new Dictionary<PhysicsTriggerEventType, Action<object, PhysicsTriggerEventArgs>>();
+		}
+
+		/// <inheritdoc />
+		public void RegisterTriggerEventSubscription(PhysicsTriggerEventType physicsType, [JetBrains.Annotations.NotNull] Action<object, PhysicsTriggerEventArgs> physicsCallback)
+		{
+			if(physicsCallback == null) throw new ArgumentNullException(nameof(physicsCallback));
+			if(!Enum.IsDefined(typeof(PhysicsTriggerEventType), physicsType)) throw new InvalidEnumArgumentException(nameof(physicsType), (int)physicsType, typeof(PhysicsTriggerEventType));
+
+			lock(SyncObj)
+			{
+				if(PhysicsCallbackMap.ContainsKey(physicsType))
+				{
+					PhysicsCallbackMap[physicsType] += physicsCallback;
+				}
+				else
+					PhysicsCallbackMap.Add(physicsType, physicsCallback);
+			}
+		}
+	}
+}

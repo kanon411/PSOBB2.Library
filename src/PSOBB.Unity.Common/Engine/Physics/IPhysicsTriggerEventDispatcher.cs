68ce06ab972821169f2bd247bd283acdd7d707bb
HelloKitty <andrew.blakely@ymail.com>
2019-02-15 19:33:08 -0600

    Added IPhysicsTriggerEventDispatcher

---
+++ src/PSOBB.Unity.Common/Engine/Physics/IPhysicsTriggerEventDispatcher.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PSOBB
+{
+	public interface IPhysicsTriggerEventDispatcher
+	{
+		void DispatchPhysicsEvent(PhysicsTriggerEventType physicsType, Collider colliderThatRanTrigger, Collider colliderThatTriggered);
+	}
+}

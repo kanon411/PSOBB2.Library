75f70ffa4b2bfdeef664ee0d5198abedfadc9288
HelloKitty <andrew.blakely@ymail.com>
2019-02-16 22:03:30 -0600

    Added EventQueueBasedTickable

---
+++ src/Server/PSOBB.Server.ZoneServer/Engine/EventQueueBasedTickable.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fasterflect;
+
+namespace PSOBB
+{
+	public abstract class EventQueueBasedTickable<TEventInterface, TEventArgs> : BaseSingleEventListenerInitializable<TEventInterface, TEventArgs>, IGameTickable
+		where TEventInterface : class 
+		where TEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Indicates if all events in <see cref="EventQueue"/> should be serviced each tick.
+		/// </summary>
+		public bool ServiceAllQueue { get; }
+
+		/// <summary>
+		/// Syncronization object.
+		/// </summary>
+		private readonly object SyncObj = new object();
+
+		private Queue<TEventArgs> EventQueue { get; }
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="subscriptionService">The subscription service.</param>
+		/// <param name="serviceAllQueue">Indicates if all events should be serviced from the event queue each tick.</param>
+		protected EventQueueBasedTickable(TEventInterface subscriptionService, bool serviceAllQueue) 
+			: base(subscriptionService)
+		{
+			ServiceAllQueue = serviceAllQueue;
+			EventQueue = new Queue<TEventArgs>(5);
+		}
+
+		/// <inheritdoc />
+		protected sealed override void OnEventFired(object source, TEventArgs args)
+		{
+			//For event queue based tickables we just enqueue the args recieved.
+			lock(SyncObj)
+				EventQueue.Enqueue(args);
+		}
+
+		/// <inheritdoc />
+		public void Tick()
+		{
+			//On tick we just check if the eventqueue has events
+			//if it does we can dispatch one. Otherwise,
+			if(EventQueue.Count == 0)
+				return;
+
+			if(ServiceAllQueue)
+			{
+				//We don't want to loop on count since it
+				//could change midloop and exploit could cause
+				//a server to stall under some circumstances.
+				int count = EventQueue.Count;
+
+				lock(SyncObj)
+					for(int i = 0; i < count; i++)
+					{
+						HandleEvent(EventQueue.Dequeue());
+					}
+
+				//At this point, it's possible that the EventQueue has more events
+				//but we are NOT going to handle them until next tick.
+			}
+			else
+			{
+				lock(SyncObj)
+					HandleEvent(EventQueue.Dequeue());
+			}
+		}
+
+		/// <summary>
+		/// Implementers should handle the event <see cref="args"/>
+		/// that will be dispatched during <see cref="Tick"/> on the main thread.
+		/// </summary>
+		/// <param name="args"></param>
+		protected abstract void HandleEvent(TEventArgs args);
+	}
+}

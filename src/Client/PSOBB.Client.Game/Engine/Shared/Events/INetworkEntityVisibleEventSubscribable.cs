e151370559df1e341f084d66906399bfbe374f4a
HelloKitty <andrew.blakely@ymail.com>
2019-02-17 02:41:14 -0600

    Added INetworkEntityVisibleEventSubscribable

---
+++ src/Client/PSOBB.Client.Game/Engine/Shared/Events/INetworkEntityVisibleEventSubscribable.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSOBB
+{
+	public interface INetworkEntityVisibleEventSubscribable
+	{
+		event EventHandler<NetworkEntityNowVisibleEventArgs> OnNetworkEntityNowVisible;
+	}
+
+	public sealed class NetworkEntityNowVisibleEventArgs : EventArgs, IEntityGuidContainer
+	{
+		//I know EntityGuid is apart of CreationData but it probbaly won't be for too much longer.
+		/// <summary>
+		/// The entity guid.
+		/// </summary>
+		public NetworkEntityGuid EntityGuid { get; }
+
+		/// <summary>
+		/// The creation data for the entity.
+		/// </summary>
+		public EntityCreationData CreationData { get; }
+
+		/// <inheritdoc />
+		public NetworkEntityNowVisibleEventArgs([NotNull] NetworkEntityGuid entityGuid, [NotNull] EntityCreationData creationData)
+		{
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+			CreationData = creationData ?? throw new ArgumentNullException(nameof(creationData));
+		}
+	}
+}

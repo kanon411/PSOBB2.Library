1fc0df3d8b68f977dbbe24e2e6fe6af83bf9f017
HelloKitty <andrew.blakely@ymail.com>
2019-02-16 21:51:45 -0600

    Added IPlayerSessionClaimedEventSubscribable

---
+++ src/Server/PSOBB.Server.ZoneServer/Engine/Events/IPlayerSessionClaimedEventSubscribable.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+namespace PSOBB
+{
+	public interface IPlayerSessionClaimedEventSubscribable
+	{
+		event EventHandler<PlayerSessionClaimedEventArgs> OnSuccessfulSessionClaimed;
+	}
+
+	public sealed class PlayerSessionClaimedEventArgs : EventArgs
+	{
+		public NetworkEntityGuid EntityGuid { get; }
+
+		public Vector3 SpawnPosition { get; }
+
+		/// <summary>
+		/// The connection ID of the session.
+		/// </summary>
+		public int ConnectionId { get; }
+
+		/// <inheritdoc />
+		public PlayerSessionClaimedEventArgs([NotNull] NetworkEntityGuid entityGuid, Vector3 spawnPosition, int connectionId)
+		{
+			if(connectionId < 0) throw new ArgumentOutOfRangeException(nameof(connectionId));
+
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+			SpawnPosition = spawnPosition;
+			ConnectionId = connectionId;
+		}
+	}
+}

6f4fa2282610d4f8640b92507f8263ee8af4783f
HelloKitty <andrew.blakely@ymail.com>
2019-02-16 22:22:06 -0600

    Refactored PlayerEntityEntryManager to new EventQueueBasedTickable design

--- src/Server/PSOBB.Server.ZoneServer/Engine/Tickable/PlayerEntityEntryManager.cs
+++ src/Server/PSOBB.Server.ZoneServer/Engine/Tickable/PlayerEntityEntryManager.cs
@@ -9,28 +9,19 @@ using UnityEngine;
 
 namespace PSOBB
 {
-	//TODO: Refactor clean this mess up
+	[AdditionalRegisterationAs(typeof(IPlayerWorldSessionCreatedEventSubscribable))]
 	[GameInitializableOrdering(2)]
 	[SceneTypeCreate(GameSceneType.DefaultLobby)]
-	public sealed class PlayerEntityEntryManager : IGameTickable, ITickableSkippable
+	public sealed class PlayerEntityEntryManager : EventQueueBasedTickable<IPlayerSessionClaimedEventSubscribable, PlayerSessionClaimedEventArgs>, IPlayerWorldSessionCreatedEventSubscribable
 	{
-		private IDequeable<KeyValuePair<NetworkEntityGuid, PlayerEntityEnterWorldCreationContext>> PlayerEntitySessionDequeable { get; }
-
 		private IFactoryCreatable<GameObject, PlayerEntityCreationContext> PlayerFactory { get; }
 
 		private INetworkMessageSender<GenericSingleTargetMessageContext<PlayerSelfSpawnEventPayload>> SpawnPayloadSender { get; }
 
-		private ILog Logger { get; }
-
 		private IFactoryCreatable<FieldValueUpdate, EntityFieldUpdateCreationContext> EntityDataUpdateFactory { get; }
 
-		private ISessionCollection SessionCollection { get; }
-
-		private PlayerSessionDeconstructionQueue SessionCleanupQueue { get; }
-
-		//If it's empty, this can be skipped.
 		/// <inheritdoc />
-		public bool IsTickableSkippable => PlayerEntitySessionDequeable.isEmpty;
+		public event EventHandler<PlayerWorldSessionCreationEventArgs> OnPlayerWorldSessionCreated;
 
 		/// <summary>
 		/// The collections locking policy.
@@ -39,79 +30,53 @@ namespace PSOBB
 
 		/// <inheritdoc />
 		public PlayerEntityEntryManager(
-			[NotNull] IDequeable<KeyValuePair<NetworkEntityGuid, PlayerEntityEnterWorldCreationContext>> playerEntitySessionDequeable, 
-			[NotNull] IFactoryCreatable<GameObject, PlayerEntityCreationContext> playerFactory,
-			[NotNull] INetworkMessageSender<GenericSingleTargetMessageContext<PlayerSelfSpawnEventPayload>> spawnPayloadSender,
-			[NotNull] ILog logger,
-			[NotNull] IFactoryCreatable<FieldValueUpdate, EntityFieldUpdateCreationContext> entityDataUpdateFactory,
-			[NotNull] ISessionCollection sessionCollection,
-			[NotNull] PlayerSessionDeconstructionQueue sessionCleanupQueue,
-			[NotNull] GlobalEntityCollectionsLockingPolicy lockingPolicy)
+			IPlayerSessionClaimedEventSubscribable subscriptionService, 
+			IFactoryCreatable<GameObject, PlayerEntityCreationContext> playerFactory, 
+			INetworkMessageSender<GenericSingleTargetMessageContext<PlayerSelfSpawnEventPayload>> spawnPayloadSender, 
+			IFactoryCreatable<FieldValueUpdate, EntityFieldUpdateCreationContext> entityDataUpdateFactory,
+			ILog logger,
+			GlobalEntityCollectionsLockingPolicy lockingPolicy) 
+			: base(subscriptionService, false, logger)
 		{
-			PlayerEntitySessionDequeable = playerEntitySessionDequeable ?? throw new ArgumentNullException(nameof(playerEntitySessionDequeable));
-			PlayerFactory = playerFactory ?? throw new ArgumentNullException(nameof(playerFactory));
-			SpawnPayloadSender = spawnPayloadSender ?? throw new ArgumentNullException(nameof(spawnPayloadSender));
-			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
-			EntityDataUpdateFactory = entityDataUpdateFactory ?? throw new ArgumentNullException(nameof(entityDataUpdateFactory));
-			SessionCollection = sessionCollection ?? throw new ArgumentNullException(nameof(sessionCollection));
-			SessionCleanupQueue = sessionCleanupQueue ?? throw new ArgumentNullException(nameof(sessionCleanupQueue));
-			LockingPolicy = lockingPolicy ?? throw new ArgumentNullException(nameof(lockingPolicy));
+			PlayerFactory = playerFactory;
+			SpawnPayloadSender = spawnPayloadSender;
+			EntityDataUpdateFactory = entityDataUpdateFactory;
+			LockingPolicy = lockingPolicy;
 		}
 
 		/// <inheritdoc />
-		public void Tick()
+		protected override void HandleEvent(PlayerSessionClaimedEventArgs args)
 		{
-			//If we have no new players we can just 
-			if(PlayerEntitySessionDequeable.isEmpty)
-				return;
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"Dequeueing entity creation request for: {args.EntityGuid.EntityType}:{args.EntityGuid.EntityId}");
 
-			using(LockingPolicy.WriterLock(null, CancellationToken.None))
-			{
-				//TODO: Should we limit this? We might want to stagger this or else under extreme conditions we could lag the main thread.
-				while(!PlayerEntitySessionDequeable.isEmpty)
-				{
-					KeyValuePair<NetworkEntityGuid, PlayerEntityEnterWorldCreationContext> dequeuedPlayerSession = PlayerEntitySessionDequeable.Dequeue();
+			//TODO: This is test data
+			EntityFieldDataCollection<EntityDataFieldType> testData = new EntityFieldDataCollection<EntityDataFieldType>();
 
-					if(Logger.IsDebugEnabled)
-						Logger.Debug($"Dequeueing entity creation request for: {dequeuedPlayerSession.Key.EntityType}:{dequeuedPlayerSession.Key.EntityId}");
+			//TODO: Test values set
+			testData.SetFieldValue(EntityDataFieldType.EntityCurrentHealth, 100);
+			testData.SetFieldValue(EntityDataFieldType.EntityMaxHealth, 120);
 
-					//TODO: This is test data
-					EntityFieldDataCollection<EntityDataFieldType> testData = new EntityFieldDataCollection<EntityDataFieldType>();
+			//TODO: Time stamp
+			//TODO: We should check if the result is valid? Maybe return a CreationResult?
+			//We don't need to do anything with the returned object.
+			GameObject playerGameObject = PlayerFactory.Create(new PlayerEntityCreationContext(args.EntityGuid, args.SessionContext, new PositionChangeMovementData(0, args.SpawnPosition, Vector2.zero), EntityPrefab.RemotePlayer, testData));
 
-					//TODO: Test values set
-					testData.SetFieldValue(EntityDataFieldType.EntityCurrentHealth, 100);
-					testData.SetFieldValue(EntityDataFieldType.EntityMaxHealth, 120);
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"Sending player spawn payload Id: {args.EntityGuid.EntityId}");
 
-					//TODO: Time stamp
-					//TODO: We should check if the result is valid? Maybe return a CreationResult?
-					//We don't need to do anything with the returned object.
-					GameObject playerGameObject = PlayerFactory.Create(new PlayerEntityCreationContext(dequeuedPlayerSession.Key, dequeuedPlayerSession.Value.SessionContext, new PositionChangeMovementData(0, dequeuedPlayerSession.Value.SpawnPosition, Vector2.zero), EntityPrefab.RemotePlayer, testData));
+			OnPlayerWorldSessionCreated?.Invoke(this, new PlayerWorldSessionCreationEventArgs(args.EntityGuid));
 
-					if(Logger.IsDebugEnabled)
-						Logger.Debug($"Sending player spawn payload Id: {dequeuedPlayerSession.Key.EntityId}");
-
-					//This should always contain it.
-					//The reason we check if we're still connected is we want to send the spawn event to the player
-					//if they are connected. If the connection has ended, then we want to enqueue an entity deconstruction request.
-					if(SessionCollection.Retrieve(dequeuedPlayerSession.Value.SessionContext.ConnectionId).Connection.isConnected)
-						//Once added we then need to send to the client a packet indicating its creation
-						SpawnPayloadSender.Send(BuildSpawnEventPayload(dequeuedPlayerSession, testData));
-					else
-						//Warning: This can end up with duplicate enqueued deconstruction requests. But ConnectionId is never shared or reused. So it's ok, it'll just be ingnored in those cases.
-						SessionCleanupQueue.Enqueue(new PlayerSessionDeconstructionContext(dequeuedPlayerSession.Value.SessionContext.ConnectionId));
-
-					//TODO: If we want to do anything post-creation with the provide gameobject we could. But we really don't want to at the moment.
-				}
-			}
+			//TODO: If we want to do anything post-creation with the provide gameobject we could. But we really don't want to at the moment.
 		}
 
-		private GenericSingleTargetMessageContext<PlayerSelfSpawnEventPayload> BuildSpawnEventPayload(KeyValuePair<NetworkEntityGuid, PlayerEntityEnterWorldCreationContext> dequeuedPlayerSession, EntityFieldDataCollection<EntityDataFieldType> testData)
+		/*private GenericSingleTargetMessageContext<PlayerSelfSpawnEventPayload> BuildSpawnEventPayload(KeyValuePair<NetworkEntityGuid, PlayerEntityEnterWorldCreationContext> dequeuedPlayerSession, EntityFieldDataCollection<EntityDataFieldType> testData)
 		{
 			//TODO: Time stamp
 			//TODO: get real movement info
 			EntityCreationData data = new EntityCreationData(dequeuedPlayerSession.Key, new PositionChangeMovementData(0, dequeuedPlayerSession.Value.SpawnPosition, Vector2.zero), EntityDataUpdateFactory.Create(new EntityFieldUpdateCreationContext(testData, testData.DataSetIndicationArray)));
 
 			return new GenericSingleTargetMessageContext<PlayerSelfSpawnEventPayload>(dequeuedPlayerSession.Key, new PlayerSelfSpawnEventPayload(data));
-		}
+		}*/
 	}
 }

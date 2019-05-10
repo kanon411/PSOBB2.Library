809343ee494dbf9b86175ef83e49eeda9e19b201
HelloKitty <andrew.blakely@ymail.com>
2019-05-10 06:10:28 -0500

    Updated Buildscripts

--- build-norestore-zoneserver.bat
+++
@@ -1,6 +0,0 @@
-dotnet publish src/server/PSOBB.Server.ZoneServer/PSOBB.Server.ZoneServer.csproj -c Release
-
-if not exist "build\zoneserver" mkdir build\zoneserver
-
-xcopy src\server\PSOBB.Server.ZoneServer\bin\Release\netstandard2.0\publish build\zoneserver /Y /q /EXCLUDE:BuildExclude.txt
-EXIT 0
--- build.bat
+++ build.bat
@@ -1,6 +1,5 @@
-dotnet restore PSOBB.Library.sln
+dotnet restore GladMMO.Library.sln
 if not exist "build" mkdir build
 start cmd /c build-norestore-client.bat
-start cmd /c build-norestore-zoneserver.bat
 start cmd /c build-norestore-backend.bat
 EXIT 0

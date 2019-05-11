b007bad503ae6d1a5bac12dd3788261dbdb6040b
HelloKitty <andrew.blakely@ymail.com>
2019-05-10 19:44:17 -0500

    Added BaseUnityUITypeEditor for enum dropdown

---
+++ src/GladMMO.Client.Editor/TypeEditors/BaseUnityUITypeEditor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Fasterflect;
+using Glader.Essentials;
+using UnityEditor;
+
+namespace GladMMO
+{
+	[CustomEditor(typeof(BaseUnityUI), true)]
+	public sealed class BaseUnityUITypeEditor : Editor
+	{
+		/// <inheritdoc />
+		public override void OnInspectorGUI()
+		{
+			base.OnInspectorGUI();
+
+			var t = (BaseUnityUI)target;
+
+			//HelloKitty: At the bottom we'll just add the enum popup/dropdown for easy selection.
+			t.SetFieldValue("_RegisterationKey", Convert.ToInt32(EditorGUILayout.EnumPopup((UnityUIRegisterationKey)t.RegisterationKey)));
+		}
+	}
+}

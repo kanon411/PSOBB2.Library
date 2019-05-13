a43f44632a78e2fe0aa2eefeec7078f05fc5dbe1
HelloKitty <andrew.blakely@ymail.com>
2019-05-12 23:01:57 -0500

    Enabled character selection networking to TrinityCore

--- src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener.cs
+++ src/Client/GladMMO.Client.Game/Engine/CharacterSelection/Initializables/CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using System.Text;
 using Common.Logging;
+using FreecraftCore;
 using Glader.Essentials;
+using GladNet;
 using Nito.AsyncEx;
 using UnityEngine;
 using UnityEngine.SceneManagement;
@@ -15,44 +17,40 @@ namespace GladMMO
 	{
 		private ICharacterDataRepository CharacterData { get; }
 
-		private IReadonlyAuthTokenRepository AuthTokenRepo { get; }
-
 		private ILog Logger { get; }
+
+		private IPeerPayloadSendService<GamePacketPayload> SendService { get; }
 
 		/// <inheritdoc />
 		public CharacterSelectionSelectCharacterImmediatelyOnButtonClickedEventListener(
 			[NotNull] ICharacterSelectionButtonClickedEventSubscribable subscriptionService, 
 			[NotNull] ICharacterDataRepository characterData, 
-			[NotNull] IReadonlyAuthTokenRepository authTokenRepo,
-			[NotNull] ILog logger) 
+			[NotNull] ILog logger,
+			[NotNull] IPeerPayloadSendService<GamePacketPayload> sendService) 
 			: base(subscriptionService)
 		{
 			//CharacterService = characterService ?? throw new ArgumentNullException(nameof(characterService));
 			CharacterData = characterData ?? throw new ArgumentNullException(nameof(characterData));
-			AuthTokenRepo = authTokenRepo ?? throw new ArgumentNullException(nameof(authTokenRepo));
 			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			SendService = sendService ?? throw new ArgumentNullException(nameof(sendService));
 		}
 
 		/// <inheritdoc />
 		protected override void OnEventFired(object source, CharacterButtonClickedEventArgs args)
 		{
-			UnityAsyncHelper.UnityMainThreadContext.PostAsync(async () =>
-			{
-				//We must actually create a session
-				//before the character can login.
-				/*CharacterSessionEnterResponse enterResponse = await CharacterService.TryEnterSession(args.CharacterGuid.EntityId, AuthTokenRepo.RetrieveWithType())
-					.ConfigureAwait(true);
+			//So, this just tells TrinityCore that we want to login into THIS character.
+			//So, it'll attempt to do so.
+			//There isn't really a response to this, TrinityCore will just send back: SMSG_LOGIN_VERIFY_WORLD
+			//when it's successful and then the player will begin actually creating itself in the world and recieving
+			//some data about the world state.
+			SendService.SendMessage(new CharacterLoginRequest(args.CharacterGuid));
 
-				//TODO: handle character session failure
-				CharacterData.UpdateCharacterId(args.CharacterGuid.EntityId);
+			//TODO: handle character session failure
+			CharacterData.UpdateCharacterId(args.CharacterGuid.CurrentObjectGuid);
 
-				if(Logger.IsInfoEnabled)
-					Logger.Info($"Recieved character session entry response: {enterResponse.isSuccessful} Result: {enterResponse.ResultCode} for ZoneId: {enterResponse.ZoneId}");*/
-
-				//TODO: Use the scene manager service.
-				//TODO: Don't hardcode scene ids. Don't load scenes directly.
-				SceneManager.LoadSceneAsync(2).allowSceneActivation = true;
-			});
+			//TODO: Use the scene manager service.
+			//TODO: Don't hardcode scene ids. Don't load scenes directly.
+			SceneManager.LoadSceneAsync(2).allowSceneActivation = true;
 		}
 	}
 }

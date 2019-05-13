efa148d2323cd192abbe3e8c134ea59977c45d89
HelloKitty <andrew.blakely@ymail.com>
2019-05-13 01:19:36 -0500

    Removed Uneeded RegisterEntityMappableCollections

--- src/Client/GladMMO.Client.Game/IoC/Modules/LobbyGameDependenciesAutofacModule.cs
+++ src/Client/GladMMO.Client.Game/IoC/Modules/LobbyGameDependenciesAutofacModule.cs
@@ -35,37 +35,10 @@ namespace GladMMO
 				.AsImplementedInterfaces()
 				.SingleInstance();
 
-			RegisterEntityMappableCollections(builder);
-
 			//TODO: This is legacy
 			builder.RegisterType<DefaultGameObjectToEntityMappable>()
 				.As<IReadonlyGameObjectToEntityMappable>()
 				.As<IGameObjectToEntityMappable>()
-				.SingleInstance();
-		}
-
-		//TODO: Refactor into module.
-		private static void RegisterEntityMappableCollections(ContainerBuilder builder)
-		{
-			//The below is kinda a hack to register the non-generic types in the
-			//removabale collection
-			List<IEntityCollectionRemovable> removableComponentsList = new List<IEntityCollectionRemovable>(10);
-
-			builder.RegisterGeneric(typeof(EntityGuidDictionary<>))
-				.AsSelf()
-				.As(typeof(IReadonlyEntityGuidMappable<>))
-				.As(typeof(IEntityGuidMappable<>))
-				.OnActivated(args =>
-				{
-					if(args.Instance is IEntityCollectionRemovable removable)
-						removableComponentsList.Add(removable);
-				})
-				.SingleInstance();
-
-			//This will allow everyone to register the removable collection collection.
-			builder.RegisterInstance(removableComponentsList)
-				.AsImplementedInterfaces()
-				.AsSelf()
 				.SingleInstance();
 		}
 	}

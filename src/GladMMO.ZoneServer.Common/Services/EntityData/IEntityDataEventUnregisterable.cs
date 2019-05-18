ddfcbbd53a92a3a6fdb26996c6a1cc8fb043358f
HelloKitty <andrew.blakely@ymail.com>
2019-05-18 11:18:06 -0500

    Added IEntityDataEventUnregisterable

---
+++ src/GladMMO.ZoneServer.Common/Services/EntityData/IEntityDataEventUnregisterable.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Contract for types that can handle unregisters events.
+	/// </summary>
+	public interface IEntityDataEventUnregisterable
+	{
+		/// <summary>
+		/// Unregisters a stored event registeration.
+		/// </summary>
+		void Unregister();
+	}
+}

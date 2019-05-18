61321c0a97bce9b9f6f55db93013fcf7f0693c3d
HelloKitty <andrew.blakely@ymail.com>
2019-05-18 09:27:17 -0500

    Added UnitFrameCollectionUnityUIAdapter

---
+++ src/Client/GladMMO.Client.Game/UI/Adapters/UnitFrameCollectionUnityUIAdapter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+using UnityEngine;
+
+namespace GladMMO
+{
+	/// <summary>
+	/// Exposed designers the ability to register collections of <see cref="IUILabeledButton"/>s.
+	/// </summary>
+	public sealed class UnitFrameCollectionUnityUIAdapter : BaseCollectionUnityUIAdapter<IUIUnitFrame>
+	{
+		[SerializeField]
+		[Tooltip("The collection of labeled buttons to aggregate.")]
+		private UnitFrameElementsAdapter[] _elements;
+
+		/// <inheritdoc />
+		protected override IEnumerable<IUIUnitFrame> Elements => _elements;
+	}
+}

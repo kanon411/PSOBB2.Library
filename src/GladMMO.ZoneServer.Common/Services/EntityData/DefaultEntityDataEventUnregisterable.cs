d60d61288ac042509609b9a252bfbd2e15cbfbef
HelloKitty <andrew.blakely@ymail.com>
2019-05-18 11:18:17 -0500

    Added DefaultEntityDataEventUnregisterable

---
+++ src/GladMMO.ZoneServer.Common/Services/EntityData/DefaultEntityDataEventUnregisterable.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GladMMO
+{
+	public sealed class DefaultEntityDataEventUnregisterable : IEntityDataEventUnregisterable
+	{
+		private Action RemoveDelegate { get; }
+
+		private bool isUnregistered { get; set; }
+
+		/// <inheritdoc />
+		public DefaultEntityDataEventUnregisterable([NotNull] Action removeDelegate)
+		{
+			RemoveDelegate = removeDelegate ?? throw new ArgumentNullException(nameof(removeDelegate));
+			isUnregistered = false;
+		}
+
+		/// <inheritdoc />
+		public void Unregister()
+		{
+			if(!isUnregistered)
+			{
+				RemoveDelegate();
+				isUnregistered = true;
+			}
+		}
+	}
+}

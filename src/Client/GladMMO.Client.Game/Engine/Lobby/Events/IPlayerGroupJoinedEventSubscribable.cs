f638c40d1610bd2f8916415ede76711d9703263b
HelloKitty <andrew.blakely@ymail.com>
2019-05-18 03:47:56 -0500

    Added group join/leave event interfaces

---
+++ src/Client/GladMMO.Client.Game/Engine/Lobby/Events/IPlayerGroupJoinedEventSubscribable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public interface IPlayerGroupJoinedEventSubscribable
+	{
+		event EventHandler<PlayerJoinedGroupEventArgs> OnPlayerJoinedGroup;
+	}
+
+	public sealed class PlayerJoinedGroupEventArgs : EventArgs
+	{
+		public ObjectGuid PlayerGuid { get; }
+
+		/// <inheritdoc />
+		public PlayerJoinedGroupEventArgs([NotNull] ObjectGuid playerGuid)
+		{
+			PlayerGuid = playerGuid ?? throw new ArgumentNullException(nameof(playerGuid));
+		}
+	}
+}
---
+++ src/Client/GladMMO.Client.Game/Engine/Lobby/Events/IPlayerGroupLeftEventSubscribable.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO
+{
+	public interface IPlayerGroupLeftEventSubscribable
+	{
+		event EventHandler<PlayerJoinedGroupEventArgs> OnPlayerLeftGroup;
+	}
+
+	public sealed class PlayerLeftGroupEventArgs : EventArgs
+	{
+		public ObjectGuid PlayerGuid { get; }
+
+		/// <inheritdoc />
+		public PlayerLeftGroupEventArgs([NotNull] ObjectGuid playerGuid)
+		{
+			PlayerGuid = playerGuid ?? throw new ArgumentNullException(nameof(playerGuid));
+		}
+	}
+}

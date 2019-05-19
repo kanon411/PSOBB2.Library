7d20aa60ed90929ccbd15d96038b135c3a22b43b
HelloKitty <andrew.blakely@ymail.com>
2019-05-19 05:26:17 -0500

    Added IGroupUnitFrameIssuable

---
+++ src/Client/GladMMO.Client.Game/Services/Group/IGroupUnitFrameIssuable.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+
+namespace GladMMO.Services
+{
+	public interface IGroupUnitFrameIssuable
+	{
+		/// <summary>
+		/// Attempts to claim a group unitframe for
+		/// the provided Player <see cref="guid"/>.
+		/// </summary>
+		/// <param name="guid">The guid of the player.</param>
+		/// <returns>True if a unitframe could be claimed.</returns>
+		bool TryClaimUnitFrame(ObjectGuid guid);
+
+		/// <summary>
+		/// Attempts to release a claimed unitframe
+		/// for the provided Player <see cref="guid"/>.
+		/// </summary>
+		/// <param name="guid">The guid of the player.</param>
+		/// <returns>True if a unitframe was claimed and successfully released for the player.</returns>
+		bool TryReleaseUnitFrame(ObjectGuid guid);
+	}
+}

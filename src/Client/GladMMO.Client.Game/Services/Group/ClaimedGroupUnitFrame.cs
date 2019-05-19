3cbf90829ffe4cc59f96fe7f11c8d4fb3b1b24c3
HelloKitty <andrew.blakely@ymail.com>
2019-05-19 06:29:33 -0500

    Added ClaimedGroupUnitFrame

---
+++ src/Client/GladMMO.Client.Game/Services/Group/ClaimedGroupUnitFrame.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public sealed class ClaimedGroupUnitFrame
+	{
+		public IUIUnitFrame UnitFrame { get; }
+
+		private List<IEntityDataEventUnregisterable> Unregisterables { get; }
+
+		/// <inheritdoc />
+		public ClaimedGroupUnitFrame(IUIUnitFrame unitFrame)
+		{
+			UnitFrame = unitFrame;
+			Unregisterables = new List<IEntityDataEventUnregisterable>(5);
+		}
+
+		public void ClearRegisteredCallbacks()
+		{
+			foreach(var unr in Unregisterables)
+				unr.Unregister();
+
+			Unregisterables.Clear();
+		}
+	}
+}

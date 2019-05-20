c83214b4cbbbf1c5ca29345f4981ddfe7952371f
HelloKitty <andrew.blakely@ymail.com>
2019-05-20 12:45:04 -0500

    Readded slim form of DefaultEntityFactory

---
+++ src/GladMMO.ZoneServer.Common/Factory/Context/DefaultEntityCreationContext.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using FreecraftCore;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public sealed class DefaultEntityCreationContext : IEntityCreationContext
+	{
+		/// <inheritdoc />
+		public ObjectGuid EntityGuid { get; }
+
+		/// <inheritdoc />
+		public Vector3 InitialPosition { get; }
+
+		/// <inheritdoc />
+		public float Orientation { get; }
+
+		/// <inheritdoc />
+		public EntityPrefab PrefabType { get; }
+
+		/// <inheritdoc />
+		public DefaultEntityCreationContext([NotNull] ObjectGuid entityGuid, Vector3 initialPosition, float orientation, EntityPrefab prefabType)
+		{
+			if(!Enum.IsDefined(typeof(EntityPrefab), prefabType)) throw new InvalidEnumArgumentException(nameof(prefabType), (int)prefabType, typeof(EntityPrefab));
+			EntityGuid = entityGuid ?? throw new ArgumentNullException(nameof(entityGuid));
+
+			InitialPosition = initialPosition;
+			Orientation = orientation;
+			PrefabType = prefabType;
+		}
+	}
+}
---
+++ src/GladMMO.ZoneServer.Common/Factory/DefaultEntityFactory.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Common.Logging;
+using FreecraftCore;
+using Nito.AsyncEx;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public class DefaultEntityFactory<TCreationContext> : IFactoryCreatable<GameObject, TCreationContext>
+		where TCreationContext : IEntityCreationContext
+	{
+		private ILog Logger { get; }
+
+		private IEntityGuidMappable<GameObject> GuidToGameObjectMappable { get; }
+
+		private IGameObjectToEntityMappable GameObjectToEntityMap { get; }
+
+		private IFactoryCreatable<GameObject, EntityPrefab> PrefabFactory { get; }
+
+		/// <inheritdoc />
+		public DefaultEntityFactory(
+			ILog logger,
+			IEntityGuidMappable<GameObject> guidToGameObjectMappable,
+			IGameObjectToEntityMappable gameObjectToEntityMap,
+			IFactoryCreatable<GameObject, EntityPrefab> prefabFactory)
+		{
+			Logger = logger ?? throw new ArgumentNullException(nameof(logger));
+			GuidToGameObjectMappable = guidToGameObjectMappable ?? throw new ArgumentNullException(nameof(guidToGameObjectMappable));
+			GameObjectToEntityMap = gameObjectToEntityMap ?? throw new ArgumentNullException(nameof(gameObjectToEntityMap));
+			PrefabFactory = prefabFactory ?? throw new ArgumentNullException(nameof(prefabFactory));
+		}
+
+		/// <inheritdoc />
+		public GameObject Create(TCreationContext context)
+		{
+			if(Logger.IsDebugEnabled)
+				Logger.Debug($"Creating entity. Type: {context.EntityGuid.ObjectType} Id: {context.EntityGuid.CurrentObjectGuid}");
+
+			//load the entity's prefab from the factory
+			GameObject prefab = PrefabFactory.Create(context.PrefabType);
+
+			GameObject entityGameObject = GameObject.Instantiate(prefab, context.InitialPosition, Quaternion.Euler(0, 0, 0));
+
+			GameObjectToEntityMap.ObjectToEntityMap.Add(entityGameObject, context.EntityGuid);
+
+			GuidToGameObjectMappable.Add(context.EntityGuid, entityGameObject);
+
+			return entityGameObject;
+		}
+	}
+}

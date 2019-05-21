fe06b4d44d0c571fe5bc9f5998893459bb8f61bf
HelloKitty <andrew.blakely@ymail.com>
2019-05-21 04:49:28 -0500

    Added initial conversion between WoW coordinate to Unity coordinates

---
+++ src/GladMMO.Unity.Common/Extensions/GenericVectorConversionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using FreecraftCore;
+using UnityEngine;
+
+namespace GladMMO
+{
+	public static class GenericVectorConversionExtensions
+	{
+		public static Vector3 ToUnityVector(this Vector3<float> freecraftVector)
+		{
+			//HelloKitty: So, in WoW compared to Unity3D the coordinate system is as follows
+			//Z is actually up in WoW.
+			return new Vector3(freecraftVector.X, freecraftVector.Z, freecraftVector.Y);
+		}
+	}
+}

eb62ff2a8429ae1eec24bc446484af0f2bb6ac7e
HelloKitty <andrew.blakely@ymail.com>
2019-05-22 11:26:49 -0500

    Fixed Vector translation

--- src/GladMMO.Unity.Common/Extensions/GenericVectorConversionExtensions.cs
+++ src/GladMMO.Unity.Common/Extensions/GenericVectorConversionExtensions.cs
@@ -12,7 +12,12 @@ namespace GladMMO
 		{
 			//HelloKitty: So, in WoW compared to Unity3D the coordinate system is as follows
 			//Z is actually up in WoW.
-			return new Vector3(freecraftVector.X, freecraftVector.Z, freecraftVector.Y);
+			return new Vector3(freecraftVector.Y, freecraftVector.Z, freecraftVector.X);
+		}
+
+		public static Vector3<float> ToWoWVector(this ref Vector3 unityVector)
+		{
+			return new Vector3<float>(unityVector.z, unityVector.x, unityVector.y);
 		}
 	}
 }

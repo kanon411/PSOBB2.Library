07bbb0450c3c485f7a338a6603ea5dd7d1e8ba5a
HelloKitty <andrew.blakely@ymail.com>
2019-05-22 15:58:43 -0500

    Added IUICharacterSlot

---
+++ src/Client/GladMMO.Client.Game/UI/Interfaces/IUICharacterSlot.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Glader.Essentials;
+
+namespace GladMMO
+{
+	public interface IUICharacterSlot : IUIElement, IUIToggle, IUIText
+	{
+		
+	}
+}

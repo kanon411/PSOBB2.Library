bbb2064a3cc7178bd8e7e514a0d806df81296635
HelloKitty <andrew.blakely@ymail.com>
2019-05-25 13:39:42 -0500

    Partially added back some missing backend services

---
+++ src/Server/GladMMO.Database.GameServer/Repositories/Social/IGuildCharacterMembershipRepository.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PSOBB
+{
+	public interface IGuildCharacterMembershipRepository : IGenericRepositoryCrudable<int, CharacterGuildMemberRelationshipModel>
+	{
+		
+	}
+}

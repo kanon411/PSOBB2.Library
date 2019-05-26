510dcfc97f0d0637fe91cec17147313b7b249890
HelloKitty <andrew.blakely@ymail.com>
2019-05-25 19:51:13 -0500

    Added CharacterSelectionDependencyAutofacModule

---
+++ src/Client/GladMMO.Client.Game/IoC/Modules/CharacterSelectionDependencyAutofacModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Autofac;
+using Refit;
+
+namespace GladMMO
+{
+	public sealed class CharacterSelectionDependencyAutofacModule : NetworkServiceDiscoveryableAutofaceModule
+	{
+		/// <inheritdoc />
+		protected override void Load(ContainerBuilder builder)
+		{
+			builder.Register<ICharacterService>(context =>
+			{
+				IServiceDiscoveryService serviceDiscovery = context.Resolve<IServiceDiscoveryService>();
+
+				return new AsyncEndpointCharacterService(QueryForRemoteServiceEndpoint(serviceDiscovery, "GameServer"), new RefitSettings() { HttpMessageHandlerFactory = () => new FiddlerEnabledWebProxyHandler() });
+			});
+		}
+	}
+}

899312a4e1db87deaff97158c35df51505271530
HelloKitty <andrew.blakely@ymail.com>
2019-05-26 12:15:38 -0500

    Rewrote INameQueryService to use JSON request and response

--- src/Client/GladMMO.Client.NameQueryService/INameQueryService.cs
+++ src/Client/GladMMO.Client.NameQueryService/INameQueryService.cs
@@ -3,9 +3,11 @@ using System.Collections.Generic;
 using System.Text;
 using System.Threading.Tasks;
 using FreecraftCore;
+using Refit;
 
 namespace GladMMO
 {
+	[Headers("User-Agent: GuardiansClient")]
 	public interface INameQueryService
 	{
 		/// <summary>
@@ -13,8 +15,9 @@ namespace GladMMO
 		/// from the provided <see cref="entity"/>.
 		/// </summary>
 		/// <param name="entity">The id of the entity.</param>
-		/// <exception cref="KeyNotFoundException">Throws if the key is not found.</exception> 
-		/// <returns>The name.</returns>
-		Task<string> RetrieveAsync(NetworkEntityGuid entity);
+		/// <returns>Result of the namequery.</returns>
+		[Headers("Cache-Control: max-age=600")]
+		[Get("/api/namequery")]
+		Task<NameQueryResponse> RetrieveAsync([JsonBody] NetworkEntityGuid entity);
 	}
 }

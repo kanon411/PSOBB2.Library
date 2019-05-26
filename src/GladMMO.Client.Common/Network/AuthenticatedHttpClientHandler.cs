a0cb789f5d052e50c9908899123ddbeb4755f536
HelloKitty <andrew.blakely@ymail.com>
2019-05-26 13:58:07 -0500

    Added AuthenticatedHttpClientHandler

---
+++ src/GladMMO.Client.Common/Network/AuthenticatedHttpClientHandler.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace GladMMO
+{
+	public sealed class AuthenticatedHttpClientHandler : HttpClientHandler
+	{
+		private IReadonlyAuthTokenRepository AuthTokenProvider { get; }
+
+		/// <inheritdoc />
+		public AuthenticatedHttpClientHandler([NotNull] IReadonlyAuthTokenRepository authTokenProvider)
+		{
+			AuthTokenProvider = authTokenProvider ?? throw new ArgumentNullException(nameof(authTokenProvider));
+		}
+
+		protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+		{
+			//This will set the authentication token if it's required.
+			AuthenticationHeaderValue auth = request.Headers.Authorization;
+			if(auth != null)
+				request.Headers.Authorization = new AuthenticationHeaderValue(AuthTokenProvider.RetrieveType(), AuthTokenProvider.Retrieve());
+
+			return base.SendAsync(request, cancellationToken);
+		}
+	}
+}
